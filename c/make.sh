057f51e8f847b40379a696350cf002d42d9b1764
dpitts <dpitts@ese-web.com>
2013-07-17 19:27:29 -0600

    Added

--- c/make.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo Compiling FPPD ...
-gcc -o fppd fppd.c E131.c mpg123.c pixelnetDMX.c playList.c schedule.c command.c -w -l pthread -l wiringPi
-echo Compiling FPP
-gcc -o fpp fpp.c
