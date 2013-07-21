55ba1c31c19744369e3496f7431c04c0e2f106e4
dpitts <dpitts@ese-web.com>
2013-07-21 03:11:47 -0600

    lower case

---
+++ web/scheduleentry.php
@@ -0,0 +1,30 @@
+<?php
+class ScheduleEntry {
+  public $enable;
+  public $playlist;
+  public $startDay;
+  public $startHour;
+  public $startMinute;
+  public $startSecond;
+  public $endDay;
+  public $endHour;
+  public $endMinute;
+  public $endSecond;
+  public $repeat;
+
+  public function  __construct($enable,$playlist,$startDay,$startHour,$startMinute,$startSecond,
+	                             $endDay,$endHour,$endMinute,$endSecond,$repeat) {
+    $this->enable = $enable;
+    $this->playlist = $playlist;
+    $this->startDay = $startDay;
+    $this->startHour = $startHour;
+    $this->startMinute = $startMinute;
+    $this->startSecond = $startSecond;
+    $this->endDay = $endDay;
+    $this->endHour = $endHour;
+    $this->endMinute = $endMinute;
+    $this->endSecond = $endSecond;
+    $this->repeat = $repeat;
+  }
+}
+?>
---
+++ web/uploadfile.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<?php	include 'common/menuHead.inc'; ?>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>File Uploads</title>
+<script type="text/javascript" src="js/fpp.js"></script>
+<script src="js/jquery-migrate-1.1.1.min.js"></script>
+<script src="js/jquery-1.9.1.js"></script>
+<script src="js/jquery-ui.js"></script>
+<script src="js/upload/jquery.js" type="text/javascript"></script>
+<script src="js/upload/ajaxupload.js" type="text/javascript"></script>
+<link rel="stylesheet" href="css/fpp.css" />
+<link rel="stylesheet" href="css/classicTheme/style.css" type="text/css" media="all" />
+<script>
+    $(function() {
+		$('#tblSequences').on('mousedown', 'tr', function(event,ui){
+					$('#tblSequences tr').removeClass('selectedentry');
+          $(this).addClass('selectedentry');
+					SequenceNameSelected  = $(this).find('td.seqName').text();
+					var i=0;
+
+		});
+
+		$('#tblMusic').on('mousedown', 'tr', function(event,ui){
+					$('#tblMusic tr').removeClass('selectedentry');
+          $(this).addClass('selectedentry');
+					SongNameSelected  = $(this).find('td.songName').text();
+					var i=0;
+
+		});
+
+	});
+</script>
+<style>
+#top {
+	min-width: 768px;
+	width: 768px;
+	margin-left: auto;
+	margin-right: auto;
+}
+#divSeq {
+	min-height: 300px;
+	max-height: 300px;
+	width: 383px;
+	float: left;
+}
+#divSeqData {
+	overflow: auto;
+	min-height: 220px;
+	max-height: 220px;
+	width: 100%;
+}
+#tblSequences {
+	width: 100%;
+}
+#tblMusic {
+	width: 100%;
+}
+.seqDetails {
+	font-size: 13px;
+}
+.songDetails {
+	font-size: 13px;
+}
+.seqName {
+	width: 65%;
+}
+.seqTime {
+	width: 35%;
+}
+.songTime {
+	width: 35%;
+}
+.songName {
+	width: 65%;
+}
+#divMusic {
+	overflow: auto;
+	display: table;
+	min-height: 300px;
+	max-height: 300px;
+	width: 383px;
+	float: right;
+}
+#divMusicData {
+	overflow: auto;
+	min-height: 220px;
+	max-height: 220px;
+	width: 100%;
+}
+#uploader_div {
+	display: block;
+	min-height: 300px;
+	width: 100%;
+	clear: both;
+	padding-top: 10px;
+}
+fieldset {
+	height: 100%;
+	min-height: 270px;
+	border: 2px solid #000000;
+}
+h2 {
+	text-align: center;
+}
+.right {
+	text-align: right;
+}
+.selectedentry {
+	background: #EEE;
+}
+</style>
+</head>
+
+<body onload="GetSequenceFiles(); GetMusicFiles();">
+<div id="bodyWrapper">
+<?php	include 'menu.inc'; ?>
+<div id="top">
+  <br />
+  <div id= "divSeq">
+    <fieldset class="fs">
+      <legend> Sequence Files </legend>
+      <div id="divSeqData">
+        <table id="tblSequences">
+        </table>
+      </div>
+      <hr />
+      <div class='right'>
+        <input onclick="DeleteSequence();" name="btnDeleteSequence" class="buttons" type="button"  value="Delete" />
+      </div>
+    </fieldset>
+  </div>
+  <div id= "divMusic">
+    <fieldset  class="fs">
+      <legend> Music Files </legend>
+      <div id="divMusicData">
+        <table id="tblMusic">
+        </table>
+      </div>
+      <hr />
+      <div class='right'>
+        <input onclick= "DeleteMusic();" name="btnDeleteMusic" class="buttons" type="button"  value="Delete" />
+      </div>
+    </fieldset>
+  </div>
+  <div id="uploader_div" > </div>
+  <script type="text/javascript">
+$('#uploader_div').ajaxupload({
+	url:'upload.php',
+	remotePath:'/home/pi/media/upload/',
+	removeOnSuccess: true,
+	maxFileSize:'1000M',
+	chunkSize:1048576,
+	success:	function(fileName){
+				moveFile(fileName);
+	},
+	finish: function()
+	{
+		GetSequenceFiles();
+		GetMusicFiles();
+	},
+	allowExt:['mp3','fseq']
+});
+</script> 
+</div>
+<?php	include 'common/footer.inc'; ?>
+</body>
+</div>
+</html>
