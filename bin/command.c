306e2643208042279e87620f70095a6b4174f35d
dpitts <dpitts@ese-web.com>
2013-08-10 21:54:03 -0600

    Delete notes

--- bin/command.c
+++ bin/command.c
@@ -54,7 +54,7 @@ extern int numberOfSecondsPaused;
    fcntl(socket_fd, F_SETFL, O_NONBLOCK);
    memset(&server_address, 0, sizeof(server_address));
    server_address.sun_family = AF_UNIX;
-   strcpy(server_address.sun_path, "/tmp/FPPD");
+   strcpy(server_address.sun_path, "/home/pi/bin/FPPD");
    unlink("/tmp/FPPD");
 
    if(bind(socket_fd, (const struct sockaddr *) &server_address, sizeof(server_address)) < 0)
