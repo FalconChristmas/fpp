5a5fb32a7f0464ea88f455461c48f502f672c8c4
Chris Pinkham <cpinkham@bc2va.org>
2013-10-31 13:28:15 -0700

    Fix version check in config v1 upgrade.sh script

--- upgrade/1/upgrade.sh
+++ upgrade/1/upgrade.sh
@@ -2,7 +2,7 @@
 ###############################################################################
 # Convert /etc/rc.local to use the new scripts in fpp/scripts
 
-if [ "x${FPPVERSION}" = "x10" ]
+if [ ${FPPVERSION} -eq 10 ]
 then
 	echo "Updating /etc/rc.local to enable use of new fpp scripts dir"
 	cp /etc/rc.local /etc/rc.local.orig
