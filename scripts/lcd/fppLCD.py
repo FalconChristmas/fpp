7a5364c9fb78f6b9563557e5381718bb5460b367
dpitts <dpitts@ese-web.com>
2014-01-17 19:48:20 -0700

    First attempt at lcd

--- scripts/lcd/fppLCD.py
+++ scripts/lcd/fppLCD.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python
+
+# Must do apt-get install python-daemon
 import socket
 import sys
 import os
 import time
+import signal
+import daemon
+import lockfile
+import subprocess
 
 from time import sleep
 from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
 
 class fppLCD():
 
-  def __init__(self):
+  def __init__(self,directory):
+    self.THIS_DIRECTORY = directory
+
+    self.BACK_COLOR = 0
     self.PLAYLIST_DIRECTORY = "/home/pi/media/playlists/"
+    self.SETTINGS_FILE = "/home/pi/media/settings"
+
     self.DIRECTION_LEFT = 0
     self.DIRECTION_RIGHT = 1
     self.MAX_CHARS = 16
@@ -61,16 +72,20 @@ class fppLCD():
     self.lcd.noCursor()
     self.lcd.clear()
     
+    self.sequenceCount=0;
+    self.sequences = ()
+    
     self.playlists = ()
+    self.selectedPlaylistIndex = 0
     self.MenuIndex=0;
     self.SubmenuIndex=0;
 
     self.MENU_INX_STATUS = 0
     self.MENU_INX_STOP_SEQUENCE = 1
-    self.MENU_INX_START_PLAYLIST = 2
-    self.MENU_INX__SHUTDOWN_REBOOT = 3
-
-    self.MENU_INX_BACKCOLOR = 10
+    self.MENU_INX_PLAYLISTS = 2
+    self.MENU_INX_PLAYLIST_ENTRIES = 3
+    self.MENU_INX__SHUTDOWN_REBOOT = 4
+    self.MENU_INX_BACKCOLOR = 5
 
 
     #Color
@@ -86,7 +101,12 @@ class fppLCD():
                  ("Violet         ",self.lcd.VIOLET),
                  ("White          ",self.lcd.WHITE))
     
-  
+  def Initialize(self):
+    self.BackgroundColor = int(self.readSetting("piLCD_BackColor"));
+    self.SelectColor(self.BackgroundColor);
+    print "Backcolor :" + str(self.BackgroundColor) + "\n"
+    return 
+    
   def SendCommand(self,command):
     count =0;
     max_timeout = 10;
@@ -124,7 +144,7 @@ class fppLCD():
   def StopFPPD(self):
     thisPath = os.path.dirname(os.path.realpath(__file__))
     fppdStopScript = "sudo " + os.path.dirname(thisPath) + "/fppd_stop"
-    print >>sys.stderr, fppdStopScript
+    #print >>sys.stderr, fppdStopScript
     os.system(fppdStopScript)
     return;
 
@@ -134,19 +154,59 @@ class fppLCD():
     return;
     
   def ProcessLeft(self):
-    #if previousBtn != lcd.LEFT:
+    self.SubmenuIndex = 0
     self.SetStatusTimeOut(self.MAX_STATUS_TIMEOUT);
+    if self.previousBtn == self.lcd.LEFT:
+      return
+    
+    if self.MenuIndex == self.MENU_INX_STATUS:
+      self.MenuIndex = self.MENU_INX_BACKCOLOR
+      self.SubmenuIndex = fpplcd.BACK_COLOR
+      self.DisplayMenuBackColor()
+    elif self.MenuIndex == self.MENU_INX__SHUTDOWN_REBOOT:
+      self.SetStatusTimeOut(self.NORMAL_STATUS_TIMEOUT);
+      self.MenuIndex = self.MENU_INX_STATUS
+      self.UpdateStatus()
+    elif self.MenuIndex == self.MENU_INX_STOP_SEQUENCE:
+      self.SetStatusTimeOut(self.NORMAL_STATUS_TIMEOUT);
+      self.MenuIndex = self.MENU_INX_STATUS
+      self.UpdateStatus()
+    elif self.MenuIndex == self.MENU_INX_BACKCOLOR:
+      self.MenuIndex = self.MENU_INX__SHUTDOWN_REBOOT
+      self.DisplayShutdownMenu()
+    elif self.MenuIndex == self.MENU_INX_PLAYLIST_ENTRIES:
+      self.MenuIndex = self.MENU_INX_PLAYLISTS
+      self.SubmenuIndex = self.selectedPlaylistIndex
+      self.DisplayPlaylists()
+   
     self.previousBtn = self.lcd.LEFT
     return;
   
   def ProcessRight(self):
+    self.SubmenuIndex = 0
     self.SetStatusTimeOut(self.MAX_STATUS_TIMEOUT);
     if self.previousBtn == self.lcd.RIGHT:
       return
     if self.MenuIndex ==self.MENU_INX_STATUS:
       self.MenuIndex = self.MENU_INX__SHUTDOWN_REBOOT
       self.DisplayShutdownMenu()
-    
+    elif self.MenuIndex == self.MENU_INX__SHUTDOWN_REBOOT:
+      self.MenuIndex = self.MENU_INX_BACKCOLOR
+      self.SubmenuIndex = fpplcd.BACK_COLOR
+      self.DisplayMenuBackColor()
+    elif self.MenuIndex == self.MENU_INX_BACKCOLOR:
+      self.SetStatusTimeOut(self.NORMAL_STATUS_TIMEOUT);
+      self.MenuIndex = self.MENU_INX_STATUS
+      self.UpdateStatus()
+    elif self.MenuIndex == self.MENU_INX_STOP_SEQUENCE:
+      self.SetStatusTimeOut(self.NORMAL_STATUS_TIMEOUT);
+      self.MenuIndex = self.MENU_INX_STATUS
+      self.UpdateStatus()
+    elif self.MenuIndex == self.MENU_INX_PLAYLIST_ENTRIES:
+      self.MenuIndex = self.MENU_INX_PLAYLISTS
+      self.SubmenuIndex = self.selectedPlaylistIndex
+      self.DisplayPlaylists()
+
     self.previousBtn = self.lcd.RIGHT
     return;
     
@@ -160,11 +220,20 @@ class fppLCD():
       self.DisplayStopSequence()
     elif self.MenuIndex == self.MENU_INX__SHUTDOWN_REBOOT:
       self.DisplayShutdownMenu()
-    elif self.MenuIndex == self.MENU_INX_START_PLAYLIST:
-      self.DisplayStartPlaylist()
-      
+    elif self.MenuIndex == self.MENU_INX_PLAYLISTS:
+      self.DisplayPlaylists()
+    elif self.MenuIndex == self.MENU_INX_BACKCOLOR:
+      self.DisplayMenuBackColor()
+    elif self.MenuIndex == self.MENU_INX_PLAYLIST_ENTRIES:
+      self.DisplayPlaylistEntries()
+    elif self.MenuIndex == self.MENU_INX_STATUS:
+      self.SetStatusTimeOut(self.NORMAL_STATUS_TIMEOUT);
+      self.UpdateStatus()
+     
     self.previousBtn = self.lcd.UP
     return;
+    
+    
   def ProcessDown(self):
     self.SetStatusTimeOut(self.MAX_STATUS_TIMEOUT);
     if self.previousBtn == self.lcd.DOWN:
@@ -175,9 +244,16 @@ class fppLCD():
       self.DisplayStopSequence()
     elif self.MenuIndex == self.MENU_INX__SHUTDOWN_REBOOT:
       self.DisplayShutdownMenu()
-    elif self.MenuIndex == self.MENU_INX_START_PLAYLIST:
-      self.DisplayStartPlaylist()
-      
+    elif self.MenuIndex == self.MENU_INX_PLAYLISTS:
+      self.DisplayPlaylists()
+    elif self.MenuIndex == self.MENU_INX_BACKCOLOR:
+      self.DisplayMenuBackColor()
+    elif self.MenuIndex == self.MENU_INX_STATUS:
+      self.SetStatusTimeOut(self.NORMAL_STATUS_TIMEOUT);
+      self.UpdateStatus()
+    elif self.MenuIndex == self.MENU_INX_PLAYLIST_ENTRIES:
+      self.DisplayPlaylistEntries()
+		
     self.previousBtn = self.lcd.DOWN
     return;
 
@@ -189,41 +265,25 @@ class fppLCD():
       if self.FPPDstatus == self.FPPD_STATUS_STOPPED:
         self.StartFPPD()
       elif self.FPPDplayerStatus == self.STATUS_IDLE:
-        self.MenuIndex = self.MENU_INX_START_PLAYLIST
+        self.MenuIndex = self.MENU_INX_PLAYLISTS
         self.SubmenuIndex = 0
-        self.DisplayStartPlaylist()
+        self.DisplayPlaylists()
       elif self.FPPDplayerStatus == self.STATUS_PLAYING:
         self.MenuIndex = self.MENU_INX_STOP_SEQUENCE
         self.SubmenuIndex = 0
         self.DisplayStopSequence()
     elif self.MenuIndex == self.MENU_INX_STOP_SEQUENCE:
       self.StopSequence()
-      self.UpdateStatus()
-    elif self.MenuIndex == self.MENU_INX_START_PLAYLIST:
+    elif self.MenuIndex == self.MENU_INX_PLAYLISTS:
+      self.MenuIndex = self.MENU_INX_PLAYLIST_ENTRIES
+      self.DisplayPlaylistEntries()
+    elif self.MenuIndex == self.MENU_INX_PLAYLIST_ENTRIES:
       self.StartPlaylist()
+    elif self.MenuIndex == self.MENU_INX_BACKCOLOR:
+      self.BackgroundColor = self.SubmenuIndex
+      self.SelectColor(1)
 
     self.previousBtn = self.lcd.SELECT
-    return;
-
-
-  def MenuBackColor(self):
-    self.SubmenuIndex = self.SubmenuIndex % self.MENU_COLOR_COUNT
-    self.lcd.clear()
-    if self.BackgroundColor == self.SubmenuIndex:
-      selected = "*"
-    else:
-      selected = ""
-      
-    self.line1 = "Background Color"
-    self.line2 =  selected + self.colors[self.SubmenuIndex][0]
-    
-    self.UpdateDisplay()
-    return;  
-    
-  def SelectColor(self):
-    self.lcd.backlight(self.colors[self.SubmenuIndex][1])
-    self.BackgroundColor = self.SubmenuIndex
-    self.MenuBackColor()
     return;
 
   def CheckButtons(self):
@@ -256,7 +316,7 @@ class fppLCD():
   def ResetRotatePosition(self,lineNumber):
     if lineNumber == 0:
       self.rotatePositon1 = self.fixedLocation1;
-      print "Resetting pos:" + str(self.fixedLocation1)
+      #print "Resetting pos:" + str(self.fixedLocation1)
     else:  
       self.rotatePositon2 = self.fixedLocation2;
     return;
@@ -321,7 +381,7 @@ class fppLCD():
     self.SetStatusTimeOut(self.NORMAL_STATUS_TIMEOUT)
     self.statusUpdateCounter = 0;
     result = self.SendCommand("s")
-    print result
+    #print result
     if result == "false" :
       self.FPPDstatus = self.FPPD_STATUS_STOPPED
       self.DisplayFPPDstopped()
@@ -410,17 +470,41 @@ class fppLCD():
     self.UpdateDisplay()
     return
  
-  def DisplayStartPlaylist(self):
-    self.line1 = self.MakeStringWithLength("Start Playlist",16,0)
+  def DisplayPlaylists(self):
+    self.line1 = self.MakeStringWithLength("Playlists",16,0)
     if self.SubmenuIndex == 0:
       self.playlists = self.GetPlaylists()
     
+    if len(self.playlists) == 0:
+      self.line2 = self.MakeStringWithLength("No Playlists!",16,0)
+      self.UpdateDisplay()
+      return
     if self.SubmenuIndex < 0:
       self.SubmenuIndex = len(self.playlists) - 1
 
     self.SubmenuIndex = self.SubmenuIndex%len(self.playlists)
+    self.line2 = self.MakeStringWithLength(str(self.SubmenuIndex+1) + ". " + self.playlists[self.SubmenuIndex],16,0)
+    self.selectedPlaylistIndex = self.SubmenuIndex
+    self.GetSequences()
+    self.UpdateDisplay()
+    return
     
-    self.line2 = self.MakeStringWithLength(str(self.SubmenuIndex+1) + ". " + str(self.playlists[self.SubmenuIndex]),16,0)
+  def DisplayPlaylistEntries(self):
+    #print self.playlists
+    #print self.selectedPlaylistIndex
+    self.line1 = self.MakeStringWithLength(self.playlists[self.selectedPlaylistIndex],16,0)
+    
+    if self.sequenceCount == 0:
+      self.line2 = self.MakeStringWithLength("No Entries!",16,0)
+      self.UpdateDisplay()
+      return
+    if self.SubmenuIndex < 0:
+      self.SubmenuIndex = self.sequenceCount - 1
+
+    #print "before SubmenuIndex = " + str(self.SubmenuIndex)
+    self.SubmenuIndex = self.SubmenuIndex%self.sequenceCount
+    #print "after SubmenuIndex = " + str(self.SubmenuIndex)
+    self.line2 = self.MakeStringWithLength(str(self.SubmenuIndex+1) + ". " + self.sequences[self.SubmenuIndex],16,0)
     self.UpdateDisplay()
     return
     
@@ -437,6 +521,44 @@ class fppLCD():
      
     self.UpdateDisplay()
     return
+
+  def DisplayMenuBackColor(self):
+    self.SubmenuIndex = self.SubmenuIndex % self.MENU_COLOR_COUNT
+    self.lcd.clear()
+    if self.BackgroundColor == self.SubmenuIndex:
+      selected = "*"
+    else:
+      selected = ""
+      
+    self.line1 = "Background Color"
+    self.line2 =  selected + self.colors[self.SubmenuIndex][0]
+    
+    self.UpdateDisplay()
+    return;  
+    
+  def SelectColor(self,updateSetting):
+    self.lcd.backlight(self.colors[self.BackgroundColor][1])
+    if updateSetting==1:
+      self.writeSetting("piLCD_BackColor",str(self.BackgroundColor))
+
+    self.DisplayMenuBackColor()
+    return;
+
+  def writeSetting(self,setting,value):
+    writeScript = os.path.dirname(self.THIS_DIRECTORY) + "/writeSetting.awk"
+    command = "awk -f " + writeScript + " " + self.SETTINGS_FILE + " setting=" + setting + " value=" + value
+    settingsText = subprocess.check_output(command, shell=True) 
+    file = open(self.SETTINGS_FILE, "w")
+    file.write(settingsText)
+    file.close();
+    return
+      
+  def readSetting(self,setting):
+    readScript = os.path.dirname(self.THIS_DIRECTORY) + "/readSetting.awk"
+    command = "awk -f " + readScript + " " + self.SETTINGS_FILE + " setting=" + setting 
+    setting = subprocess.check_output(command, shell=True) 
+    return setting
+
 
   def GetPlaylists(self):
     return os.listdir(self.PLAYLIST_DIRECTORY)
@@ -472,38 +594,79 @@ class fppLCD():
     return
     
   def StartPlaylist(self):
+    if self.FPPDplayerStatus == self.STATUS_PLAYING or self.FPPDplayerStatus == self.STATUS_STOPPING_GRACEFULLY:
+      self.StopSequence()
+      self.line1 = self.MakeStringWithLength("Starting",16,0)
+      self.line2 = self.MakeStringWithLength("",16,0)
+      self.UpdateDisplay()
+      sleep(2)
+      
     self.SetStatusTimeOut(self.NORMAL_STATUS_TIMEOUT)
-    if self.SubmenuIndex < len(self.playlists):
-      command = "p," + str(self.playlists[self.SubmenuIndex]) + ",0"
+    if self.SubmenuIndex < self.sequenceCount:
+      command = "p," + self.playlists[self.selectedPlaylistIndex] + "," + str(self.SubmenuIndex) + ","
       self.SendCommand(command)
     return
 
   def RebootPi(self):
     self.line1 = self.MakeStringWithLength("Rebooting Pi",16,1)
     self.line2 = self.MakeStringWithLength(" ",16,1)
-    self.UpdateLCD();
+    self.UpdateDisplay()
     os.system("sudo shutdown -r now")
     return
 
   def ShutdownPi(self):
     self.line1 = self.MakeStringWithLength("Shutting Down Pi",16,1)
     self.line2 = self.MakeStringWithLength(" ",16,1)
-    self.UpdateLCD();
+    self.UpdateDisplay()
     os.system("sudo shutdown -h now")
+    return
+
+
+  def GetSequences(self):
+    playListFile = self.PLAYLIST_DIRECTORY + self.playlists[self.selectedPlaylistIndex]
+    self.sequenceCount = 0
+    self.sequences = []
+    with open(playListFile) as seqs:
+      for seq in seqs:
+        arrSeq = seq.split(",")
+        if arrSeq[0]=="e":
+          text = self.MakeStringWithLength("Event - " + arrSeq[1],16,0)
+        elif arrSeq[0] == "p":
+          text = self.MakeStringWithLength("Pause - " + arrSeq[1] + "s",16,0)
+        else:
+          text = self.MakeStringWithLength(self.RemoveExtensionFromFilename(arrSeq[1]),16,0)
+        
+        if self.sequenceCount != 0:
+          self.sequences.append(text)
+        
+        self.sequenceCount = self.sequenceCount + 1
+
+    # Subtract one because first line in file is not playlist entry      
+    self.sequenceCount = self.sequenceCount - 1
     return
         
  # Clear display and show greeting, pause 1 sec
-sleep(1)
-fpplcd = fppLCD()
-while True:
-  fpplcd.CheckButtons()
-  fpplcd.CheckRotate()
-  sleep(.10)
-  fpplcd.statusUpdateCounter = fpplcd.statusUpdateCounter + 1
-  if fpplcd.statusUpdateCounter > fpplcd.maxStatusUpdateCount:
-     fpplcd.UpdateStatus()
-    
-  
+ 
+#context = daemon.DaemonContext(
+#    working_directory='/home/pi',
+#    umask=0o002,
+#    pidfile=lockfile.FileLock('/home/pi'),
+#)
 
+context = daemon.DaemonContext()
+THIS_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
-           
+context.open()
+with context:
+  sleep(1)
+  fpplcd = fppLCD(THIS_DIRECTORY)
+  fpplcd.Initialize()
+
+  while True:
+    fpplcd.CheckButtons()
+    fpplcd.CheckRotate()
+    sleep(.10)
+    fpplcd.statusUpdateCounter = fpplcd.statusUpdateCounter + 1
+    if fpplcd.statusUpdateCounter > fpplcd.maxStatusUpdateCount:
+      fpplcd.UpdateStatus()
+      
--- scripts/readSetting.awk
+++ scripts/readSetting.awk
@@ -2,21 +2,20 @@
 BEGIN { start = 0;
  
     if (ARGC < 3 || ARGC > 3) {
-        print "awk -f readInterfaces.awk <interfaces file> device=<eth device> [debug]"
-        exit 1;
+        print "awk -f readSetting.awk <settingName> value"
+        exit 0;
     }
     
     split(ARGV[2], arg, "=");
     if (arg[1] != "setting")
     {
-      print "awk -f readInterfaces.awk <interfaces file> device=<eth device> [debug]"
-      exit 1;
+      print "awk -f readSetting.awk <settingName> value"
+      exit 0;
     }
 
     file = ARGV[1];
     setting = arg[2];
     settingFound = 0;
-    
 }
  
 {
@@ -28,17 +27,14 @@ BEGIN { start = 0;
       gsub (/^ */, "", arrSetting[2]);
       gsub (/ *$/, "", arrSetting[2]);
       print(arrSetting[2]);
-      exit 1
+      exit 0
     }
 }
  
-
 END{
     if (settingFound != 1)
     { 
       print("Not Found");
     }
-    exit 1;
+    exit 0;
 }
-
-Modify message
