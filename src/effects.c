668e74830465652eba4e21c59b30862be3308195
Chris Pinkham <cpinkham@bc2va.org>
2014-04-30 12:10:04 -0700

    Add GPL License file and update sample copyright notice in effects.c.

--- src/effects.c
+++ src/effects.c
@@ -1,12 +1,18 @@
 /*
  *   Effects handler for Falcon Pi Player (FPP)
  *
- *   Copyright (C) Chris Pinkham 2013
+ *   Copyright (C) 2013 the Falcon Pi Player Developers
+ *      Initial development by:
+ *      - David Pitts (dpitts)
+ *      - Tony Mace (MyKroFt)
+ *      - Mathew Mrosko (Materdaddy)
+ *      - Chris Pinkham (CaptainMurdoch)
+ *      For additional credits and developers, see credits.php.
  *
- *   Falcon Pi Player is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
+ *   The Falcon Pi Player (FPP) is free software; you can redistribute it
+ *   and/or modify it under the terms of the GNU General Public License
+ *   as published by the Free Software Foundation; either version 2 of
+ *   the License, or (at your option) any later version.
  *
  *   This program is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
--- www/credits.php
+++ www/credits.php
@@ -30,7 +30,7 @@ unset($output);
 <script src="js/jquery-ui.js"></script>
 <link rel="stylesheet" type="text/css" href="css/jquery.timeentry.css">
 <link rel="stylesheet" href="css/fpp.css" />
-<title>About FPP</title>
+<title>Credits</title>
 <style>
 .pl_title {
   font-size: larger;
@@ -62,23 +62,23 @@ a:visited {
       <legend>Credits</legend>
       <div style="overflow: hidden; padding: 10px;">
     <div>
-      <div>
-        <table width="100%">
-          <tr><td><b><center>Credits</center></b></td></tr>
-        </table>
-      </div>
       <div id='credits'>
-        FPP Designed By:<br />
-        <br />
+        <b>FPP Developed By:</b><br />
+		<br />
         David Pitts (dpitts)<br />
         Tony Mace (MyKroFt)<br />
         Mathew Mrosko (Materdaddy)<br />
         Chris Pinkham (CaptainMurdoch)<br />
-        <br />
-        Video Tutorials by:<br />
+		<br />
+        <hr width=300 />
+		<br />
+        <b>Video Tutorials by:</b><br />
+		<br />
         Alan Dahl (bajadahl)<br />
-        <br />
-        Copyright 2013-2014
+		<br />
+        <hr width=180 />
+		<br />
+        Copyright &copy; 2013-2014
       </div>
     </div>
     </fieldset>
