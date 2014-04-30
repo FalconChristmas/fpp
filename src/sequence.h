eb425664df75b4159243f99a22e5a029cc1f2eb3
ihbar <ihbar@libertysurf.fr>
2014-05-01 00:25:28 +0200

    Add header to source file. License is FPP GPL V2

--- src/sequence.h
+++ src/sequence.h
@@ -1,3 +1,28 @@
+/*
+ *   Sequence handler for Falcon Pi Player (FPP)
+ *
+ *   Copyright (C) 2013 the Falcon Pi Player Developers
+ *      Initial development by:
+ *      - David Pitts (dpitts)
+ *      - Tony Mace (MyKroFt)
+ *      - Mathew Mrosko (Materdaddy)
+ *      - Chris Pinkham (CaptainMurdoch)
+ *      For additional credits and developers, see credits.php.
+ *
+ *   The Falcon Pi Player (FPP) is free software; you can redistribute it
+ *   and/or modify it under the terms of the GNU General Public License
+ *   as published by the Free Software Foundation; either version 2 of
+ *   the License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef _SEQUENCE_H
 #define _SEQUENCE_H
 
