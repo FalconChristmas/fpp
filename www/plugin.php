6c9bd235121748f8d0540bb998970dd86f159399
Chris Pinkham <cpinkham@bc2va.org>
2014-05-28 11:51:16 -0700

    Plugin helper updates.
    plugin.php can now be used to load .php, .js, and .css files:
    
    plugin.php?plugin=PLUGINNAME&page=somepage.php
    plugin.php?plugin=PLUGINNAME&page=somepage.php&nopage=1
    plugin.php?plugin=PLUGINNAME&file=somefile.js&nopage=1
    plugin.php?plugin=PLUGINNAME&file=somefile.css&nopage=1
    
    Subdirs are supported as well for these, so you can keep your
    js and css in their respective subdirs.
    
    plugin.php has also been modified to automatically insert
    tags to load any js/* and css/* files found in the plugin
    directory when it is run in 'page' mode (ie, no 'nopage').

--- www/plugin.php
+++ www/plugin.php
@@ -1,10 +1,52 @@
-<?php if ( !isset($_GET['nopage']) ): ?>
+<?php
+
+if ( !isset($_GET['nopage']) ):
+
+require_once("config.php");
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
 <?php include 'common/menuHead.inc'; ?>
 <script type="text/javascript" src="/js/fpp.js"></script>
 <title>Falcon PI Player - FPP</title>
+<?
+
+$jsDir = $pluginDirectory . "/" . $_GET['plugin'] . "/js/";
+if ( file_exists($jsDir))
+{
+	if ($handle = opendir($jsDir))
+	{
+		while (($file = readdir($handle)) !== false)
+		{
+			if (!in_array($file, array('.', '..')) && !is_dir($jsDir . $file))
+			{
+				printf( "<script type='text/javascript' src='/plugin.php?plugin=%s&file=js/%s&nopage=1'></script>\n",
+					$_GET['plugin'], $file);
+			}
+		}
+	}
+}
+
+$cssDir = $pluginDirectory . "/" . $_GET['plugin'] . "/css/";
+if ( file_exists($cssDir))
+{
+	if ($handle = opendir($cssDir))
+	{
+		while (($file = readdir($handle)) !== false)
+		{
+			if (!in_array($file, array('.', '..')) && !is_dir($cssDir . $file))
+			{
+				printf( "<link rel='stylesheet' type='text/css' href='/plugin.php?plugin=%s&file=css/%s&nopage=1'>\n",
+					$_GET['plugin'], $file);
+			}
+		}
+	}
+}
+
+?>
 </head>
 <body>
 <div id="bodyWrapper">
@@ -13,10 +55,9 @@
 
 <?php
 else:
+$skipJSsettings = 1;
 require_once("config.php");
 endif;
-
-if ( file_exists($pluginDirectory."/".$_GET['plugin']."/".$_GET['page']) )
 
 if ( !isset($_GET['plugin']) )
 {
@@ -36,6 +77,21 @@ elseif ( isset($_GET['page']) && !empty($_GET['page']) )
 	{
 		echo "Error with plugin, requesting a page that doesn't exist";
 	}
+}
+elseif ( isset($_GET['file']) && !empty($_GET['file']) )
+{
+	if (preg_match('/\.js$/', $_GET['file']))
+		header('Content-type: text/javascript;');
+	else if (preg_match('/\.css$/', $_GET['file']))
+		header('Content-type: text/css;');
+	else
+		header('Content-type: text/plain;');
+
+
+	$file = $pluginDirectory . "/" . $_GET['plugin'] . "/" . $_GET['file'];
+
+	if (file_exists($file))
+		readfile($file);
 }
 elseif ( file_exists($pluginDirectory."/".$_GET['plugin']."/plugin.php") )
 {
