d2cbb6fe349c1e079249676ec3346d5821507f00
Chris Pinkham <cpinkham@bc2va.org>
2014-06-24 21:59:21 -0700

    Playlist Editor fixes.
    - Fix 'Event' type creation.
    - Treat deprecated 'Video' type as 'Media' in the editor, which is
      what we do in fppd.  This rips out the rest of the Video type
      code

--- www/fppxml.php
+++ www/fppxml.php
@@ -1353,19 +1353,18 @@ function AddPlayListEntry()
 	$seqFile = $_GET['seqFile'];
 	$songFile = $_GET['mediaFile'];
 	$pause = $_GET['pause'];
-	$videoFile = $_GET['videoFile'];
 	$eventName = $_GET['eventName'];
 	$eventID = $_GET['eventID'];
 	check($type);
 	check($seqFile);
 	check($songFile);
 	check($pause);
-	check($videoFile);
 	check($eventName);
 	check($eventID);
+	$index = 0;
 
-	$_SESSION['playListEntries'][] = new PlaylistEntry($type,$songFile,$seqFile,$pause,$videoFile,$eventName,$eventID,$index,count($_SESSION['playListEntries']));
-	EchoStatusXML($_GET['songFile']);
+	$_SESSION['playListEntries'][] = new PlaylistEntry($type,$songFile,$seqFile,$pause,$eventName,$eventID,$index,count($_SESSION['playListEntries']));
+	EchoStatusXML($_GET['mediaFile']);
 }
 
 function GetPlaylists()
@@ -1598,6 +1597,8 @@ function LoadPlayListDetails($file)
 	{
 		$line=fgets($f);
 		$entry = explode(",",$line,50);
+		if ($entry[0] == 'v')
+			$entry[0] = 'm';
 		$type = $entry[0];
 		if(strlen($line)==0)
 			break;
@@ -1608,7 +1609,6 @@ function LoadPlayListDetails($file)
 				$songFile = $entry[2];
 				$pause = 0;
 				$index = $i;
-				$videoFile = "";
 				$eventName = "";
 				$eventID = "";
 				break;
@@ -1619,7 +1619,6 @@ function LoadPlayListDetails($file)
 				$seqFile = "";
 				$pause = 0;
 				$index = $i;
-				$videoFile = "";
 				$eventName = "";
 				$eventID = "";
 				break;
@@ -1628,7 +1627,6 @@ function LoadPlayListDetails($file)
 				$seqFile = $entry[1];
 				$pause = 0;
 				$index = $i;
-				$videoFile = "";
 				$eventName = "";
 				$eventID = "";
 				break;
@@ -1637,16 +1635,6 @@ function LoadPlayListDetails($file)
 				$seqFile = "";
 				$pause = $entry[1];
 				$index = $i;
-				$videoFile = "";
-				$eventName = "";
-				$eventID = "";
-				break;
-			case 'v':
-				$seqFile = "";
-				$songFile = "";
-				$pause = $entry[2];
-				$index = $i;
-				$videoFile = $entry[1];
 				$eventName = "";
 				$eventID = "";
 				break;
@@ -1655,7 +1643,6 @@ function LoadPlayListDetails($file)
 				$songFile = "";
 				$pause = 0;
 				$index = $i;
-				$videoFile = "";
 				$eventID = $entry[1];
 
 				$eventFile = $eventDirectory . "/" . $eventID . ".fevt";
@@ -1667,7 +1654,7 @@ function LoadPlayListDetails($file)
 				}
 				break;
 		}
-		$playListEntries[$i] = new PlaylistEntry($type,$songFile,$seqFile,$pause,$videoFile,$eventName,$eventID,$index);
+		$playListEntries[$i] = new PlaylistEntry($type,$songFile,$seqFile,$pause,$eventName,$eventID,$index);
 		$i++;
 	}
 	fclose($f);
@@ -1744,11 +1731,6 @@ function GetPlaylistEntries()
 		$index = $playListEntry->appendChild($index);
 		$value = $doc->createTextNode($_SESSION['playListEntries'][$i]->index);
 		$value = $index->appendChild($value);
-		// videoFile
-		$videoFile = $doc->createElement('videoFile');
-		$videoFile = $playListEntry->appendChild($videoFile);
-		$value = $doc->createTextNode($_SESSION['playListEntries'][$i]->videoFile);
-		$value = $videoFile->appendChild($value);
 		// eventName
 		$eventName = $doc->createElement('eventName');
 		$eventName = $playListEntry->appendChild($eventName);
@@ -1824,11 +1806,6 @@ function SavePlaylist()
 		else if($_SESSION['playListEntries'][$i]->type == 'p')
 		{
 			$entries .= sprintf("%s,%s,\n",$_SESSION['playListEntries'][$i]->type,$_SESSION['playListEntries'][$i]->pause);
-		}
-		else if($_SESSION['playListEntries'][$i]->type == 'v')
-		{
-			$entries .= sprintf("%s,%s,%d,\n",$_SESSION['playListEntries'][$i]->type,$_SESSION['playListEntries'][$i]->videoFile,
-								$_SESSION['playListEntries'][$i]->pause);
 		}
 		else if($_SESSION['playListEntries'][$i]->type == 'e')
 		{
