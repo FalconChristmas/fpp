eeed6bc085428a59a32ee3ce6d42e0665a86f4e0
Chris Pinkham <cpinkham@bc2va.org>
2014-10-08 12:56:50 -0700

    [Events] Lower priority of Event Script process after fork()
    Event Scripts were running with the same elevated priority as the
    rest of FPP which could cause FPP and audio playback to hickup
    when an Event Script was run.  Especially noticable when running
    a perl script in an Event Script.
    (cherry picked from commit 40982d9165698cfd99dccc777372c8f6c5b6ede4)

--- src/events.c
+++ src/events.c
@@ -24,6 +24,7 @@
  */
 
 #include <errno.h>
+#include <sched.h>
 #include <stdlib.h>
 #include <string.h>
 #include <strings.h>
@@ -239,6 +240,16 @@ int RunEventScript(FPPevent *e)
 	pid = fork();
 	if (pid == 0) // Event Script process
 	{
+#ifndef NOROOT
+		struct sched_param param;
+		param.sched_priority = 0;
+		if (sched_setscheduler(0, SCHED_OTHER, &param) != 0)
+		{
+			perror("sched_setscheduler");
+			exit(EXIT_FAILURE);
+		}
+#endif
+
 		char *args[128];
 		char *token = strtok(eventScript, " ");
 		int   i = 1;
