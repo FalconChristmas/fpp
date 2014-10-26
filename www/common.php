FalconChristmas/fpp

commit b1c6145e9afa314b58aba081bc0278e3e83e8fda

[Settings] Show warnings when restart or reboot is necessary.

www/common.php
@@ -1,19 +1,6 @@
 <?php 
 require_once("config.php");
 
-function check($var, $var_name = "", $function_name = "")
-{
-	if ( empty($function_name) )
-	{
-		global $args;
-		$function_name = $args['command'];
-	}
-
-	if ( empty($var) )
-		error_log("WARNING: Variable '$var_name' in function '$function_name' was empty");
-}
-
-
 function ReadSettingFromFile($settingName, $plugin = "")
 {
 	global $settingsFile;
@@ -85,7 +72,7 @@ function IfSettingEqualPrint($setting, $value, $print, $pluginName = "")
 	}
 }
 
-function PrintSettingCheckbox($title, $setting, $checkedValue, $uncheckedValue, $pluginName = "", $callbackName = "")
+function PrintSettingCheckbox($title, $setting, $restart = 1, $reboot = 0, $checkedValue, $uncheckedValue, $pluginName = "", $callbackName = "")
 {
 	global $settings;
 	global $pluginSettings;
@@ -118,7 +105,16 @@ function " . $setting . "Changed() {
 			else
 				$.jGrowl('$title Disabled');
 			$settingsName" . "['$setting'] = value;
+";
+
+if ($restart)
+	echo "SetRestartFlag();\n";
+if ($reboot)
+	echo "SetRebootFlag();\n";
+
+echo "
 			$callbackName
+			CheckRestartRebootFlags();
 		}).fail(function() {
 			if (checked) {
 				DialogError('$title', 'Failed to Enable $title');
@@ -138,7 +134,7 @@ function " . $setting . "Changed() {
 	echo " onChange='" . $setting . "Changed();'>\n";
 }
 
-function PrintSettingSelect($title, $setting, $defaultValue, $values, $pluginName = "", $callbackName = "")
+function PrintSettingSelect($title, $setting, $restart = 1, $reboot = 0, $defaultValue, $values, $pluginName = "", $callbackName = "")
 {
 	global $settings;
 	global $pluginSettings;
@@ -164,6 +160,14 @@ function " . $setting . "Changed() {
 			$.jGrowl('$title saved');
 			$settingsName" . "['$setting'] = value;
 			$callbackName
+";
+
+if ($restart)
+	echo "SetRestartFlag();\n";
+if ($reboot)
+	echo "SetRebootFlag();\n";
+
+echo "
 		}).fail(function() {
 			DialogError('$title', 'Failed to save $title');
 		});
@@ -187,7 +191,7 @@ function " . $setting . "Changed() {
 	echo "</select>\n";
 }
 
-function PrintSettingText($setting, $maxlength = 32, $size = 32, $pluginName = "")
+function PrintSettingText($setting, $restart = 1, $reboot = 0, $maxlength = 32, $size = 32, $pluginName = "")
 {
 	global $settings;
 	global $pluginSettings;
@@ -211,7 +215,7 @@ function PrintSettingText($setting, $maxlength = 32, $size = 32, $pluginName = "
 	echo "'>\n";
 }
 
-function PrintSettingSave($title, $setting, $pluginName = "", $callbackName = "")
+function PrintSettingSave($title, $setting, $restart = 1, $reboot = 0, $pluginName = "", $callbackName = "")
 {
 	global $settings;
 	global $pluginSettings;
@@ -237,6 +241,14 @@ function save" . $setting . "() {
 			$.jGrowl('$title saved');
 			$settingsName" . "['$setting'] = value;
 			$callbackName
+";
+
+if ($restart)
+	echo "SetRestartFlag();\n";
+if ($reboot)
+	echo "SetRebootFlag();\n";
+
+echo "
 		}).fail(function() {
 			DialogError('$title', 'Failed to save $title');
 			$('#$setting').prop('checked', false);

www/fppjson.php
@@ -22,6 +22,7 @@ $command_array = Array(
 	"applyDNSInfo"        => 'ApplyDNSInfo',
 	"getDNSInfo"          => 'GetDNSInfo',
 	"setDNSInfo"          => 'SetDNSInfo',
+	"getFPPDUptime"       => 'GetFPPDUptime',
 	"applyInterfaceInfo"  => 'ApplyInterfaceInfo',
 	"getInterfaceInfo"    => 'GetInterfaceInfo',
 	"setInterfaceInfo"    => 'SetInterfaceInfo',
@@ -68,6 +69,15 @@ function returnJSON($arr) {
 	exit(0);
 }
 
+function check($var)
+{
+	if ( empty($var) || !isset($var) )
+	{
+		error_log("WARNING: Variable we checked in function '".$_GET['command']."' was empty");
+//		die();
+	}
+}
+
 /////////////////////////////////////////////////////////////////////////////
 
 function GetSetting()
@@ -75,7 +85,7 @@ function GetSetting()
 	global $args;
 
 	$setting = $args['key'];
-	check($setting, "setting", __FUNCTION__);
+	check($setting);
 
 	$value = ReadSettingFromFile($setting);
 
@@ -92,8 +102,8 @@ function SetSetting()
 	$setting = $args['key'];
 	$value   = $args['value'];
 
-	check($setting, "setting", __FUNCTION__);
-	check($value, "value", __FUNCTION__);
+	check($setting);
+	check($value);
 
 	WriteSettingToFile($setting, $value);
 
@@ -113,6 +123,32 @@ function SetSetting()
 	}
 
 	GetSetting();
+}
+
+function GetFPPDUptime()
+{
+	$result = Array();
+
+	$uptimeStr = SendCommand("GetFPPDUptime");
+	$uptime = explode(',', $uptimeStr)[3];
+
+	$days = $uptime / 86400;
+	$hours = $uptime % 86400 / 3600;
+	$seconds = $uptime % 86400 % 3600;
+	$minutes = $seconds / 60;
+	$seconds = $seconds % 60;
+
+	$result['uptime'] = $uptime;
+	$result['days'] = $days;
+	$result['hours'] = $hours;
+	$result['minutes'] = $minutes;
+	$result['seconds'] = $seconds;
+
+	$result['uptimeStr'] = 
+		sprintf($f, "%d days, %d hours, %d minutes, %d seconds\n",
+		$days, $hours, $minutes, $seconds );
+
+	returnJSON($result);
 }
 
 function ToggleSequencePause()
@@ -136,8 +172,8 @@ function GetPluginSetting()
 
 	$setting = $args['key'];
 	$plugin  = $args['plugin'];
-	check($setting, "setting", __FUNCTION__);
-	check($plugin, "plugin", __FUNCTION__);
+	check($setting);
+	check($plugin);
 
 	$value = ReadSettingFromFile($setting, $plugin);
 
@@ -155,9 +191,9 @@ function SetPluginSetting()
 	$value   = $args['value'];
 	$plugin  = $args['plugin'];
 
-	check($setting, "setting", __FUNCTION__);
-	check($value, "value", __FUNCTION__);
-	check($plugin, "plugin", __FUNCTION__);
+	check($setting);
+	check($value);
+	check($plugin);
 
 	WriteSettingToFile($setting, $value, $plugin);
 
@@ -454,7 +490,7 @@ function GetInterfaceInfo()
 
 	$interface = $args['interface'];
 
-	check($interface, "interface", __FUNCTION__);
+	check($interface);
 
 	$result = Array();
 

www/settings.php
@@ -120,13 +120,6 @@ function AudioOutputChanged()
 		+ $('#AudioOutput').val()).fail(function() { alert("Failed to change audio output!") });
 }
 
-function ToggleLCDNow()
-{
-	var enabled = $('#PI_LCD_Enabled').is(":checked");
-	$.get("fppxml.php?command=setPiLCDenabled&enabled="
-		+ enabled).fail(function() { alert("Failed to change audio output!") });
-}
-
 </script>
 <title><? echo $pageTitle; ?></title>
 </head>
@@ -141,19 +134,19 @@ function ToggleLCDNow()
   <table table width = "100%">
     <tr>
       <td width = "35%">Blank screen on startup:</td>
-      <td width = "65%"><? PrintSettingCheckbox("Screensaver", "screensaver", "1", "0"); ?></td>
+      <td width = "65%"><? PrintSettingCheckbox("Screensaver", "screensaver", 0, 1, "1", "0"); ?></td>
     </tr>
     <tr>
       <td>Force Analog Audio Output:</td>
-      <td><? PrintSettingCheckbox("Force Analog Audio Output", "forceLocalAudio", "1", "0"); ?></td>
+      <td><? PrintSettingCheckbox("Force Analog Audio Output", "forceLocalAudio", 0, 0, "1", "0"); ?></td>
     </tr>
     <tr>
       <td>Pi 2x16 LCD Enabled:</td>
-      <td><? PrintSettingCheckbox("Enable LCD Display", "PI_LCD_Enabled", "enabled", "disabled", "", "ToggleLCDNow"); ?></td>
+      <td><? PrintSettingCheckbox("Enable LCD Display", "PI_LCD_Enabled", 0, 1, "1", "0"); ?></td>
     </tr>
     <tr>
       <td>Always transmit channel data:</td>
-      <td><? PrintSettingCheckbox("Always Transmit", "alwaysTransmit", "1", "0"); ?></td>
+      <td><? PrintSettingCheckbox("Always Transmit", "alwaysTransmit", 1, 0, "1", "0"); ?></td>
     </tr>
     <tr>
       <td>Audio Output Device:</td>

www/advancedsettings.php
@@ -13,7 +13,7 @@
 	<fieldset>
 	<legend>FPP Advanced Settings</legend>
 	<table table width = "100%">
-		<tr><td valign='top'><? PrintSettingSelect("E1.31 Bridging Transmit Interval", "E131BridgingInterval", "50", Array('25ms' => '25', '40ms' => '40', '50ms' => '50', '100ms' => '100')); ?></td>
+		<tr><td valign='top'><? PrintSettingSelect("E1.31 Bridging Transmit Interval", "E131BridgingInterval", 1, 0,"50", Array('25ms' => '25', '40ms' => '40', '50ms' => '50', '100ms' => '100')); ?></td>
 			<td valign='top'><b>E1.31 Bridge Mode Transmit Interval</b> - The
 				default Transmit Interval in E1.31 Bridge Mode is 50ms.  This
 				setting allows changing this to match the rate the player is
@@ -21,7 +21,7 @@
 				output devices such as the FPD do not support rates other than 50ms.</td>
 		</tr>
 		<tr><td colspan='2'><hr></td></tr>
-		<tr><td valign='top'><? PrintSettingCheckbox("E1.31 to E1.31 Bridging", "E131Bridging", "1", "0"); ?></td>
+		<tr><td valign='top'><? PrintSettingCheckbox("E1.31 to E1.31 Bridging", "E131Bridging", 1, 0, "1", "0"); ?></td>
 			<td valign='top'><b>Enable E1.31 to E1.31 Bridging</b> - 
 				<font color='#ff0000'><b>WARNING</b></font> -
 				E1.31 to E1.31 bridging over wireless is not recommended for

www/channeloutputs.php
@@ -52,6 +52,7 @@ $(document).ready(function() {
 						success: function (response) {
 								getUniverses();
 								$.jGrowl("E1.31 Universes Saved");
+								SetRestartFlag();
 						}
 				}).fail( function() {
 					DialogError("Save E1.31 Universes", "Save Failed");
@@ -119,6 +120,7 @@ $(document).ready(function(){
 		}).success(function() {
 			getPixelnetDMXoutputs('TRUE');
 			$.jGrowl("FPD Config Saved");
+			SetRestartFlag();
 		}).fail(function() {
 			DialogError("Save FPD Config", "Save Failed");
 		});
@@ -845,8 +847,8 @@ function SetChannelOutputs() {
 
 	$.post("fppjson.php", postData).success(function(data) {
 		PopulateChannelOutputTable(data);
-	}).success(function() {
 		$.jGrowl("Channel Output Configuration Saved");
+		SetRestartFlag();
 	}).fail(function() {
 		DialogError("Save Channel Outputs", "Save Failed");
 	});
@@ -1062,7 +1064,7 @@ tr.rowUniverseDetails td
 <!-- --------------------------------------------------------------------- -->
 
   <div style="overflow: hidden; padding: 10px;">
-	<b>Enable E1.31 Output:</b> <? PrintSettingCheckbox("E1.31 Output", "E131Enabled", "1", "0"); ?><br><br>
+	<b>Enable E1.31 Output:</b> <? PrintSettingCheckbox("E1.31 Output", "E131Enabled", 1, 0, "1", "0"); ?><br><br>
 	E1.31 Interface: <select id="selE131interfaces" onChange="SetE131interface();"><? PopulateInterfaces(); ?></select>
 	<br><br>
 
@@ -1102,7 +1104,7 @@ tr.rowUniverseDetails td
 <!-- --------------------------------------------------------------------- -->
 
 							<div style="overflow: hidden; padding: 10px;">
-								<b>Enable FPD Output:</b> <? PrintSettingCheckbox("FPD Output", "FPDEnabled", "1", "0"); ?><br><br>
+								<b>Enable FPD Output:</b> <? PrintSettingCheckbox("FPD Output", "FPDEnabled", 1, 0, "1", "0"); ?><br><br>
 								<form id="frmPixelnetDMX">
 									<input name="command" id="command"  type="hidden" value="saveHardwareConfig" />
                   <input name='model' id="model" type='hidden' value='FPDv1' />

www/channelremaps.php
@@ -62,6 +62,7 @@ function SetChannelRemaps() {
 	$.post("fppjson.php", postData).success(function(data) {
 		$.jGrowl("Channel Remap Table saved");
 		PopulateChannelRemapTable(data);
+		SetRestartFlag();
 	}).fail(function() {
 		DialogError("Save Channel Remap Table", "Save Failed");
 	});

www/common/footer.inc
@@ -7,12 +7,9 @@
 <div id="footer">
 <br />
   <div id="rebootShutdown">
-    <table  width="100%">
-      <tr>
-        <td width="20%"><input name="btnReboot" onClick="RebootPi();" type="button" class = "buttons" value="Reboot Pi"></td>
-        <td id = "txtNextPlaylist" width = "80%"><input name="btnShutdown" type="button" onClick="ShutdownPi();" class = "buttons" value="Shutdown Pi"></td>
-      </tr>
-    </table>
+    <input name="btnReboot" onClick="RebootPi();" type="button" class = "buttons" value="Reboot Pi">
+    <input name="btnShutdown" type="button" onClick="ShutdownPi();" class = "buttons" value="Shutdown Pi">
+    <input name="btnRestartFPPD" type="button" onClick="RestartFPPD();" class = "buttons" value="Restart FPPD">
   </div>
 <br />
 <a href="http://www.falconchristmas.com">www.falconchristmas.com</a>

www/config.php
@@ -1,5 +1,13 @@
 <?php
 
+$a = session_id();
+
+if(empty($a))
+{
+	session_start();
+}
+$_SESSION['session_id'] = session_id();
+
 $SUDO = "sudo";
 $debug = false;
 $settingsFile = "/home/pi/media/settings";
@@ -274,6 +282,12 @@ $settings['emailenable'] = $emailenable;
 $settings['emailguser'] = $emailguser;
 $settings['emailfromtext'] = $emailfromtext;
 $settings['emailtoemail'] = $emailtoemail;
+
+if (!isset($settings['restartFlag']))
+	$settings['restartFlag'] = 0;
+
+if (!isset($settings['rebootFlag']))
+	$settings['rebootFlag'] = 0;
 
 putenv("SCRIPTDIR=$scriptDirectory");
 putenv("MEDIADIR=$mediaDirectory");

commit 2d017b36956d5a6f1488f51d5ab8817af2e2311e

[xml/json] Fix accidental code removal in b1c6145e9
b1c6145e9 accidentally removed some code from 9acad203 due to a failed
git pull.  This restores those pieces which were removed.

www/common.php
@@ -1,6 +1,18 @@
 <?php 
 require_once("config.php");
 
+function check($var, $var_name = "", $function_name = "")
+{
+	if ( empty($function_name) )
+	{
+		global $args;
+		$function_name = $args['command'];
+	}
+
+	if ( empty($var) )
+		error_log("WARNING: Variable '$var_name' in function '$function_name' was empty");
+}
+
 function ReadSettingFromFile($settingName, $plugin = "")
 {
 	global $settingsFile;

www/fppjson.php
@@ -69,15 +69,6 @@ function returnJSON($arr) {
 	exit(0);
 }
 
-function check($var)
-{
-	if ( empty($var) || !isset($var) )
-	{
-		error_log("WARNING: Variable we checked in function '".$_GET['command']."' was empty");
-//		die();
-	}
-}
-
 /////////////////////////////////////////////////////////////////////////////
 
 function GetSetting()
@@ -85,7 +76,7 @@ function GetSetting()
 	global $args;
 
 	$setting = $args['key'];
-	check($setting);
+	check($setting, "setting", __FUNCTION__);
 
 	$value = ReadSettingFromFile($setting);
 
@@ -102,8 +93,8 @@ function SetSetting()
 	$setting = $args['key'];
 	$value   = $args['value'];
 
-	check($setting);
-	check($value);
+	check($setting, "setting", __FUNCTION__);
+	check($value, "value", __FUNCTION__);
 
 	WriteSettingToFile($setting, $value);
 
@@ -172,8 +163,8 @@ function GetPluginSetting()
 
 	$setting = $args['key'];
 	$plugin  = $args['plugin'];
-	check($setting);
-	check($plugin);
+	check($setting, "setting", __FUNCTION__);
+	check($plugin, "plugin", __FUNCTION__);
 
 	$value = ReadSettingFromFile($setting, $plugin);
 
@@ -191,9 +182,9 @@ function SetPluginSetting()
 	$value   = $args['value'];
 	$plugin  = $args['plugin'];
 
-	check($setting);
-	check($value);
-	check($plugin);
+	check($setting, "setting", __FUNCTION__);
+	check($value, "value", __FUNCTION__);
+	check($plugin, "plugin", __FUNCTION__);
 
 	WriteSettingToFile($setting, $value, $plugin);
 

www/settings.php
@@ -120,6 +120,13 @@ function AudioOutputChanged()
 		+ $('#AudioOutput').val()).fail(function() { alert("Failed to change audio output!") });
 }
 
+function ToggleLCDNow()
+{
+	var enabled = $('#PI_LCD_Enabled').is(":checked");
+	$.get("fppxml.php?command=setPiLCDenabled&enabled="
+		+ enabled).fail(function() { alert("Failed to change audio output!") });
+}
+
 </script>
 <title><? echo $pageTitle; ?></title>
 </head>