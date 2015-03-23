3fd1c6b3f843c90fd4598bff1b1b239c32461d16
Chris Pinkham <cpinkham@bc2va.org>
2015-03-23 15:00:46 -0700

    [ChannelOutput] Add FBMatrix Channel Output, UI to come shortly.

---
+++ src/channeloutput/FBMatrix.h
@@ -0,0 +1,64 @@
+/*
+ *   FrameBuffer Virtual matrix handler for Falcon Player (FPP)
+ *
+ *   Copyright (C) 2015 the Falcon Player Developers
+ *      Initial development by:
+ *      - David Pitts (dpitts)
+ *      - Tony Mace (MyKroFt)
+ *      - Mathew Mrosko (Materdaddy)
+ *      - Chris Pinkham (CaptainMurdoch)
+ *      For additional credits and developers, see credits.php.
+ *
+ *   The Falcon Player (FPP) is free software; you can redistribute it
+ *   and/or modify it under the terms of the GNU General Public License
+ *   as published by the Free Software Foundation; either version 2 of
+ *   the License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _FBMATRIX_H
+#define _FBMATRIX_H
+
+#include <linux/fb.h>
+#include <string>
+
+using namespace std;
+
+#include "ChannelOutputBase.h"
+
+class FBMatrixOutput : public ChannelOutputBase {
+  public:
+	FBMatrixOutput(unsigned int startChannel, unsigned int channelCount);
+	~FBMatrixOutput();
+
+	int Init(char *configStr);
+	int Close(void);
+
+	int RawSendData(unsigned char *channelData);
+
+	void DumpConfig(void);
+
+  private:
+	int     m_fbFd;
+	int     m_ttyFd;
+
+	string  m_layout;
+	int     m_width;
+	int     m_height;
+
+	char   *m_fbp;
+	int     m_screenSize;
+
+	struct fb_var_screeninfo m_vInfo;
+	struct fb_var_screeninfo m_vInfoOrig;
+	struct fb_fix_screeninfo m_fInfo;
+};
+
+#endif /* _FBMATRIX_H */
