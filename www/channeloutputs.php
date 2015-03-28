71d15e278981d6df5df1f12a708cccd7f08d3a86
Chris Pinkham <cpinkham@bc2va.org>
2015-03-27 20:23:42 -0700

    [ChannelOutput] Allow ttyAMA* so Pi's onboard serial can be used

--- www/channeloutputs.php
+++ www/channeloutputs.php
@@ -636,6 +636,7 @@ var SerialDevices = new Array();
 	{
 		if ((preg_match("/^ttyS[0-9]+/", $fileName)) ||
 			(preg_match("/^ttyACM[0-9]+/", $fileName)) ||
+			(preg_match("/^ttyAMA[0-9]+/", $fileName)) ||
 			(preg_match("/^ttyUSB[0-9]+/", $fileName))) {
 			echo "SerialDevices['$fileName'] = '$fileName';\n";
 		}
