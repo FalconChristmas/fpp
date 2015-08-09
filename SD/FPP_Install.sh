576992ea48e1cbd5d909de546dcc14b319ac59d6
Chris Pinkham <cpinkham@bc2va.org>
2015-08-08 21:41:22 -0700

    [Install] Add OLA Packages during FPP_Install and via an upgrade script.
    This should allow us to turn back on OLA support.  This will be done
    in another commit.

--- SD/FPP_Install.sh
+++ SD/FPP_Install.sh
@@ -20,11 +20,54 @@
 SCRIPTVER="0.5"
 FPPBRANCH="master"
 FPPIMAGEVER="1.5"
-FPPCFGVER="7"
+FPPCFGVER="8"
 FPPPLATFORM="UNKNOWN"
 FPPDIR="/opt/fpp"
 OSVER="UNKNOWN"
-STARTTIME=$(date)
+
+#############################################################################
+# Some Helper Functions
+#############################################################################
+# Check local time against U.S. Naval Observatory time, set if too far out.
+# This function was copied from /opt/fpp/scripts/functions in FPP source
+# since we don't have access to the source until we clone the repo.
+checkTimeAgainstUSNO () {
+	# www.usno.navy.mil is not pingable, so check github.com instead
+	ping -q -c 1 github.com > /dev/null 2>&1
+
+	if [ $? -eq 0 ]
+	then
+		echo "FPP: Checking local time against U.S. Naval Observatory"
+
+		# allow clocks to differ by 24 hours to handle time zone differences
+		THRESHOLD=86400
+		USNOSECS=$(curl -s http://www.usno.navy.mil/cgi-bin/time.pl | sed -e "s/.*\">//" -e "s/<\/t.*//" -e "s/...$//")
+		LOCALSECS=$(date +%s)
+		MINALLOW=$(expr ${USNOSECS} - ${THRESHOLD})
+		MAXALLOW=$(expr ${USNOSECS} + ${THRESHOLD})
+
+		#echo "FPP: USNO Secs  : ${USNOSECS}"
+		#echo "FPP: Local Secs : ${LOCALSECS}"
+		#echo "FPP: Min Valid  : ${MINALLOW}"
+		#echo "FPP: Max Valid  : ${MAXALLOW}"
+
+		echo "FPP: USNO Time  : $(date --date=@${USNOSECS})"
+		echo "FPP: Local Time : $(date --date=@${LOCALSECS})"
+
+		if [ ${LOCALSECS} -gt ${MAXALLOW} -o ${LOCALSECS} -lt ${MINALLOW} ]
+		then
+			echo "FPP: Local Time is not within 24 hours of USNO time, setting to USNO time"
+			date $(date --date="@${USNOSECS}" +%m%d%H%M%Y.%S)
+
+			LOCALSECS=$(date +%s)
+			echo "FPP: New Local Time: $(date --date=@${LOCALSECS})"
+		else
+			echo "FPP: Local Time is OK"
+		fi
+	else
+		echo "FPP: Not online, unable to check time against U.S. Naval Observatory."
+	fi
+}
 
 #############################################################################
 # Gather some info about our system
@@ -60,6 +103,8 @@ else
 	FPPPLATFORM="UNKNOWN"
 fi
 
+checkTimeAgainstUSNO
+
 #############################################################################
 echo "============================================================"
 echo "$0 v${SCRIPTVER}"
@@ -94,6 +139,17 @@ then
 	echo
 	exit
 fi
+
+STARTTIME=$(date)
+
+#######################################
+# Log output and notify user
+echo "ALL output will be copied to FPP_Install.log"
+exec > >(tee -a FPP_Install.log)
+exec 2>&1
+echo "========================================================================"
+echo "FPP_Install.sh started at ${STARTTIME}"
+echo "------------------------------------------------------------------------"
 
 #######################################
 # Remove old /etc/fpp if it exists
@@ -176,9 +232,12 @@ case "${OSVER}" in
 		# Do a clean in between each iteration
 		apt-get -y install alsa-base alsa-utils apache2 apache2.2-bin apache2.2-common apache2-mpm-prefork apache2-utils arping avahi-daemon avahi-discover avahi-utils bash-completion bc build-essential bzip2 ca-certificates ccache curl device-tree-compiler dh-autoreconf
 		apt-get -y clean
-		apt-get -y install ethtool fbi fbset file flite 'g++-4.7' gcc-4.7 gdb git i2c-tools ifplugd imagemagick less libapache2-mod-php5 libboost-dev libconvert-binary-c-perl libdbus-glib-1-dev libdevice-serialport-perl libjson-perl libjsoncpp-dev
+		apt-get -y install ethtool fbi fbset file flite 'g++-4.7' gcc-4.7 gdb gdebi-core git i2c-tools ifplugd imagemagick less libapache2-mod-php5 libboost-dev libconvert-binary-c-perl libdbus-glib-1-dev libdevice-serialport-perl libjs-jquery libjs-jquery-ui libjson-perl libjsoncpp-dev
 		apt-get -y clean
 		apt-get -y install libnet-bonjour-perl libpam-smbpass libtagc0-dev locales mp3info mpg123 mpg321 mplayer nano nginx node ntp perlmagick php5 php5-cli php5-common php5-curl php5-fpm php5-mcrypt php5-sqlite php-apc python-daemon python-smbus samba samba-common-bin shellinabox sudo sysstat usbmount vim vim-common vorbis-tools vsftpd
+		apt-get -y clean
+		# packages for building and running OLA
+		apt-get -y --force-yes install libcppunit-dev libcppunit-1.12-1 uuid-dev pkg-config libncurses5-dev libtool autoconf automake libmicrohttpd-dev protobuf-compiler python-protobuf libprotobuf-dev libprotoc-dev zlib1g-dev bison flex libftdi-dev libftdi1 libusb-1.0-0-dev liblo-dev
 		apt-get -y clean
 
 		echo "FPP - Installing wireless firmware packages"
@@ -237,7 +296,8 @@ case "${OSVER}" in
 		echo "FPP - Installing required packages"
 		if [ "x${FPPPLATFORM}" = "xODROID" ]
 		then
-			apt-get -y install apache2 apache2-bin apache2-mpm-prefork apache2-utils avahi-discover fbi flite i2c-tools imagemagick libapache2-mod-php5 libboost-dev libconvert-binary-c-perl libjson-perl libjsoncpp-dev libnet-bonjour-perl libpam-smbpass mp3info mpg123 perlmagick php5 php5-cli php5-common php-apc python-daemon python-smbus samba samba-common-bin shellinabox sysstat vorbis-tools vsftpd
+			echo "FPP - WARNING: This list may be incomplete, it needs to be updated to match the Pi/BBB Debian package install list"
+			apt-get -y install apache2 apache2-bin apache2-mpm-prefork apache2-utils avahi-discover fbi flite gdebi-core i2c-tools imagemagick libapache2-mod-php5 libboost-dev libconvert-binary-c-perl libjson-perl libjsoncpp-dev libnet-bonjour-perl libpam-smbpass mp3info mpg123 perlmagick php5 php5-cli php5-common php-apc python-daemon python-smbus samba samba-common-bin shellinabox sysstat vorbis-tools vsftpd
 		fi
 		;;
 esac
@@ -252,7 +312,16 @@ case "${FPPPLATFORM}" in
 		echo "cape_disable=capemgr.disable_partno=BB-BONELT-HDMI,BB-BONELT-HDMIN" >> /boot/uboot/uEnv.txt
 		echo >> /boot/uboot/uEnv.txt
 
-		echo "FPP - Installing OLA"
+		echo "FPP - Installing OLA packages"
+		mkdir /tmp/deb
+		cd /tmp/deb
+		FILES="libola-dev_0.9.7-1_armhf.deb libola1_0.9.7-1_armhf.deb ola-python_0.9.7-1_all.deb ola-rdm-tests_0.9.7-1_all.deb ola_0.9.7-1_armhf.deb"
+		for FILE in ${FILES}
+		do
+			wget -nd http://www.bc2va.org/chris/tmp/fpp/deb/${FILE}
+		done
+		dpkg --unpack ${FILES}
+		rm -f ${FILES}
 #		apt-get -y --force-yes install libcppunit-dev libcppunit-1.12-1 uuid-dev pkg-config libncurses5-dev libtool autoconf automake  libmicrohttpd-dev protobuf-compiler python-protobuf libprotobuf-dev libprotoc-dev zlib1g-dev bison flex libftdi-dev libftdi1 libusb-1.0-0-dev liblo-dev
 #		git clone https://github.com/OpenLightingProject/ola.git /opt/ola
 #		(cd /opt/ola && autoreconf -i && ./configure --enable-python-libs && make && make install && ldconfig && cd /opt/ && rm -rf ola)
