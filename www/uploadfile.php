cad1fa325a1a5d89a0ea4fa82432682f736168d5
Chris Pinkham <cpinkham@bc2va.org>
2015-11-05 15:25:12 -0800

    [UI] Update note about .fseq sources in File Manager

--- www/uploadfile.php
+++ www/uploadfile.php
@@ -164,7 +164,7 @@ h2 {
             <input onclick="DeleteFile('Sequences', SequenceNameSelected);" id="btnDeleteSequence" class="disableButtons" type="button"  value="Delete" />
           </div>
           <br />
-          <font size=-1>Sequence files must be in the Falcon Player .fseq format and may be converted from various other sequencer formats using <a href='https://github.com/smeighan/xLights' target='_sequencer'>xLights</a> or <a href='https://github.com/pharhp/Light-Elf' target='_sequencer'>Light-Elf</a>.  The <a href='http://www.vixenlights.com' target='_sequencer'>Vixen 3</a> sequencer has the ability to directly export .fseq files.</font>
+          <font size=-1>Sequence files must be in the Falcon Player .fseq format and may be converted from various other sequencer formats using <a href='https://github.com/smeighan/xLights' target='_sequencer'>xLights</a> or <a href='https://github.com/pharhp/Light-Elf' target='_sequencer'>Light-Elf</a>.  <a href='https://github.com/smeighan/xLights' target='_sequencer'>xLights v4</a> uses .fseq as its native file format.  <a href='http://www.vixenlights.com' target='_sequencer'>Vixen 3</a> and recent versions of <a href='http://vixenplus.com/'>Vixen+</a> also have the ability to directly export .fseq files.</font>
         </fieldset>
       </div>
     </div>
