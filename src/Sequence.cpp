a701291e5b2dc991da6a443c54947c57e3f313c7
Chris Pinkham <cpinkham@bc2va.org>
2015-12-04 11:31:12 -0800

    [Core] Don't clear channel data in ReadSequenceData() if no sequence is open
    This was causing Remotes to send blanking data between sequences.  We
    already handle sending blanking data in other places and the master sends
    out a sync blanking data sync packet when the remote needs to blank.
    (cherry picked from commit 121efb68b38bb5d8e741545d437280197c82fc28)

--- src/Sequence.cpp
+++ src/Sequence.cpp
@@ -385,10 +385,6 @@ void Sequence::ReadSequenceData(void) {
 		m_seqSecondsElapsed = (int)((float)(m_seqFilePosition - m_seqChanDataOffset)/((float)m_seqStepSize*(float)m_seqRefreshRate));
 		m_seqSecondsRemaining = m_seqDuration - m_seqSecondsElapsed;
 	}
-	else if ( getFPPmode() != BRIDGE_MODE )
-	{
-		BlankSequenceData();
-	}
 }
 
 void Sequence::ProcessSequenceData(void) {
@@ -429,7 +425,7 @@ void Sequence::SendBlankingData(void) {
 	if (getFPPmode() == MASTER_MODE)
 		SendBlankingDataPacket();
 
-	ReadSequenceData();
+	BlankSequenceData();
 	SendSequenceData();
 }
 
