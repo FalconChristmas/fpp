1a2207a103c50388fb7b8a734699d8438a11b5fe
T. Debo <tim@rawcreativestudios.com>
2016-05-24 21:27:49 -0400

    Extract api routes to their own file

--- www/app/Providers/RouteServiceProvider.php
+++ www/app/Providers/RouteServiceProvider.php
@@ -37,8 +37,45 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function map(Router $router)
     {
-        $router->group(['namespace' => $this->namespace], function ($router) {
+       $this->mapApiRoutes($router);
+
+       $router->group(['namespace' => $this->namespace], function ($router) {
+            require app_path('Http/routes.php');
+        });
+
+
+    }
+
+     /**
+     * Define the "web" routes for the application.
+     *
+     * These routes all receive session state, CSRF protection, etc.
+     *
+     * @param  \Illuminate\Routing\Router  $router
+     * @return void
+     */
+    protected function mapWebRoutes(Router $router)
+    {
+        $router->group([
+            'namespace' => $this->namespace, 'middleware' => ['web'],
+        ], function ($router) {
             require app_path('Http/routes.php');
         });
     }
+
+    /**
+     * Define the "api" routes for the application.
+     *
+     * @param  \Illuminate\Routing\Router  $router
+     * @return void
+     */
+    protected function mapApiRoutes(Router $router)
+    {
+        $router->group([
+            'namespace' => $this->namespace,
+        ], function ($router) {
+            require app_path('Http/api.php');
+        });
+    }
+
 }
