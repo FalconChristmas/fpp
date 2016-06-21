523d3cf9592d42369764d51d7fcad5dcfb66e35c
Chris Pinkham <cpinkham@bc2va.org>
2016-06-21 15:07:17 -0700

    [ChannelOutput] Add ability to toggle subMatrices on/off

--- src/channeloutput/RGBMatrix.cpp
+++ src/channeloutput/RGBMatrix.cpp
@@ -168,6 +168,7 @@ int RGBMatrixOutput::Init(Json::Value config)
 			Json::Value sm = config["subMatrices"][i];
 
 			m_matrix->AddSubMatrix(
+				sm["enabled"].asInt(),
 				sm["startChannel"].asInt() - 1,
 				sm["width"].asInt(),
 				sm["height"].asInt(),
