9fa4c8ef02bd2d92f81fa119d9c86964a20e2090
Chris Pinkham <cpinkham@bc2va.org>
2016-10-05 21:54:49 +0000

    [ChannelOutput] Fix USBRelay output by converting CSV to JSON config.

--- src/channeloutput/channeloutput.c
+++ src/channeloutput/channeloutput.c
@@ -100,6 +100,34 @@ void PrintRemappedChannels(void);
 
 /////////////////////////////////////////////////////////////////////////////
 
+Json::Value ChannelOutputCSV2JSON(char *deviceConfig)
+{
+	Json::Value result;
+
+	char *s = strtok(deviceConfig, ";");
+
+	while (s)
+	{
+		char tmp[128];
+		char *div = NULL;
+
+		strcpy(tmp, s);
+		div = strchr(tmp, '=');
+
+		if (div)
+		{
+			*div = '\0';
+			div++;
+
+			result[tmp] = div;
+		}
+
+		s = strtok(NULL, ";");
+	}
+
+	return result;
+}
+
 /*
  *
  */
@@ -269,11 +297,13 @@ int InitializeChannelOutputs(void) {
 
 		while(fgets(buf, 2048, fp) != NULL)
 		{
+			Json::Value jsonConfig;
+			int  useJSON = 0;
 			int  enabled = 0;
 			char type[32];
 			int  start = 0;
 			int  count = 0;
-			char deviceConfig[160];
+			char deviceConfig[256];
 
 			if (buf[0] == '#') // Allow # comments for testing
 				continue;
@@ -317,7 +347,6 @@ int InitializeChannelOutputs(void) {
 			channelOutputs[i].startChannel = start;
 			channelOutputs[i].channelCount = count;
 
-
 			if ((!strcmp(type, "Pixelnet-Lynx")) ||
 				(!strcmp(type, "Pixelnet-Open")))
 			{
@@ -350,10 +379,18 @@ int InitializeChannelOutputs(void) {
 				channelOutputs[i].output = new GPIO595Output(start, count);
 			} else if (!strcmp(type, "Debug")) {
 				channelOutputs[i].output = new DebugOutput(start, count);
+			} else if (!strcmp(type, "USBRelay")) {
+				channelOutputs[i].output = new USBRelayOutput(start, count);
+				useJSON = 1;
+				jsonConfig = ChannelOutputCSV2JSON(deviceConfig);
 			} else {
 				LogErr(VB_CHANNELOUT, "Unknown Channel Output type: %s\n", type);
 				continue;
 			}
+
+			jsonConfig["type"] = type;
+			jsonConfig["startChannel"] = start;
+			jsonConfig["channelCount"] = count;
 
 			if ((channelOutputs[i].outputOld) &&
 				(channelOutputs[i].outputOld->open(deviceConfig, &channelOutputs[i].privData)))
@@ -370,7 +407,8 @@ int InitializeChannelOutputs(void) {
 				}
 				i++;
 			} else if ((channelOutputs[i].output) &&
-					   (channelOutputs[i].output->Init(deviceConfig))) {
+					   (((useJSON) && (channelOutputs[i].output->Init(jsonConfig))) ||
+						((!useJSON) && (channelOutputs[i].output->Init(deviceConfig))))) {
 				i++;
 			} else {
 				LogErr(VB_CHANNELOUT, "ERROR Opening %s Channel Output\n", type);
