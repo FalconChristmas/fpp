110deb7759ecf05404d55b730935f4f0f3c176ae
Daniel Kulp <dan@kulp.com>
2018-02-14 14:28:20 +0000

    Add "grow SD" option for RaspPi

--- www/growsd.php
+++ www/growsd.php
@@ -2,7 +2,6 @@
 
 $skipJSsettings = 1;
 require_once("common.php");
-
 DisableOutputBuffering();
 
 
@@ -18,10 +17,12 @@ Grow SD Card Filesystem
 <h2>Grow SD Card Filesystem</h2>
 <pre>
 <?php
-    
-		echo "==================================================================================\n";
 
-		$command = "sudo /opt/scripts/tools/grow_partition.sh 2>&1";
+if ($settings['Platform'] == "BeagleBone Black") {
+    $command = "sudo /opt/scripts/tools/grow_partition.sh 2>&1";
+} else if ($settings['Platform'] == "Raspberry Pi") {
+    $command = "sudo /usr/bin/raspi-config --expand-rootfs";
+}
 
 		echo "Command: $command\n";
 		echo "----------------------------------------------------------------------------------\n";
