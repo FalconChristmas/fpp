78857746eff93bb1430b3fbb14fec9b0c7e90566
Daniel Kulp <dan@kulp.com>
2018-05-10 17:54:45 +0000

    Update BBB pixels outputs to support the various 4 channel nodes (RGBW)

--- src/channeloutput/rpi_ws281x.cpp
+++ src/channeloutput/rpi_ws281x.cpp
@@ -157,11 +157,11 @@ void RPIWS281xOutput::PrepData(unsigned char *channelData)
 		ps = m_strings[s];
 		inCh = 0;
 
-		for (int p = 0; p < ps->m_pixels; p++)
+		for (int p = 0; p < ps->m_outputChannels; )
 		{
-			r = ps->m_brightnessMaps[p][channelData[ps->m_outputMap[inCh++]]];
-			g = ps->m_brightnessMaps[p][channelData[ps->m_outputMap[inCh++]]];
-			b = ps->m_brightnessMaps[p][channelData[ps->m_outputMap[inCh++]]];
+			r = ps->m_brightnessMaps[p++][channelData[ps->m_outputMap[inCh++]]];
+			g = ps->m_brightnessMaps[p++][channelData[ps->m_outputMap[inCh++]]];
+			b = ps->m_brightnessMaps[p++][channelData[ps->m_outputMap[inCh++]]];
 
 			ledstring.channel[s].leds[p] =
 				(r << 16) | (g <<  8) | (b);
