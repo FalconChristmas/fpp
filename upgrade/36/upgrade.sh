1bfd31dcd63660066055c208c74382d364760fff
Daniel Kulp <dan@kulp.com>
2018-11-05 13:12:13 -0500

    This shouldn't have happened

--- upgrade/36/upgrade.sh
+++ upgrade/36/upgrade.sh
@@ -1,26 +1,4 @@
 #!/bin/bash
 
-#add symlinks from our bin.pi or bin.bbb directories to the bins in /src
-FPPPLATFORM=$(cat /etc/fpp/platform 2> /dev/null)
-case "${FPPPLATFORM}" in
-	'Raspberry Pi')
-        #remove existing entries
-        sudo rm -f /opt/fpp/bin.pi/fppmm
-        sudo rm -f /opt/fpp/bin.pi/fppd
-        sudo rm -f /opt/fpp/bin.pi/fpp
-        #add symlinks
-        sudo ln -s /opt/fpp/src/fpp /opt/fpp/bin.pi/
-        sudo ln -s /opt/fpp/src/fppd /opt/fpp/bin.pi/
-        sudo ln -s /opt/fpp/src/fppmm /opt/fpp/bin.pi/
-	;;
-	'BeagleBone Black')
-        #remove existing entries
-        sudo rm -f /opt/fpp/bin.bbb/fppmm
-        sudo rm -f /opt/fpp/bin.bbb/fppd
-        sudo rm -f /opt/fpp/bin.bbb/fpp
-        #add symlinks
-        sudo ln -s /opt/fpp/src/fpp /opt/fpp/bin.bbb/
-        sudo ln -s /opt/fpp/src/fppd /opt/fpp/bin.bbb/
-        sudo ln -s /opt/fpp/src/fppmm /opt/fpp/bin.bbb/
-	;;
-esac
+
+# do nothing... this should not have happened
