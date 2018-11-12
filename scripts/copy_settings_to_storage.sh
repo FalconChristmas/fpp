737b81ef5ab75720d00d5aeb2dd498fba55be96b
Daniel Kulp <dkulp@apache.org>
2018-11-12 14:35:53 -0500

    Try to work on FAT

--- scripts/copy_settings_to_storage.sh
+++ scripts/copy_settings_to_storage.sh
@@ -2,6 +2,7 @@
 DEVICE=$1
 
 FSTYPE=$(file -sL /dev/$DEVICE)
+EXTRA_ARGS=""
 
 mkdir -p /tmp/smnt
 if [[ "$FSTYPE" =~ "BTRFS" ]]; then
@@ -9,16 +10,18 @@ if [[ "$FSTYPE" =~ "BTRFS" ]]; then
 elif [[ "$FSTYPE" =~ "ext4" ]]; then
   mount -t ext4 -o noatime,nodiratime,nofail /dev/$DEVICE /tmp/smnt
 elif [[ "$FSTYPE" =~ "FAT" ]]; then
+  EXTRA_ARGS="--no-perms"
   mount -t ext4 -o noatime,nodiratime,exec,nofail,flush,uid=500,gid=500 /dev/$DEVICE /tmp/smnt
 elif [[ "$FSTYPE" =~ "DOS" ]]; then
+  EXTRA_ARGS="--no-perms"
   mount -t auto -o noatime,nodiratime,exec,nofail,flush,uid=500,gid=500 /dev/$DEVICE /tmp/smnt
 else
   mount -t ext4 -o noatime,nodiratime,nofail /dev/$DEVICE /tmp/smnt
 fi
-rsync -av --modify-window=1 --exclude=music/* --exclude=sequences/* --exclude=videos/* /home/fpp/media/* /tmp/smnt
-rsync -av --modify-window=1 /home/fpp/media/music /tmp/smnt
-rsync -av --modify-window=1 /home/fpp/media/sequences /tmp/smnt
-rsync -av --modify-window=1 /home/fpp/media/videos /tmp/smnt
+rsync -av --modify-window=1 $EXTRA_ARGS --exclude=music/* --exclude=sequences/* --exclude=videos/* /home/fpp/media/* /tmp/smnt
+rsync -av --modify-window=1 $EXTRA_ARGS /home/fpp/media/music /tmp/smnt
+rsync -av --modify-window=1 $EXTRA_ARGS /home/fpp/media/sequences /tmp/smnt
+rsync -av --modify-window=1 $EXTRA_ARGS /home/fpp/media/videos /tmp/smnt
 
 
 umount /tmp/smnt
