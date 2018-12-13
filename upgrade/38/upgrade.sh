64f6bf23f26920d0b24a13fd8b25ae7af0fa49a2
Chris Pinkham <cpinkham@bc2va.org>
2018-12-12 20:47:42 -0800

    Update Apache config to allow cloning FPP git repo from a FPP system

---
+++ upgrade/38/upgrade.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#####################################
+# This script enables CGI on the Apache server and sets up Apache to be able
+# to serve the local /opt/fpp/.git directory as a repository.  This will allow
+# remotes to pull in updates from a Master rather than going out to the
+# internet while also ensuring that Remotes are running the same code as
+# the Master system.
+
+# Enable Apache2's CGI module
+a2enmod cgi
+
+# Copy new Apache site config into place
+cp ${FPPDIR}/etc/apache2.site /etc/apache2/sites-enabled/000-default.conf
+
+# Restart Apache
+systemctl restart apache2
+
