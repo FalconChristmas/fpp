96043a50716bdbb776ee660cbd7802dd9baae1ff
Chris Pinkham <cpinkham@bc2va.org>
2018-12-19 11:11:47 -0800

    Update test scripts to use full fppd path and always run in foreground.

--- src/fppd.sh
+++ src/fppd.sh
@@ -4,4 +4,4 @@ export BINDIR=$(dirname $(realpath $0))
 export FPPDIR=$(dirname $(pwd))
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${FPPDIR}/lib:${FPPDIR}/external/RF24
 
-${BINDIR}/fppd $*
+${BINDIR}/fppd -f $*
--- src/fppd_gdb.sh
+++ src/fppd_gdb.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+export BINDIR=$(dirname $(realpath $0))
 export FPPDIR=$(dirname $(pwd))
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${FPPDIR}/lib:${FPPDIR}/external/RF24
 
 
-gdb --args ./fppd $*
+gdb --args ${BINDIR}/fppd -f $*
