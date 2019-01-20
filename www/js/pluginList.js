9a07400854af2cb407f0dfe986c98e6848666139
Chris Pinkham <cpinkham@bc2va.org>
2019-01-20 11:28:11 -0800

    Delete the now-unused pluginList.js file.

--- www/js/pluginList.js
+++
@@ -1,9 +0,0 @@
-var pluginList = [
-	[ 'fpp-BigButtons', 'https://raw.githubusercontent.com/FalconChristmas/fpp-BigButtons/master/pluginInfo.json' ],
-	[ 'fpp-vastfmt', 'https://raw.githubusercontent.com/FalconChristmas/fpp-vastfmt/master/pluginInfo.json' ],
-	[ 'big-green-button', 'https://raw.githubusercontent.com/FalconChristmas/big-green-button/master/pluginInfo.json' ],
-	[ 'fpp-matrixtools', 'https://raw.githubusercontent.com/cpinkham/fpp-matrixtools/master/pluginInfo.json' ],
-	[ 'fpp-after-hours', 'https://raw.githubusercontent.com/jcrossbdn/fpp-after-hours/master/pluginInfo.json' ],
-	[ 'fpp-plugin-Template', 'https://raw.githubusercontent.com/FalconChristmas/fpp-plugin-Template/master/pluginInfo.json' ]
-];
-
