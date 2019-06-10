57c03e1a7e5b7b03ed74f1610af4affab6c2c1b1
Daniel Kulp <dan@kulp.com>
2019-06-10 19:10:06 +0000

    Re-write the capedetect stuff in C/C++ to avoid loading the giant php runtime interpretter at boot.  Cuts about 2-3 seconds off boot.

---
+++ src/CapeUtils.h
@@ -0,0 +1,20 @@
+/*
+ *   Falcon Player (FPP) header file
+ *
+ *   Copyright (C) 2013-2019 the Falcon Player Developers
+ *
+ *   The Falcon Player (FPP) is free software; you can redistribute it
+ *   and/or modify it under the terms of the GNU General Public License
+ *   as published by the Free Software Foundation; either version 2 of
+ *   the License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+bool fpp_detectCape();
---
+++ src/fppcapedetect.c
@@ -0,0 +1,29 @@
+/*
+ *   Falcon Player (FPP)
+ *
+ *   Copyright (C) 2013-2019 the Falcon Player Developers
+ *      For additional credits and developers, see credits.php.
+ *
+ *   The Falcon Player (FPP) is free software; you can redistribute it
+ *   and/or modify it under the terms of the GNU General Public License
+ *   as published by the Free Software Foundation; either version 2 of
+ *   the License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+#include <stdlib.h>
+#include "CapeUtils.h"
+
+int main(int argc, char *argv[])
+{
+    if (fpp_detectCape()) {
+        exit(-1);
+    }
+}
+
