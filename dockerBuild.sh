8553d3c7b680cfd7b022f7c59bb8ebb85a6e27f7
Daniel Kulp <dkulp@apache.org>
2019-07-02 08:21:08 -0400

    Flip to falconchristmas org on dockerhub

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # build the docker image
-docker build --no-cache -t fpp:master -f Docker/Dockerfile .
+docker build --no-cache -t falconchristmas/fpp:latest -f Docker/Dockerfile .
+#or
+#docker build -t falconchristmas/fpp:latest -f Docker/Dockerfile .
 
-# to push to dockerhub:
-#docker build -t dkulp/fpp:master -f Docker/Dockerfile .
 
 #to run the image using the local content to overwride:
-# docker run --rm -t -i -p 80:80 -p 4048:4048/udp -p 5568:5568/udp -p 32320:32320/udp -v ${PWD}:/opt/fpp fpp:master
+# docker run --rm -t -i -p 80:80 -p 4048:4048/udp -p 5568:5568/udp -p 32320:32320/udp -v ${PWD}:/opt/fpp falconchristmas/fpp:latest
