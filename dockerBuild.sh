3fee22446116d620229639c79da4cecb4817a005
Daniel Kulp <dkulp@apache.org>
2019-07-02 02:41:13 -0400

    Update command for running docker image

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -3,6 +3,8 @@
 # build the docker image
 docker build --no-cache -t fpp:master -f Docker/Dockerfile .
 
+# to push to dockerhub:
+#docker build -t dkulp/fpp:master -f Docker/Dockerfile .
 
 #to run the image using the local content to overwride:
-# docker run --rm -t -i  -v ${PWD}:/opt/fpp fpp:master
+# docker run --rm -t -i -p 80:80 -p 4048:4048/udp -p 5568:5568/udp -p 32320:32320/udp -v ${PWD}:/opt/fpp fpp:master
