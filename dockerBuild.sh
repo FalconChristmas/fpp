953f72ad1450440fc0af7658852dbfe23339aa2c
Daniel Kulp <dkulp@apache.org>
2019-07-11 11:59:54 -0400

    By default, clone  a new /opt/fpp directory and not use the local copy

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -3,16 +3,16 @@
 # build the docker image
 docker build --no-cache -t falconchristmas/fpp:latest -f Docker/Dockerfile .
 #or
-#docker build -t falconchristmas/fpp:latest -f Docker/Dockerfile .
+#docker build -t falconchristmas/fpp:latest  --build-arg EXTRA_INSTALL_FLAG=--skip-clone -f Docker/Dockerfile .
 
 
 
 
-#to run the image using the local content to overwride:
+#to run the image using the local content to override:
 # On OSX, it's highly suggested to set an env variable of:
 # export DOCKER_MOUNT_FLAG=":delegated"
 # Using :delegated will drop the "make" time by almost half.
-# However, editing files from OSX may take a few
-# extra second before the changes are available within the docker container.
+# However, editing files from OSX may take an extra second
+# before the changes are available within the docker container.
 
 # docker run --rm -t -i -p 80:80 -p 4048:4048/udp -p 5568:5568/udp -p 32320:32320/udp -v ${PWD}:/opt/fpp${DOCKER_MOUNT_FLAG} falconchristmas/fpp:latest
