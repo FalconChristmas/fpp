088de27007804011675bf70544b8c1ba7afa3fdd
Daniel Kulp <dkulp@apache.org>
2019-07-29 20:52:19 -0400

    Fix ola build

--- src/makefiles/libfpp-co-OLA.mk
+++ src/makefiles/libfpp-co-OLA.mk
@@ -1,7 +1,7 @@
 OLAFLAGS := $(shell pkg-config --cflags --libs libola 2> /dev/null)
 ifneq '$(OLAFLAGS)' ''
 
-SRC_fpp_co_OLA_so += channeloutput/OLAOutput.cpp
+SRC_fpp_co_OLA_so += channeloutput/OLAOutput.o
 LIBS_fpp_co_OLA_so=-L. -lfpp
 
 TARGETS += libfpp-co-OLA.so
