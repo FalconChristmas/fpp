d92f8834c74dd987ad418eb0d143def8ecaead80
Daniel Kulp <dan@kulp.com>
2019-08-01 11:11:04 +0000

    Fix build of USB/DMX outputs

---
+++ src/makefiles/libfpp-co-USBDMX.mk
@@ -0,0 +1,9 @@
+OBJECTS_fpp_co_USBDMX_so += channeloutput/USBDMX.o
+LIBS_fpp_co_USBDMX_so += -L. -lfpp
+
+TARGETS += libfpp-co-USBDMX.so
+OBJECTS_ALL+=$(OBJECTS_fpp_co_USBDMX_so)
+
+libfpp-co-USBDMX.so: libfpp.so $(OBJECTS_fpp_co_USBDMX_so)
+	$(CCACHE) $(CC) -shared $(CFLAGS_$@) $(OBJECTS_fpp_co_USBDMX_so) $(LIBS_fpp_co_USBDMX_so) $(LDFLAGS) $(LDFLAGS_fpp_co_USBDMX_so) -o $@
+
