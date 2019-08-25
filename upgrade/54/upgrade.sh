e60bbf32a06fd212b037f9a603e054ec937c6e2a
Daniel Kulp <dkulp@apache.org>
2019-08-25 18:28:07 -0400

    Fix attempt to set prop in connman.conf

--- upgrade/54/upgrade.sh
+++ upgrade/54/upgrade.sh
@@ -6,11 +6,11 @@ BINDIR=$(cd $(dirname $0) && pwd)
 
 
 sed -i 's/^NetworkInterfaceBlacklist.*/NetworkInterfaceBlacklist=SoftAp0,usb0,usb1/g' /etc/connman/main.conf
-if ! grep -q NetworkInterfaceBlacklist "/etc/connman/main.conf"; then
+if ! grep -q "^NetworkInterfaceBlacklist" "/etc/connman/main.conf"; then
     echo "NetworkInterfaceBlacklist=SoftAp0,usb0,usb1" >> /etc/connman/main.conf
 fi
 
-cp /opt/fpp/etc/systemd/connman/fppd.service /lib/systemd/system
+cp /opt/fpp/etc/systemd/fppd.service /lib/systemd/system
 systemctl daemon-reload
 systemctl reenable fppd
 
