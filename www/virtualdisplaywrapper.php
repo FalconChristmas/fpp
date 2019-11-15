46f741907a72ebb7e1c3141631f664ee09acbb87
Chris Pinkham <cpinkham@bc2va.org>
2019-11-14 19:22:31 -0800

    Minor text changes in Virtual Display UI.

--- www/virtualdisplaywrapper.php
+++ www/virtualdisplaywrapper.php
@@ -18,11 +18,11 @@ include_once 'common/menuHead.inc';
   <br>
   <div>
     <fieldset style="padding: 10px; border: 2px solid #000;">
-      <legend>FPP HTML Virtual Display</legend>
+      <legend>Virtual Display</legend>
 <?
 require_once('virtualdisplaybody.php');
 ?>
-	<br>NOTE: If the HTML Virtual Display is not working, you need to add the HTML Virtual Display Channel Output on the 'Other' tab of the Channel Outputs config page.  Your background image must be uploaded as a file called 'virtualdisplaybackground.jpg' and your pixel map must be copied to FPP's configuration directory from xLights as a file called 'virtualdisplaymap'.
+	<br>NOTE: If the Virtual Display is not working, you need to add the HTTP Virtual Display Channel Output on the 'Other' tab of the Channel Outputs config page.  Your background image must be uploaded as a file called 'virtualdisplaybackground.jpg' and your pixel map must be copied to FPP's configuration directory from xLights as a file called 'virtualdisplaymap'.
     </fieldset>
   </div>
   <?php include 'common/footer.inc'; ?>
