3870ae5b7f4d01a6788e000fe97758ad3e3ab47d
Chris Pinkham <cpinkham@bc2va.org>
2019-12-12 14:31:21 -0800

    Rename Touch Punch JS since it isn't the minified version anymore
    This file has a patch applied to fix the UI on mobile devices,
    so the non-minified version is used since it is a small script
    anyway.

--- www/upgradefpp.php
+++ www/upgradefpp.php
@@ -15,7 +15,7 @@ Upgrading FPP
 <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
 <script type="text/javascript" src="js/jquery-migrate-3.1.0.min.js"></script>
 <script type="text/javascript" src="js/jquery-ui.min.js"></script>
-<script type="text/javascript" src="js/jquery.ui.touch-punch.min.js"></script>
+<script type="text/javascript" src="js/jquery.ui.touch-punch.js"></script>
 <script type="text/javascript" src="js/jquery.jgrowl.min.js"></script>
 <link rel="stylesheet" href="css/jquery.jgrowl.min.css" />
 <script>
