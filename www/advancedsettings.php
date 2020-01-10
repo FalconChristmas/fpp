3554a9d4f415980c219fed7b507b4d87503830fc
Daniel Kulp <dan@kulp.com>
2020-01-10 14:01:23 +0000

    Move storage location to advanced settings, add warnings about using it

--- www/advancedsettings.php
+++ www/advancedsettings.php
@@ -5,6 +5,9 @@
 <?php include 'common/menuHead.inc'; ?>
 <title><? echo $pageTitle; ?></title>
 
+<script type="text/javascript" src="jquery/jQuery.msgBox/scripts/jquery.msgBox.js"></script>
+<link href="jquery/jQuery.msgBox/styles/msgBoxLight.css" rel="stylesheet" type="text/css">
+
 <script>
 function growSDCardFS() {
     $('#dialog-confirm')
@@ -87,7 +90,170 @@ function flashEMMCBtrfs() {
 }
 <?php
     }
+    
+    
+function PrintStorageDeviceSelect($platform)
+{
+	global $SUDO;
+
+	# FIXME, this would be much simpler by parsing "lsblk -l"
+	exec('lsblk -l | grep /boot | cut -f1 -d" " | sed -e "s/p[0-9]$//"', $output, $return_val);
+    if (count($output) > 0) {
+        $bootDevice = $output[0];
+    } else {
+        $bootDevice = "";
+    }
+	unset($output);
+
+    if ($platform == "BeagleBone Black") {
+        exec('findmnt -n -o SOURCE / | colrm 1 5', $output, $return_val);
+        $rootDevice = $output[0];
+        unset($output);
+        
+        if ($bootDevice == "") {
+            exec('findmnt -n -o SOURCE / | colrm 1 5 | sed -e "s/p[0-9]$//"', $output, $return_val);
+            $bootDevice = $output[0];
+            unset($output);
+        }
+    } else {
+        exec('lsblk -l | grep " /$" | cut -f1 -d" "', $output, $return_val);
+        $rootDevice = $output[0];
+        unset($output);
+    }
+
+	$storageDevice = "";
+	exec('grep "fpp/media" /etc/fstab | cut -f1 -d" " | sed -e "s/\/dev\///"', $output, $return_val);
+	if (isset($output[0]))
+		$storageDevice = $output[0];
+	unset($output);
+
+	$found = 0;
+	$values = Array();
+
+	foreach(scandir("/dev/") as $fileName)
+	{
+		if ((preg_match("/^sd[a-z][0-9]/", $fileName)) ||
+			(preg_match("/^mmcblk[0-9]p[0-9]/", $fileName)))
+		{
+			exec($SUDO . " sfdisk -s /dev/$fileName", $output, $return_val);
+			$GB = intval($output[0]) / 1024.0 / 1024.0;
+			unset($output);
+
+			if ($GB <= 0.1)
+				continue;
+
+			$FreeGB = "Not Mounted";
+			exec("df -k /dev/$fileName | grep $fileName | awk '{print $4}'", $output, $return_val);
+			if (count($output))
+			{
+				$FreeGB = sprintf("%.1fGB Free", intval($output[0]) / 1024.0 / 1024.0);
+				unset($output);
+			}
+			else
+			{
+				unset($output);
+
+				if (preg_match("/^$rootDevice/", $fileName))
+				{
+					exec("df -k / | grep ' /$' | awk '{print \$4}'", $output, $return_val);
+					if (count($output))
+						$FreeGB = sprintf("%.1fGB Free", intval($output[0]) / 1024.0 / 1024.0);
+					unset($output);
+				}
+			}
+
+			$key = $fileName . " ";
+			$type = "";
+
+			if (preg_match("/^$bootDevice/", $fileName))
+			{
+				$type .= " (boot device)";
+			}
+
+			if (preg_match("/^sd/", $fileName))
+			{
+				$type .= " (USB)";
+			}
+
+			$key = sprintf( "%s - %.1fGB (%s) %s", $fileName, $GB, $FreeGB, $type);
+
+			$values[$key] = $fileName;
+
+			if ($storageDevice == $fileName)
+				$found = 1;
+		}
+	}
+
+	if (!$found)
+	{
+		$arr = array_reverse($values, true);
+		$values = array_reverse($arr);
+	}
+    if ($storageDevice == "") {
+        $storageDevice = $rootDevice;
+    }
+
+	PrintSettingSelect('StorageDevice', 'storageDevice', 0, 1, $storageDevice, $values, "", "", "checkFormatStorage");
+}
+
 ?>
+
+
+function checkForStorageCopy() {
+    $.msgBox({
+             title: "Copy settings?",
+             content: "Would you like to copy all files to the new storage location?\nAll settings on the new storage will be overwritten.",
+             type: "info",
+             buttons: [{ value: "Yes" }, { value: "No" }],
+             success: function (result) {
+                 storageDeviceChanged();
+                 if (result == "Yes") {
+                    window.location.href="copystorage.php?storageLocation=" + $('#storageDevice').val();
+                 }
+            }
+        });
+}
+
+function checkFormatStorage()
+{
+    var value = $('#storageDevice').val();
+    
+    var e = document.getElementById("storageDevice");
+    var name = e.options[e.selectedIndex].text;
+    if (name.includes("Not Mounted")) {
+        var btitle = "Format Storage Location (" + value + ")" + name;
+        $.msgBox({ type: "prompt",
+                 title: btitle,
+                 inputs: [
+                     { header: "Don't Format", type: "radio", name: "formatType", checked:"", value: "none" },
+                     { header: "ext4 (Most stable)", type: "radio", name: "formatType", value: "ext4" },
+                     { header: "FAT (Compatible with Windows/OSX, unsupported, slow)", type: "radio", name: "formatType", value: "FAT"}
+                 ],
+                 buttons: [ { value: "OK" } ],
+                 opacity: 0.5,
+                 success: function (result, values) {
+                 var v = $('input[name=formatType]:checked').val();
+                 if (v != "none") {
+                    $.ajax({ url: "formatstorage.php?fs=" + v + "&storageLocation=" + $('#storageDevice').val(),
+                        async: false,
+                        success: function(data) {
+                           checkForStorageCopy();
+                        },
+                        failure: function(data) {
+                        DialogError("Formate Storage", "Error formatting storage.");
+                        }
+                        });
+                    } else {
+                        checkForStorageCopy();
+                    }
+                 }
+                 });
+    } else {
+        storageDeviceChanged();
+    }
+}
+
+
 </script>
 
 </head>
@@ -99,6 +265,15 @@ function flashEMMCBtrfs() {
 	<fieldset>
 	<legend>FPP Advanced Settings</legend>
 	<table table width = "100%">
+        <? if ($settings['SubPlatform'] != "Docker" ) { ?>
+        <tr>
+          <td><b>Storage Device:</b></td>
+          <td><? PrintStorageDeviceSelect($settings['Platform']); ?><br>
+            Changing the storage device to anything other than the SD card is NOT supported and untested.  If it fails to work, your system may not boot properly.  Using a partition type other than ext4 is also not supported and may cause playback issues.  If you are not extremely comfortable with Linux and diagnosing issues, do not change this.  This option will likely be removed in a future version of FPP. 
+            </td>
+        </tr>
+        <tr><td colspan='2'><hr></td></tr>
+    <? } ?>
         <tr><td valign='top' align="right"><? PrintSettingCheckbox("Show All Options", "showAllOptions", 0, 0, "1", "0"); ?></td><td><b>Display all options/settings</b> -
 If
                 turned off and FPPD can detect what hardware (cape/hat/etc...) is connected, certain options that are either incompatible with the
--- www/settings.php
+++ www/settings.php
@@ -154,109 +154,6 @@ $BBBPowerLed = Array();
     $BBBPowerLed['Disabled'] = 0;
     $BBBPowerLed['Enabled'] = 1;
 
-function PrintStorageDeviceSelect($platform)
-{
-	global $SUDO;
-
-	# FIXME, this would be much simpler by parsing "lsblk -l"
-	exec('lsblk -l | grep /boot | cut -f1 -d" " | sed -e "s/p[0-9]$//"', $output, $return_val);
-    if (count($output) > 0) {
-        $bootDevice = $output[0];
-    } else {
-        $bootDevice = "";
-    }
-	unset($output);
-
-    if ($platform == "BeagleBone Black") {
-        exec('findmnt -n -o SOURCE / | colrm 1 5', $output, $return_val);
-        $rootDevice = $output[0];
-        unset($output);
-        
-        if ($bootDevice == "") {
-            exec('findmnt -n -o SOURCE / | colrm 1 5 | sed -e "s/p[0-9]$//"', $output, $return_val);
-            $bootDevice = $output[0];
-            unset($output);
-        }
-    } else {
-        exec('lsblk -l | grep " /$" | cut -f1 -d" "', $output, $return_val);
-        $rootDevice = $output[0];
-        unset($output);
-    }
-
-	$storageDevice = "";
-	exec('grep "fpp/media" /etc/fstab | cut -f1 -d" " | sed -e "s/\/dev\///"', $output, $return_val);
-	if (isset($output[0]))
-		$storageDevice = $output[0];
-	unset($output);
-
-	$found = 0;
-	$values = Array();
-
-	foreach(scandir("/dev/") as $fileName)
-	{
-		if ((preg_match("/^sd[a-z][0-9]/", $fileName)) ||
-			(preg_match("/^mmcblk[0-9]p[0-9]/", $fileName)))
-		{
-			exec($SUDO . " sfdisk -s /dev/$fileName", $output, $return_val);
-			$GB = intval($output[0]) / 1024.0 / 1024.0;
-			unset($output);
-
-			if ($GB <= 0.1)
-				continue;
-
-			$FreeGB = "Not Mounted";
-			exec("df -k /dev/$fileName | grep $fileName | awk '{print $4}'", $output, $return_val);
-			if (count($output))
-			{
-				$FreeGB = sprintf("%.1fGB Free", intval($output[0]) / 1024.0 / 1024.0);
-				unset($output);
-			}
-			else
-			{
-				unset($output);
-
-				if (preg_match("/^$rootDevice/", $fileName))
-				{
-					exec("df -k / | grep ' /$' | awk '{print \$4}'", $output, $return_val);
-					if (count($output))
-						$FreeGB = sprintf("%.1fGB Free", intval($output[0]) / 1024.0 / 1024.0);
-					unset($output);
-				}
-			}
-
-			$key = $fileName . " ";
-			$type = "";
-
-			if (preg_match("/^$bootDevice/", $fileName))
-			{
-				$type .= " (boot device)";
-			}
-
-			if (preg_match("/^sd/", $fileName))
-			{
-				$type .= " (USB)";
-			}
-
-			$key = sprintf( "%s - %.1fGB (%s) %s", $fileName, $GB, $FreeGB, $type);
-
-			$values[$key] = $fileName;
-
-			if ($storageDevice == $fileName)
-				$found = 1;
-		}
-	}
-
-	if (!$found)
-	{
-		$arr = array_reverse($values, true);
-		$values = array_reverse($arr);
-	}
-    if ($storageDevice == "") {
-        $storageDevice = $rootDevice;
-    }
-
-	PrintSettingSelect('StorageDevice', 'storageDevice', 0, 1, $storageDevice, $values, "", "", "checkFormatStorage");
-}
 
 ?>
 
@@ -264,60 +161,6 @@ function PrintStorageDeviceSelect($platform)
 <script type="text/javascript" src="jquery/jQuery.msgBox/scripts/jquery.msgBox.js"></script>
 <link href="jquery/jQuery.msgBox/styles/msgBoxLight.css" rel="stylesheet" type="text/css">
 <script>
-
-function checkForStorageCopy() {
-    $.msgBox({
-             title: "Copy settings?",
-             content: "Would you like to copy all files to the new storage location?\nAll settings on the new storage will be overwritten.",
-             type: "info",
-             buttons: [{ value: "Yes" }, { value: "No" }],
-             success: function (result) {
-                 storageDeviceChanged();
-                 if (result == "Yes") {
-                    window.location.href="copystorage.php?storageLocation=" + $('#storageDevice').val();
-                 }
-            }
-        });
-}
-
-function checkFormatStorage()
-{
-    var value = $('#storageDevice').val();
-    
-    var e = document.getElementById("storageDevice");
-    var name = e.options[e.selectedIndex].text;
-    if (name.includes("Not Mounted")) {
-        var btitle = "Format Storage Location (" + value + ")" + name;
-        $.msgBox({ type: "prompt",
-                 title: btitle,
-                 inputs: [
-                 { header: "Don't Format", type: "radio", name: "formatType", checked:"", value: "none" },
-                 { header: "FAT (Compatible with Windows/OSX)", type: "radio", name: "formatType", value: "FAT"},
-                 { header: "ext4 (Most stable)", type: "radio", name: "formatType", value: "ext4" },
-                 { header: "btrfs (Compression, Fastest)", type: "radio", name: "formatType", value: "btrfs" }],
-                 buttons: [ { value: "OK" } ],
-                 opacity: 0.5,
-                 success: function (result, values) {
-                 var v = $('input[name=formatType]:checked').val();
-                 if (v != "none") {
-                    $.ajax({ url: "formatstorage.php?fs=" + v + "&storageLocation=" + $('#storageDevice').val(),
-                        async: false,
-                        success: function(data) {
-                           checkForStorageCopy();
-                        },
-                        failure: function(data) {
-                        DialogError("Formate Storage", "Error formatting storage.");
-                        }
-                        });
-                    } else {
-                        checkForStorageCopy();
-                    }
-                 }
-                 });
-    } else {
-        storageDeviceChanged();
-    }
-}
 
 	var queuedChanges = 0;
 	function MaskChanged(cbox)
@@ -496,12 +339,7 @@ function ToggleLCDNow()
       <td>UI Border Color:</td>
       <td><? PrintSettingSelect("UI Background Color", "backgroundColor", 0, 0, isset($settings['backgroundColor']) ? $settings['backgroundColor'] : "", $backgroundColors, "", "reloadPage"); ?></td>
     </tr>
-<? if ($settings['SubPlatform'] != "Docker" ) { ?>
-    <tr>
-      <td>Storage Device:</td>
-      <td><? PrintStorageDeviceSelect($settings['Platform']); ?></td>
-    </tr>
-<? }
+<? 
    if ( file_exists("/dev/i2c-1") || file_exists("/dev/i2c-2") ) { ?>
     <tr>
         <td>OLED Status Display:</td>
