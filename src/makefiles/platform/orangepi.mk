81d7cbfdac68651f7ac1fc7c4f3e9a61d0084d0a
Chris Pinkham <cpinkham@bc2va.org>
2020-01-21 11:20:46 -0800

    No WiringPi on OrangePi

--- src/makefiles/platform/orangepi.mk
+++ src/makefiles/platform/orangepi.mk
@@ -1,12 +1,6 @@
 ifeq '$(ARCH)' 'OrangePi'
 # do something for Orange Pi
 
-LIBS_GPIO_ADDITIONS=-lwiringPi -lwiringPiDev
-OBJECTS_GPIO_ADDITIONS+=util/WiringPiGPIO.o
-
-CXXFLAGS_util/SPIUtils.o+=-DUSEWIRINGPI
-CXXFLAGS_util/GPIOUtils.o+=-DUSEWIRINGPI
-
 CFLAGS += \
 	-DPLATFORM_ORANGEPI \
 	$(NULL)
