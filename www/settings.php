6ad38579bd6780af5f9b9c4171af0824123606c9
Chris Pinkham <cpinkham@bc2va.org>
2020-01-22 18:57:31 -0800

    Fix UI formatting issue and file indention

--- www/settings.php
+++ www/settings.php
@@ -34,112 +34,114 @@ function reloadSettingsPage() {
 <body>
 <div id="bodyWrapper">
 <?php include 'menu.inc'; ?>
-<div id="fileManager">
-    <br />
-    <div class='title'>FPP Settings</div>
-    <div id="tabs">
-        <ul>
-            <li><a href="#tab-playback">Playback</a></li>
-            <li><a href="#tab-network">Network</a></li>
-            <li><a href="#tab-time">Time</a></li>
-            <li><a href="#tab-ui">UI</a></li>
-            <li><a href="#tab-email">Email</a></li>
-            <li><a href="#tab-proxy">Proxy</a></li>
-            <li><a href="#tab-mqtt">MQTT</a></li>
-            <li><a href="#tab-output">Output</a></li>
-            <li><a href="#tab-logs">Logging</a></li>
-            <li><a href="#tab-system">System</a></li>
+    <div class='fppTabs'>
+        <div id="settingsManager">
+        <br />
+            <div class='title'>FPP Settings</div>
+            <div id="tabs" style='display:none'>
+                <ul>
+                    <li><a href="#tab-playback">Playback</a></li>
+                    <li><a href="#tab-network">Network</a></li>
+                    <li><a href="#tab-time">Time</a></li>
+                    <li><a href="#tab-ui">UI</a></li>
+                    <li><a href="#tab-email">Email</a></li>
+                    <li><a href="#tab-proxy">Proxy</a></li>
+                    <li><a href="#tab-mqtt">MQTT</a></li>
+                    <li><a href="#tab-output">Output</a></li>
+                    <li><a href="#tab-logs">Logging</a></li>
+                    <li><a href="#tab-system">System</a></li>
 <? if ($uiLevel >= 3) echo "<li><a href='#tab-developer'>Developer</a></li>\n"; ?>
-        </ul>
+                </ul>
 
-    <div id='tab-playback'>
-        <? include 'settings-playback.php'; ?>
-    </div>
+                <div id='tab-playback'>
+                    <? include 'settings-playback.php'; ?>
+                </div>
 
-    <div id='tab-network'>
-        <? include 'settings-network.php'; ?>
-    </div>
+                <div id='tab-network'>
+                    <? include 'settings-network.php'; ?>
+                </div>
 
-    <div id='tab-time'>
-        <? include 'settings-time.php'; ?>
-    </div>
+                <div id='tab-time'>
+                    <? include 'settings-time.php'; ?>
+                </div>
 
-    <div id='tab-ui'>
-        <? include 'settings-ui.php'; ?>
-    </div>
+                <div id='tab-ui'>
+                    <? include 'settings-ui.php'; ?>
+                </div>
 
-    <div id='tab-email'>
-        <? include 'settings-email.php'; ?>
-    </div>
+                <div id='tab-email'>
+                    <? include 'settings-email.php'; ?>
+                </div>
 
-    <div id='tab-proxy'>
-        <? include 'settings-proxy.php'; ?>
-    </div>
+                <div id='tab-proxy'>
+                    <? include 'settings-proxy.php'; ?>
+                </div>
 
-    <div id='tab-mqtt'>
-        <? include 'settings-mqtt.php'; ?>
-    </div>
+                <div id='tab-mqtt'>
+                    <? include 'settings-mqtt.php'; ?>
+                </div>
 
-    <div id='tab-output'>
-        <? include 'settings-output.php'; ?>
-    </div>
+                <div id='tab-output'>
+                    <? include 'settings-output.php'; ?>
+                </div>
 
-    <div id='tab-logs'>
-        <? include 'settings-logs.php'; ?>
-    </div>
+                <div id='tab-logs'>
+                    <? include 'settings-logs.php'; ?>
+                </div>
 
-    <div id='tab-system'>
-        <? include 'settings-system.php'; ?>
-    </div>
+                <div id='tab-system'>
+                    <? include 'settings-system.php'; ?>
+                </div>
 
 <? if ($uiLevel >= 3) { ?>
-    <div id='tab-developer'>
-        <? include 'settings-developer.php'; ?>
-    </div>
+                <div id='tab-developer'>
+                    <? include 'settings-developer.php'; ?>
+                </div>
 <? } ?>
+            </div>
+        </div>
 
-</div>
-
-<table>
+        <table>
 <? if ($uiLevel >= 1) { ?>
-<tr><th align='right'>*</th><th align='left'>- Advanced Level Setting</th>
+            <tr><th align='right'>*</th><th align='left'>- Advanced Level Setting</th></tr>
 <? } ?>
 <? if ($uiLevel >= 2) { ?>
-<tr><th align='right'>**</th><th align='left'>- Experimental Level Setting</th>
+            <tr><th align='right'>**</th><th align='left'>- Experimental Level Setting</th></tr>
 <? } ?>
 <? if ($uiLevel >= 3) { ?>
-<tr><th align='right'>***</th><th align='left'>- Developer Level Setting</th>
+            <tr><th align='right'>***</th><th align='left'>- Developer Level Setting</th></tr>
 <? } ?>
-</table>
+        </table>
 
 <? if ($uiLevel >= 1) { ?>
-<br>
-<a href="advancedsettings.php">Advanced Settings</a>  NOTE: The Advanced Settings page is being deprecated with settings moved into appropriate tabs on this page and marked as Advanced settings.
+        <br>
+        <a href="advancedsettings.php">Advanced Settings</a>  NOTE: The Advanced Settings page is being deprecated with settings moved into appropriate tabs on this page and marked as Advanced settings.
 <? } ?>
+    </div>
 
 <?php	include 'common/footer.inc'; ?>
 
 <script>
-	var activeTabNumber = 
+var activeTabNumber =
 <?php
-	if (isset($_GET['tab']))
-		print $_GET['tab'];
-	else
-		print "0";
+if (isset($_GET['tab']))
+    print $_GET['tab'];
+else
+    print "0";
 ?>;
 
-    $("#tabs").tabs( {
-        cache: true,
-        active: activeTabNumber,
-        spinner: "",
-        fx: {
-            opacity: 'toggle',
-            height: 'toggle'
-        },
-        activate: function(event, ui) {
-            $('.ui-tooltip').hide();
-        }
-    });
+$("#tabs").tabs( {
+    cache: true,
+    active: activeTabNumber,
+    spinner: "",
+    fx: {
+        opacity: 'toggle',
+        height: 'toggle'
+    },
+    activate: function(event, ui) {
+        $('.ui-tooltip').hide();
+    }
+});
 
 $( function() {
     $(document).tooltip({
@@ -154,7 +156,10 @@ $( function() {
     });
 });
 
+$('#tabs').show();
+
 </script>
 
+</div>
 </body>
 </html>
