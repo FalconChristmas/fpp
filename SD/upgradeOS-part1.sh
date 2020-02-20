9ac1a3fedc3b72be25f19083515a4df5dec465d8
Daniel Kulp <dan@kulp.com>
2020-02-19 19:25:55 -0500

    Attempt to add basic image platform checks

--- SD/upgradeOS-part1.sh
+++ SD/upgradeOS-part1.sh
@@ -2,6 +2,16 @@
 
 mount $1 /mnt
 
+
+ORIGTYPE=$(</etc/fpp/platform)
+NEWTYPE=$(</mnt/etc/fpp/platform)
+
+if [ $ORIGTYPE != $NEWTYPE ]; then
+    echo "Image of '${NEWTYPE}' does no match '${ORIGTYPE}'"
+    umount /mnt
+    exit 1;
+fi
+
 mount -o bind / /mnt/mnt
 mount -o bind /boot /mnt/mnt/boot
 mount -t tmpfs tmpfs /mnt/tmp
@@ -17,5 +27,12 @@ umount /mnt/mnt/boot
 umount /mnt/mnt
 
 sync
+
+echo "</pre>"
+echo "<b>Rebooting.... please wait for FPP to reboot.</b>"
+echo "<a href='index.php'>Go to FPP Main Status Page</a><br>"
+echo "<a href='about.php'>Go back to FPP About page</a><br>"
+echo "</body>"
+echo "</html>"
 
 reboot -f -f
