69845c26eeb73ca0f01e1616ad8942ef7f16d79c
Daniel Kulp <dan@kulp.com>
2020-07-25 14:15:56 -0400

    More packages needed for VLC

--- SD/buildVLC.sh
+++ SD/buildVLC.sh
@@ -10,16 +10,16 @@ git checkout d7c05336177a07e445d5c9021b5dba075cafc3a0
 
 DISABLES=""
 if [ -f /etc/fpp/desktop ]; then
-    apt-get install libxcb-composite0-dev libxcb-xkb-dev
-    DISABLES="--disable-qt"
+    apt-get install libxcb-composite0-dev libxcb-xkb-dev libxcb-randr0-dev libxcb-shm0-dev
+    DISABLES=""
 else
-    DISABLES="--disable-qt --disable-xcb"
+    DISABLES="--disable-xcb"
 fi
 
 
 export LDFLAGS="-latomic"
 ./bootstrap
-./configure --disable-lua --disable-a52 --disable-chromecast --disable-chromaprint  --disable-pulse --disable-jack --disable-dbus --disable-avahi $DISABLES --enable-run-as-root
+./configure --disable-lua --disable-a52 --disable-chromecast --disable-chromaprint  --disable-pulse --disable-jack --disable-dbus --disable-avahi --disable-qt $DISABLES --enable-run-as-root
 make
 make install
 ldconfig
