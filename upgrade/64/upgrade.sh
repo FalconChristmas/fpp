10c102d93fa96f892ef85cbd998e86cf1ad5372b
Daniel Kulp <dan@kulp.com>
2020-10-27 02:04:52 +0000

    Wrong spelling

--- upgrade/64/upgrade.sh
+++ upgrade/64/upgrade.sh
@@ -4,8 +4,11 @@
 BINDIR=$(cd $(dirname $0) && pwd)
 . ${BINDIR}/../../scripts/common
 
-if [ "${FPPPLATFORM}" == "Beaglebone Black" ]; then
-    cp /opt/fpp/upgrade/64/bbb-fpp-reserve-memory.dtbo /lib/firmware/bbb-fpp-reserve-memory.dtbo
+if [ "${FPPPLATFORM}" == "BeagleBone Black" ]; then
+    echo "Setting up new reserved memory  block"
+    cp -f /opt/fpp/upgrade/64/bbb-fpp-reserve-memory.dtbo /lib/firmware/bbb-fpp-reserve-memory.dtbo
     setSetting rebootFlag 1
+else
+    echo "No need to update for platform ${FPPPLATFORM}"
 fi
 
