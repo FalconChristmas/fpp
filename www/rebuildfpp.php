26992eb5b055691e70e5fa0137957b3767cc4193
Scott Hanson <scooter_seh@yahoo.com>
2020-12-03 15:59:48 -0500

    removed my new scrip and used existing scripts

--- www/rebuildfpp.php
+++ www/rebuildfpp.php
@@ -1,34 +1,68 @@
-<!DOCTYPE html>
-<html>
-<?php
+<?
+header( "Access-Control-Allow-Origin: *");
+
+$wrapped = 0;
+
+if (isset($_GET['wrapped']))
+    $wrapped = 1;
+
+if (!$wrapped)
+    echo "<html>\n";
 
 $skipJSsettings = 1;
 require_once("common.php");
 
 DisableOutputBuffering();
-?>
 
+if (!$wrapped) {
+?>
 <head>
 <title>
 Rebuild FPP
 </title>
 </head>
 <body>
-<h2>Rebuilding FPP</h2>
+<h2>Rebuild FPP</h2>
 <pre>
-<?php
-    echo "==================================================================================\n";
-
-	$command = "sudo /opt/fpp/scripts/fpp_rebuild 2>&1";
-
-	echo "Command: $command\n";
-	echo "----------------------------------------------------------------------------------\n";
-    system($command);
-	echo "\n";
+<?
+}
 ?>
+Stopping fppd...
+<?php
+if (file_exists("/.dockerenv")) {
+    system($SUDO . " $fppDir/scripts/fppd_stop");
+} else {
+    exec($SUDO . " systemctl stop fppd");
+}
 
+touch("$mediaDirectory/tmp/fppd_restarted");
+?>
 ==========================================================================
-</pre>
+Rebuilding FPP...
+<?
+system("$fppDir/scripts/fpp_build");
+?>
+==========================================================================
+Restarting fppd...
+<?
+if (file_exists("/.dockerenv")) {
+    system($SUDO . " $fppDir/scripts/fppd_start");
+} else {
+    exec($SUDO . " systemctl restart fppd");
+}
+exec($SUDO . " rm -f /tmp/cache_*.cache");
+?>
+==========================================================================
+Rebuild Complete.
+<?
+if (!$wrapped) {
+?>
 <a href='index.php'>Go to FPP Main Status Page</a><br>
+<a href='about.php'>Go back to FPP About page</a><br>
+
 </body>
 </html>
+<?
+}
+?>
+
