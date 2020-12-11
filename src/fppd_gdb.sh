035af59190637273a21c6379c9b066875e6835ad
Chris Pinkham <cpinkham@bc2va.org>
2020-12-10 23:39:27 -0800

    Only log to stdout when using gdb shell script wrapper

--- src/fppd_gdb.sh
+++ src/fppd_gdb.sh
@@ -5,4 +5,4 @@ export FPPDIR=$(dirname $(pwd))
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${FPPDIR}/lib:${FPPDIR}/external/RF24
 
 
-gdb --args ${BINDIR}/fppd -f $*
+gdb --args ${BINDIR}/fppd -f -l stdout $*
