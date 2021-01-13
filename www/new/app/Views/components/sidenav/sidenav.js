12b70d8f337f0f3ad3f99837bc60c761387cd68d
Rick de Graaff <rick@lemon-internet.nl>
2021-01-13 20:16:45 +0100

    Responsive sidenav implemented

--- www/new/app/Views/components/sidenav/sidenav.js
+++ www/new/app/Views/components/sidenav/sidenav.js
@@ -1,10 +1,36 @@
 import {ComponentController} from 'assets/js/helpers/stimulus/component-controller';
+import {renderClasses} from "assets/js/helpers/bem";
 
 const NAME = 'sidenav';
 
+const CLASSES = {
+    'toggleButton': renderClasses(NAME, 'toggle'),
+    'toggleButtonClose': renderClasses(NAME, 'toggle', ['close']),
+    'pageSidenavContainer': renderClasses('page', 'sidenav'),
+    'pageSidenavContainerOpen': renderClasses('page', 'sidenav', ['open']),
+};
+
+const SELECTORS = {
+    'toggleButton': `.${CLASSES.toggleButton}`,
+    'pageSidenavContainer': `.${CLASSES.pageSidenavContainer}`,
+};
+
 class Sidenav extends ComponentController {
-    connect () {
-        console.log('Hello World!');
+    toggleMenu (evt) {
+        evt.preventDefault();
+        evt.stopPropagation();
+
+        // Add open class to page__sidenav
+        let toggleButton = document.querySelector(SELECTORS.toggleButton);
+        let pageSidenavContainer = document.querySelector(SELECTORS.pageSidenavContainer);
+
+        if (!toggleButton.classList.contains(CLASSES.toggleButtonClose)) {
+            pageSidenavContainer.classList.add(CLASSES.pageSidenavContainerOpen);
+            toggleButton.classList.add(CLASSES.toggleButtonClose);
+        } else {
+            pageSidenavContainer.classList.remove(CLASSES.pageSidenavContainerOpen);
+            toggleButton.classList.remove(CLASSES.toggleButtonClose);
+        }
     }
 }
 
