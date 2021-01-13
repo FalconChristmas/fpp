ce2abaa3a80e01c5a5e2083f6726dc24a9050678
Chris Pinkham <cpinkham@bc2va.org>
2021-01-13 09:59:04 -0800

    Fix duplication of multicast/sync input fields.
    - These are alread implemented as part of the command UI code
      so don't need to be duplicated by users of that code.
    
    Refs #941

--- www/commandEditor.php
+++ www/commandEditor.php
@@ -16,23 +16,18 @@ function CommandEditorSetup(target, data, callback, cancelCallback = '')
     $('#editorCommand').val(data['command']);
     CommandSelectChanged('editorCommand', 'tblCommandEditor');
 
-    if ((data.hasOwnProperty('multisyncCommand')) &&
-        (data['multisyncCommand'] == true)) {
-        $('#editorMultiSyncCommand').prop('checked', true);
-        $('#editorMultiSyncHostsRow').show();
-        $('#editorMultiSyncHosts').val(data['multisyncHosts']);
-    }
-
-    for (var i = 0; i < data['args'].length; i++) {
-        var inp =  $("#tblCommandEditor_arg_" + (i+1));
-        if (inp.attr('type') == 'checkbox') {
-            var checked = false;
-            if (data['args'][i] == "true" || data['args'][i] == "1") {
-                checked = true;
+    if (data.hasOwnProperty('args')) {
+        for (var i = 0; i < data['args'].length; i++) {
+            var inp =  $("#tblCommandEditor_arg_" + (i+1));
+            if (inp.attr('type') == 'checkbox') {
+                var checked = false;
+                if (data['args'][i] == "true" || data['args'][i] == "1") {
+                    checked = true;
+                }
+                inp.prop( "checked", checked);
+            } else {
+                inp.val(data['args'][i]).trigger('change');
             }
-            inp.prop( "checked", checked);
-        } else {
-            inp.val(data['args'][i]).trigger('change');
         }
     }
 }
@@ -46,13 +41,6 @@ function CommandEditorSave()
 {
     var data = {};
     data = CommandToJSON('editorCommand', 'tblCommandEditor', data);
-
-    if ($('#editorMultiSyncCommand').is(':checked')) {
-        data['multisyncCommand'] = true;
-        data['multisyncHosts'] = $('#editorMultiSyncHosts').val();
-    } else {
-        data['multisyncCommand'] = false;
-    }
 
     $('#commandEditorPopup').dialog("close");
 
@@ -69,29 +57,10 @@ function CommandEditorCancel()
         window[commandEditorCancelCallback](commandEditorTarget);
     }
 }
-
-function editorMultiSyncChanged() {
-    if ($('#editorMultiSyncCommand').is(':checked')) {
-        $('#editorMultiSyncHostsRow').show();
-    } else {
-        $('#editorMultiSyncHostsRow').hide();
-    }
-}
 </script>
 
 <table width="100%"  class="tblCommandEditor" id="tblCommandEditor">
     <tr><td>Command:</td><td><select id="editorCommand" onChange="CommandEditorCommandChanged();"></select></td></tr>
-    <tr><td>Multicast:</td><td><input type='checkbox' id='editorMultiSyncCommand' onChange='editorMultiSyncChanged(this);'></input></td></tr>
-    <tr id='editorMultiSyncHostsRow' style='display:none'><td>Multicast Hosts:</td><td><input style='width:100%;' type='text' id='editorMultiSyncHosts' list='multisyncHosts_list'></input>
-        <datalist id='multisyncHosts_list'>
-<?
-    $remotesStr = file_get_contents('http://localhost/api/remotes');
-    $remotes = json_decode($remotesStr, true);
-    foreach ($remotes as $k => $v) {
-        echo( "<option value='" . $k . "'>" . $v . "</option>\n");
-    }
-?>
-    </datalist></td></tr>
 </table>
 <hr>
 <center>
