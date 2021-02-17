edfd7300ea7efafcd3ed2773937db3b2456fde35
Chris Pinkham <cpinkham@bc2va.org>
2021-02-17 11:05:24 -0800

    Add Justin to UI credits

--- www/credits.php
+++ www/credits.php
@@ -34,6 +34,7 @@ $fpp_version = "v" . getFPPVersion();
         Greg Hormann (ghormann)<br/>
         Andrew Ayton (drew8n) - UI<br/>
         Adam Coulombe (wzadz1) - UI<br/>
+        Justin Novack - UI<br/>
         Pat Delaney (patdelaney) - QA<br/>
         Rick Harris (Poporacer) - Manual/QA<br/>
         Mark Amber (pixelpuppy) - Manual/QA<br/>
