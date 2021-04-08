d12db14fb61158399f166da7f384d232aa6e75d2
Adam Coulombe <adamcoulombe@gmail.com>
2021-04-07 22:27:00 -0400

    Remove `skipJSsettings=1` from backup.php
    not having js settings causes JS errors and breaks javascript on the page

--- www/backup.php
+++ www/backup.php
@@ -1,5 +1,4 @@
 <?php
-$skipJSsettings = 1;
 //error_reporting(E_ALL);
 //ini_set('display_errors', 1);
 
--- www/settings-time.php
+++ www/settings-time.php
@@ -6,7 +6,7 @@ require_once('common.php');
 <div class="settingsManagerTimeContent">
         <?
         $extraData = "<input type='button' class='buttons' value='Lookup Time Zone' onClick='GetTimeZone();'>";
-        $prependData = "<div class='row'><div class='col-md-5'><div class='description'><i class='fas fa-fw ui-level-0'></i>&nbsp;Current System Time</div></div><div id='currentTime' class='col-md-7 labelValue disabled'></div></div>";
+        $prependData = "<div class='row'><div class='col-md-4 col-lg-3 col-xxxl-2 align-top'><div class='description'><i class='fas fa-fw ui-level-0'></i>&nbsp;Current System Time</div></div><div id='currentTime' class='col-md labelValue disabled'></div></div>";
         PrintSettingGroup('time', $extraData, $prependData);
         ?>
 </div>
