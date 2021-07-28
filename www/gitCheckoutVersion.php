7547051c7fad855b5a56ce1743366b6a18c36ac3
Daniel Kulp <dan@kulp.com>
2021-07-28 13:29:55 -0400

    Hopefully fix www/gitCheckoutVersion.php

--- www/gitCheckoutVersion.php
+++ www/gitCheckoutVersion.php
@@ -1,22 +1,24 @@
 <?
-header( "Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: *");
 
 $wrapped = 0;
 
-if (isset($_GET['wrapped']))
+if (isset($_GET['wrapped'])) {
     $wrapped = 1;
+}
 
-if (!$wrapped)
+if (!$wrapped) {
     echo "<html>\n";
+}
 
 $skipJSsettings = 1;
-require_once("common.php");
+require_once "common.php";
 
 DisableOutputBuffering();
 
-$version = strip_tags(escapeshellcmd($_GET['version']))
+$version = strip_tags(escapeshellcmd($_GET['version']));
 if (!$wrapped) {
-?>
+    ?>
 <head>
 <title>
 Checkout specific Git version
@@ -24,7 +26,7 @@ Checkout specific Git version
 </head>
 <body>
 <h2>Checkout specific Git version</h2>
-Version: <? echo ($version); ?><br>
+Version: <?echo ($version); ?><br>
 <pre>
 <?
 } else {
@@ -41,7 +43,7 @@ system($SUDO . " $fppDir/scripts/git_checkout_version $version");
 ==========================================================================
 <?
 if (!$wrapped) {
-?>
+    ?>
 </pre>
 Switch complete, you may need to reload the page for changes to be visible.<br>
 <a href='index.php'>Go to FPP Main Status Page</a><br>
