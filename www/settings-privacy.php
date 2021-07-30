746e01c5eb168880955f7b1e682b154399599409
Poporacer <46263877+Poporacer@users.noreply.github.com>
2021-07-30 13:04:38 -0600

    Move Preview Statistics button to the Privacy Page

--- www/settings-privacy.php
+++ www/settings-privacy.php
@@ -2,6 +2,11 @@
 $skipJSsettings = 1;
 require_once('common.php');
 
-PrintSettingGroup('privacy');
+$extraData = "<input type='button' class='buttons' value='Preview Statistics' onClick='PreviewStatistics();'> ";
+PrintSettingGroup('privacy',$extraData);
 ?>
-
+<div>
+	Clicking the Preview Statistics button will show you the information that is getting reported</br>
+	to the developers so you can see what is being shared if you enable the Share Statistics option</br>
+	It is recommended to enable this option to help the developers improve FPP and its performance.
+</div>
