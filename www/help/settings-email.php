da9937dc46b06f98ec809b900630f39bdaa4e8ce
Chris Pinkham <cpinkham@bc2va.org>
2021-12-13 22:25:55 -0800

    Remove old note saying that gmail is the only mail server supported.

--- www/help/settings-email.php
+++ www/help/settings-email.php
@@ -1,10 +1,9 @@
 <center><b>Email Setup</b></center>
 <hr>
-Outbound email requires a Gmail account to relay mail thru.  Nothing is stored in/on your Gmail account.  Gmail accounts are free by going to <a href="https://gmail.com" target="new">Gmail.com</a>.  By routing thru Gmail we go around when ISPs block outbound port 25 making you route email thru them.<br /><br />
+Outbound email requires a SMTP account to relay mail thru.  Nothing is stored in/on your mail server.  You can normally use your internet provider's SMTP servers by providing the authentication information for your email account.  If you prefer, you can also use a mail service such as Gmail.  Gmail accounts are free by going to <a href="https://gmail.com" target="new">Gmail.com</a>.  By routing thru Gmail you can go around ISPs that block outbound port 25 forcing you to route email thru them.<br /><br />
 To send email from a script:<br />
 <ol>
 <li>Create your message to a temp file of your chosing</li>
-<li>Execute the mail utility:  mail -s "This is the subject line" root@localhost < tempfile.txt</li>
+<li>Execute the mail utility:  mail -s "This is the subject line" DESTINATION_EMAIL_ADDRESS < tempfile.txt</li>
 <li>If you need to attach a file also you can use the -a attachment_file param</li>
 </ol>
-All outbound email via scripts needs to be sent to <b>root@localhost</b>.
