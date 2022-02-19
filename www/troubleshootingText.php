ea8be39c90cb51caa2cd4e1a9dfe07d1f04442da
Greg Hormann <ghormann@gmail.com>
2022-02-18 21:08:41 -0500

    Fix troubleshootingText.php - related to #1242

--- www/troubleshootingText.php
+++ www/troubleshootingText.php
@@ -2,20 +2,29 @@
 
 $skipJSsettings = 1;
 
-require_once('troubleshootingCommands.php');
+require_once 'troubleshootingCommands.php';
+
+$results = array();
+
+foreach ($commands as $title => $command) {
+    $results[$command] = "Unknown";
+    exec($command . ' 2>&1 | fold -w 80 -s', $output, $return_val);
+    if ($return_val == 0) {
+        $results[$command] = implode("\n", $output) . "\n";
+    }
+
+    unset($output);
+}
 
 echo "Troubleshooting Commands:\n";
 
-foreach ($commands as $title => $command)
-{
-	echo "===================================================================\n";
-	echo "Title  : $title\n";
-	echo "Command: $command\n";
-	echo "-----------------------------------\n";
-	echo $results[$command];
-	echo "\n";
+foreach ($commands as $title => $command) {
+    echo "===================================================================\n";
+    echo "Title  : $title\n";
+    echo "Command: $command\n";
+    echo "-----------------------------------\n";
+    echo $results[$command];
+    echo "\n";
 }
 
 echo "===================================================================\n";
-
-?>
