e074858080b72064925a2d4b4fdc40ca8078840f
Daniel Kulp <dan@kulp.com>
2022-02-19 15:27:34 -0500

    Fix running fppd on bbb

--- src/makefiles/fppd.mk
+++ src/makefiles/fppd.mk
@@ -11,6 +11,10 @@ LDFLAGS_fppd += -rdynamic $(shell curl-config --libs) \
 TARGETS += fppd
 OBJECTS_ALL+=$(OBJECTS_fppd)
 
+ifneq '$(ARCH)' 'OSX'
+LIBS_fpp_so+=-Wl,-rpath=$(SRCDIR):$(SRCDIR)/../external/RF24/:.
+endif
+
 fppd: $(OBJECTS_fppd) libfpp.$(SHLIB_EXT)
 	$(CCACHE) $(CC) $(CFLAGS_$@) $(OBJECTS_$@) $(LIBS_$@) $(LDFLAGS) $(LDFLAGS_$@) -L . -l fpp $(LIBS_fpp_so) -o $@
 	$(ADJUST_PATHS_COMMAND)
