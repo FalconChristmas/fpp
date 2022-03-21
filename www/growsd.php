7594ad4f034cd12fa324fb9812c5962b53f833ba
Daniel Kulp <dan@kulp.com>
2022-03-21 14:53:26 -0400

    Maybe fix parition growing on BBB/Bullseye

--- www/growsd.php
+++ www/growsd.php
@@ -4,17 +4,25 @@ $skipJSsettings = 1;
 require_once("common.php");
 DisableOutputBuffering();
 
+$command2 = "";
 if ($settings['Platform'] == "BeagleBone Black") {
-    $command = "sudo /opt/fpp/SD/BBB-grow_partition.sh 2>&1";
+    if (file_exists("/usr/bin/bb-growpart")) {
+        $command = "sudo /usr/bin/bb-growpart 2>&1";
+        $command2 = "sudo systemctl enable enable resize_filesystem.service"
+    } else {
+        $command = "sudo /opt/fpp/SD/BBB-grow_partition.sh 2>&1";
+    }
 } else if ($settings['Platform'] == "Raspberry Pi") {
     $command = "sudo /usr/bin/raspi-config --expand-rootfs";
 }
 
-		echo "Command: $command\n";
-		echo "----------------------------------------------------------------------------------\n";
-		system($command);
-		echo "\n";
-
+echo "Command: $command\n";
+echo "----------------------------------------------------------------------------------\n";
+system($command);
+if ($command2 != "") {
+    system($command2);
+}
+echo "\n";
 ?>
 
 
