af4f98afb6c3872fc1fbc0d751a0a68436dbc6c4
Chris Pinkham <cpinkham@bc2va.org>
2022-06-07 09:21:04 -0700

    Don't show the 'expand storage' warning on Initial Setup page.

--- www/initialSetup.php
+++ www/initialSetup.php
@@ -30,18 +30,6 @@ include 'menu.inc'; ?>
     <div class="pageContent">
         <div id="initialSetup" class="">
 
-<?php
-if (isset($settings["LastBlock"]) && $settings["LastBlock"] > 1000000 && $settings["LastBlock"] < 7400000) {
-?>
-<div id='upgradeFlag' class="alert alert-danger" role="alert">
-     SD card has unused space.  Go to
-     <a href="settings.php?tab=Storage">Storage Settings</a> to expand the
-     file system or create a new storage partition.
-</div>
-
-<?php
-}
-?>
         <div id="warningsRow" class="alert alert-danger"><div id="warningsTd"><div id="warningsDiv"></div></div></div>
         <div class="row tablePageHeader">
             <div class="col-md">
