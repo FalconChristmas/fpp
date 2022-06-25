20ab4614d7d97b7c0aeb4c5b24ace93aaaffb617
Chris Pinkham <cpinkham@bc2va.org>
2022-06-25 15:13:13 -0700

    Clear the restartFlag setting when using fppd*.sh to run fppd
    These scripts are only for testing/development use normally, and
    I prefer not to use the setting that hides the restart/reboot
    warning so I can make sure patches work correctly.

--- src/fppd_gdb.sh
+++ src/fppd_gdb.sh
@@ -4,5 +4,6 @@ export BINDIR=$(dirname $(realpath $0))
 export FPPDIR=$(dirname $(pwd))
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${FPPDIR}/lib:${FPPDIR}/external/RF24
 
+sed -i -e "s/^restartFlag .*/restartFlag = \"0\"/" /home/fpp/media/settings
 
 gdb --args ${BINDIR}/fppd -f -l stdout $*
