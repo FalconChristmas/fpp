87ffb0a6709b911c02acd90a7300551f8ec8073d
Daniel Kulp <dan@kulp.com>
2022-07-29 09:19:52 -0400

    Fix crash handle to create the crashes directory if needed, don't create/upload crash reports if in crash loop, limit restart to 5 times in 60 seconds to prevent unlimitted restart crash

---
+++ upgrade/73/upgrade.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#####################################
+
+BINDIR=$(cd $(dirname $0) && pwd)
+. ${BINDIR}/../../scripts/common
+
+cp /opt/fpp/etc/systemd/fppd.service /lib/systemd/system
+
+systemctl daemon-reload
+systemctl reenable fppd
