605f360ef09feb9c0342172a875952a350b06d9b
Daniel Kulp <dan@kulp.com>
2022-10-11 21:48:44 -0400

    Add newer ubuntu release check

--- SD/FPP_Install.sh
+++ SD/FPP_Install.sh
@@ -345,7 +345,7 @@ cd /opt 2> /dev/null || mkdir /opt
 export DEBIAN_FRONTEND=noninteractive
 
 case "${OSVER}" in
-	debian_11 | debian_10 | ubuntu_20.04)
+	debian_11 | debian_10 | ubuntu_20.04 | ubuntu_22.04)
 		case $FPPPLATFORM in
 			'BeagleBone Black')
 				echo "FPP - Skipping non-free for $FPPPLATFORM"
@@ -1138,7 +1138,7 @@ sed -i -e "s/error\.log/apache2-base-error.log/" /etc/apache2/apache2.conf
 rm /etc/apache2/conf-enabled/other-vhosts-access-log.conf
 
 case "${OSVER}" in
-	debian_11 |  debian_10 | ununtu_20.04)
+	debian_11 |  debian_10 | ununtu_20.04 | ubuntu_22.04)
 		systemctl enable apache2.service
 		;;
 esac
