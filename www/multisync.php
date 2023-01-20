461ce22838441b9ad69852147ba8d5d33bec2845
Daniel Kulp <dan@kulp.com>
2023-01-20 17:48:54 -0500

    Fix "Upgrade FPP" for the local fpp instance

--- www/multisync.php
+++ www/multisync.php
@@ -790,8 +790,9 @@ if ((isset($settings['MultiSyncAdvancedView'])) &&
 
             hostRows[hostRowKey] = rowID;
 
+            var hnSpanStyle = "";
             if (data[i].local) {
-                hostname = "<b>" + hostname + "</b>";
+                hnSpanStyle = " style='font-weight:bold;'";
             } else {
                 if ((settings['MultiSyncEnabled'] == '1') &&
                     (settings['fppMode'] == 'player') &&
@@ -844,7 +845,7 @@ if ((isset($settings['MultiSyncAdvancedView'])) &&
                     ipTxt = "<small class='hostDescriptionSM'>Select IPs for Unicast Sync</small><br>" + ipTxt + star;
 
                 var newRow = "<tr id='" + rowID + "' ip='" + data[i].address + "' ipList='" + data[i].address + "' class='systemRow'>" +
-                    "<td class='hostnameColumn'><span id='fpp_" + ip.replace(/\./g,'_') + "_hostname'>" + hostname + "</span><br><small class='hostDescriptionSM' id='fpp_" + ip.replace(/\./g,'_') + "_desc'>"+ hostDescription +"</small></td>" +
+                    "<td class='hostnameColumn'><span id='fpp_" + ip.replace(/\./g,'_') + "_hostname'" +  hnSpanStyle +">" + hostname + "</span><br><small class='hostDescriptionSM' id='fpp_" + ip.replace(/\./g,'_') + "_desc'>"+ hostDescription +"</small></td>" +
                     "<td id='" + rowID + "_ip' ip='" + data[i].address + "'>" + ipTxt + "</td>" +
                     "<td><span id='" + rowID + "_platform'>" + data[i].type + "</span><br><small id='" + rowID + "_variant'>" + data[i].model + "</small><span class='hidden typeId'>" + data[i].typeId + "</span>"
                         + "<span class='hidden version'>" + data[i].version + "</span></td>" +
