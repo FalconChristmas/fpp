f1b263a24166fd684988165f8148adc3fd998af1
Scott Hanson <scooter_seh@yahoo.com>
2023-03-09 23:35:57 -0500

    working on uDMX output

---
+++ src/makefiles/libfpp-co-UDMX.mk
@@ -0,0 +1,9 @@
+OBJECTS_fpp_co_UDMX_so += channeloutput/uDMX.o
+LIBS_fpp_co_UDMX_so += -L. -lfpp -ljsoncpp -lusb-1.0
+
+TARGETS += libfpp-co-UDMX.$(SHLIB_EXT)
+OBJECTS_ALL+=$(OBJECTS_fpp_co_UDMX_so)
+
+libfpp-co-UDMX.$(SHLIB_EXT): $(OBJECTS_fpp_co_UDMX_so) libfpp.$(SHLIB_EXT)
+	$(CCACHE) $(CC) -shared $(CFLAGS_$@) $(OBJECTS_fpp_co_UDMX_so) $(LIBS_fpp_co_UDMX_so) $(LDFLAGS) $(LDFLAGS_fpp_co_UDMX_so) -o $@
+
