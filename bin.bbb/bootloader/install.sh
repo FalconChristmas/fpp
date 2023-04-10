b503de4bd27a2e4f27408fcece2f4c7af4299e06
Daniel Kulp <dan@kulp.com>
2023-04-10 08:34:45 -0400

    Update bootloader to 2022 version

--- bin.bbb/bootloader/install.sh
+++ bin.bbb/bootloader/install.sh
@@ -8,15 +8,15 @@ fi
 wdir="/opt/fpp/bin.bbb/bootloader"
 
 if [ -b /dev/mmcblk0 ] ; then
-	echo "dd if=${wdir}/MLO of=/dev/mmcblk0 count=1 seek=1 bs=128k"
-	dd if=${wdir}/MLO of=/dev/mmcblk0 count=1 seek=1 bs=128k
-	echo "dd if=${wdir}/u-boot.img of=/dev/mmcblk0 count=2 seek=1 bs=384k"
-	dd if=${wdir}/u-boot.img of=/dev/mmcblk0 count=2 seek=1 bs=384k
+	echo "dd if=${wdir}/MLO of=/dev/mmcblk0 count=2 seek=1 bs=128k"
+	dd if=${wdir}/MLO of=/dev/mmcblk0 count=2 seek=1 bs=128k
+	echo "dd if=${wdir}/u-boot-dtb.img of=/dev/mmcblk0 count=4 seek=1 bs=384k"
+	dd if=${wdir}/u-boot-dtb.img of=/dev/mmcblk0 count=4 seek=1 bs=384k
 fi
 
 if [ -b /dev/mmcblk1 ] ; then
-	echo "dd if=${wdir}/MLO of=/dev/mmcblk1 count=1 seek=1 bs=128k"
-	dd if=${wdir}/MLO of=/dev/mmcblk1 count=1 seek=1 bs=128k
-	echo "dd if=${wdir}/u-boot.img of=/dev/mmcblk1 count=2 seek=1 bs=384k"
-	dd if=${wdir}/u-boot.img of=/dev/mmcblk1 count=2 seek=1 bs=384k
+	echo "dd if=${wdir}/MLO of=/dev/mmcblk1 count=2 seek=1 bs=128k"
+	dd if=${wdir}/MLO of=/dev/mmcblk1 count=2 seek=1 bs=128k
+	echo "dd if=${wdir}/u-boot-dtb.img of=/dev/mmcblk1 count=4 seek=1 bs=384k"
+	dd if=${wdir}/u-boot-dtb.img of=/dev/mmcblk1 count=4 seek=1 bs=384k
 fi
