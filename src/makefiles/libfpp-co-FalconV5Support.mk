7c2f699421045f67316c0f5f5b0a899fca584f27
Daniel Kulp <dan@kulp.com>
2023-09-15 14:05:01 -0400

    Make sure zstd will overrite the file

--- src/makefiles/libfpp-co-FalconV5Support.mk
+++ src/makefiles/libfpp-co-FalconV5Support.mk
@@ -8,7 +8,7 @@ TARGETS += libFalconV5.$(SHLIB_EXT) libfpp-FalconV5Support.$(SHLIB_EXT)
 OBJECTS_ALL+=$(OBJECTS_fpp_FalconV5Support_so)
 
 libFalconV5.$(SHLIB_EXT): non-gpl/FalconV5Support/libFalconV5.$(SHLIB_EXT).zst
-	zstd -d non-gpl/FalconV5Support/libFalconV5.$(SHLIB_EXT).zst -o libFalconV5.$(SHLIB_EXT)
+	zstd -fd non-gpl/FalconV5Support/libFalconV5.$(SHLIB_EXT).zst -o libFalconV5.$(SHLIB_EXT)
 
 libfpp-FalconV5Support.$(SHLIB_EXT): $(OBJECTS_fpp_FalconV5Support_so) libfpp.$(SHLIB_EXT) libFalconV5.$(SHLIB_EXT)
 	$(CCACHE) $(CC) -shared $(CFLAGS_$@) $(OBJECTS_fpp_FalconV5Support_so) $(LIBS_fpp_FalconV5Support_so) $(LDFLAGS) $(LDFLAGS_fpp_FalconV5Support_so) -o $@
