6bb97653f4d3d6abe7eb31b99eee497f551404da
Daniel Kulp <dan@kulp.com>
2023-09-19 14:17:55 -0400

    fpermissive is not needed now that we compile C code with a C compiler

--- src/makefiles/libfpp-pi-gpio.mk
+++ src/makefiles/libfpp-pi-gpio.mk
@@ -6,9 +6,6 @@ LIBS_fpp_pi_gpio_so += -Wl,-rpath=$(SRCDIR):. -L. -ljsoncpp
 TARGETS += libfpp-pi-gpio.$(SHLIB_EXT)
 OBJECTS_ALL+=$(OBJECTS_fpp_pi_gpio_so)
 
-CFLAGS_util/bcm2835.o+=-fpermissive
-
-
 libfpp-pi-gpio.$(SHLIB_EXT): $(OBJECTS_fpp_pi_gpio_so) libfpp.$(SHLIB_EXT)
 	$(CCACHE) $(CC) -shared $(CFLAGS_$@) $(OBJECTS_fpp_pi_gpio_so) $(LIBS_fpp_pi_gpio_so) $(LDFLAGS) $(LDFLAGS_fpp_pi_gpio_so) -o $@
 
