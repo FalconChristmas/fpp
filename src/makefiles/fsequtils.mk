1faec2532ed30ad7fd6d9c97bc5431377c76b667
Daniel Kulp <dan@kulp.com>
2023-11-10 11:06:16 -0500

    macOS: add target to create a fsequtils zip

--- src/makefiles/fsequtils.mk
+++ src/makefiles/fsequtils.mk
@@ -6,10 +6,17 @@ OBJECTS_fsequtils = \
     fseq/FSEQUtils.o \
     fseq/FSEQFile.o
 
+ifeq '$(ARCH)' 'OSX'
+LIBS_fsequtils = \
+	-lcurl \
+	-ljsoncpp \
+    -lz $(HOMEBREW)/opt/zstd/lib/libzstd.a
+else
 LIBS_fsequtils = \
 	-lcurl \
 	-ljsoncpp \
     -lzstd -lz
+endif
 
 TARGETS += fsequtils
 OBJECTS_ALL+=$(OBJECTS_fsequtils)
@@ -19,3 +26,12 @@ fsequtils: $(OBJECTS_fsequtils) $(PCH_FILE)
 
 fseq/%.o: fseq/%.cpp fseq/%.h fppversion_defines.h Makefile makefiles/*.mk makefiles/platform/*.mk
 	$(CCACHE) $(CXXCOMPILER) $(CFLAGS) $(CXXFLAGS) $(CXXFLAGS_$@) -c $(SRCDIR)$< -o $@
+
+ifeq '$(ARCH)' 'OSX'
+OBJECTS_ALL+=libjsoncpp.25.dylib fsequtils.zip
+fsequtils.zip: fsequtils
+	cp $(HOMEBREW)/opt/jsoncpp/lib/libjsoncpp.25.dylib .
+	chmod +w libjsoncpp.25.dylib
+	install_name_tool -change $(HOMEBREW)/opt/jsoncpp/lib/libjsoncpp.25.dylib ./libjsoncpp.25.dylib fsequtils
+	zip -9 fsequtils.zip fsequtils libjsoncpp.25.dylib
+endif
