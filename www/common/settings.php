3642d4c01d34561020ecf5faaf7b7074e2c2fcde
Skymaster <55641392+mjunek@users.noreply.github.com>
2023-12-06 21:31:01 +1100

    Use FalconPlayer NTP Pool
    Remove default Debian pool and use the custom one assigned to the project. Sets this when NTP server is cleared and on initial installation.

--- www/common/settings.php
+++ www/common/settings.php
@@ -62,7 +62,8 @@ function SetNTPServer($value)
     if ($value != '') {
         exec("sudo sed -i '/^server.*/d' /etc/ntp.conf ; sudo sed -i '/^pool.*/d' /etc/ntp.conf ; sudo sed -i '\$s/\$/\\nserver $value iburst/' /etc/ntp.conf");
     } else {
-        exec("sudo sed -i '/^server.*/d' /etc/ntp.conf ; sudo sed -i '/^pool.*/d' /etc/ntp.conf ; sudo sed -i '\$s/\$/\\npool 0.debian.pool.ntp.org iburst\\npool 1.debian.pool.ntp.org iburst\\npool 2.debian.pool.ntp.org iburst\\npool 3.debian.pool.ntp.org iburst\\n/' /etc/ntp.conf");
+		// Updated as we should be using our own zone falconplayer.pool.ntp.org
+        exec("sudo sed -i '/^server.*/d' /etc/ntp.conf ; sudo sed -i '/^pool.*/d' /etc/ntp.conf ; sudo sed -i '\$s/\$/\\npool falconplayer.pool.ntp.org iburst minpoll 8 maxpoll 12 prefer/' /etc/ntp.conf");
     }
 
     // Note: Assume NTP is always enabled now.
