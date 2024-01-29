FalconChristmas/fpp

commit 01efb505483ca0f13337eb8af448dfb19ea7e0e7

fix persistentNames function spelling so devs can find the linkages in the code base

www/api/index.php
@@ -86,8 +86,10 @@ dispatch_get('/network/interface/add/:interface', 'network_add_interface');
 dispatch_post('/network/interface/:interface', 'network_set_interface');
 dispatch_post('/network/interface/:interface/apply', 'network_apply_interface');
 
-dispatch_delete('/network/presisentNames', 'network_presisentNames_delete');
-dispatch_post('/network/presisentNames', 'network_presisentNames_create');
+dispatch_delete('/network/presisentNames', 'network_persistentNames_delete');
+dispatch_post('/network/presisentNames', 'network_persistentNames_create');
+dispatch_delete('/network/persistentNames', 'network_presisentNames_delete');
+dispatch_post('/network/persistentNames', 'network_presisentNames_create');
 dispatch_get('/network/wifi/scan/:interface', 'network_wifi_scan');
 dispatch_get('/network/wifi/strength', 'network_wifi_strength');
 dispatch_get('/network/wifi_strength', 'network_wifi_strength'); // Legacy mapping

www/api/controllers/network.php
@@ -58,7 +58,7 @@ function network_wifi_scan()
     return json(array("status" => "OK", "networks" => $networks));
 }
 
-function network_presisentNames_delete()
+function network_persistentNames_delete()
 {
     shell_exec("sudo rm -f /etc/systemd/network/5?-fpp-*.link");
     shell_exec("sudo ln -sf /dev/null /etc/systemd/network/99-default.link");
@@ -68,10 +68,10 @@ function network_presisentNames_delete()
     return json($output);
 }
 
-function network_presisentNames_create()
+function network_persistentNames_create()
 {
     global $settings;
-    network_presisentNames_delete();
+    network_persistentNames_delete();
 
     $interfaces = network_list_interfaces_array();
     $count = 0;

www/networkconfig.php
@@ -325,7 +325,7 @@ function CreatePersistentNames() {
 			"Yes" : {class:'btn-success',click:function() {
                     CloseModalDialog("createPersistentDialog");
                     SetRebootFlag();
-                    $.post("api/network/presisentNames", "", function() {location.reload(true);});
+                    $.post("api/network/persistentNames", "", function() {location.reload(true);});
 				}},
 			"No" : {click:function() {
                     CloseModalDialog("createPersistentDialog");
@@ -347,7 +347,7 @@ function ClearPersistentNames() {
                     SetRebootFlag();
                     $.ajax( {
                         type: "DELETE",
-                        url: "api/network/presisentNames",
+                        url: "api/network/persistentNames",
                         data: "",
                         success: function() {location.reload(true);},
                         });

commit 63454ffd6df7966fb278af776c3f05fde59bd758

Fix persistent names problem introduced in commit 01efb505483ca0f13337eb8af448dfb19ea7e0e7

www/api/index.php
@@ -88,8 +88,8 @@ dispatch_post('/network/interface/:interface/apply', 'network_apply_interface');
 
 dispatch_delete('/network/presisentNames', 'network_persistentNames_delete');
 dispatch_post('/network/presisentNames', 'network_persistentNames_create');
-dispatch_delete('/network/persistentNames', 'network_presisentNames_delete');
-dispatch_post('/network/persistentNames', 'network_presisentNames_create');
+dispatch_delete('/network/persistentNames', 'network_persistentNames_delete');
+dispatch_post('/network/persistentNames', 'network_persistentNames_create');
 dispatch_get('/network/wifi/scan/:interface', 'network_wifi_scan');
 dispatch_get('/network/wifi/strength', 'network_wifi_strength');
 dispatch_get('/network/wifi_strength', 'network_wifi_strength'); // Legacy mapping