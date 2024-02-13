a98c7fb9a1d5aaff68a7c0251bc82ab951b66584
Daniel Kulp <dan@kulp.com>
2024-02-13 09:28:30 -0500

    If RTC fails to be setup (configured wrong, cape removed, etc..) remove the device to not spam sys log with errors

--- src/boot/FPPRTC.cpp
+++ src/boot/FPPRTC.cpp
@@ -65,14 +65,26 @@ static void exec(const std::string& cmd) {
         printf(buffer.data());
     }
 }
+static std::string execAndReturn(const std::string& cmd) {
+    std::array<char, 128> buffer;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"), pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    std::string ret;
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        ret += std::string(buffer.data());
+    }
+    return ret;
+}
 
 static void modprobe(const char* mod) {
     std::string cmd = std::string("/usr/sbin/modprobe ") + mod;
     exec(cmd);
 }
-static void hwclock(const char* action, const char* device) {
-    std::string cmd = std::string("/usr/sbin/hwclock ") + action + " -f " + device;
-    exec(cmd);
+static std::string hwclock(const char* action, const char* device) {
+    std::string cmd = std::string("/usr/sbin/hwclock ") + action + " -f " + device + " 2>&1";
+    return execAndReturn(cmd);
 }
 static void addI2CDevice(const char* dev, int bus) {
     char buf[256];
@@ -83,36 +95,63 @@ static void addI2CDevice(const char* dev, int bus) {
         fclose(fd);
     }
 }
-static bool setupRTC() {
+static void removeI2CDevice(const std::string& dev, int bus) {
+    if (!dev.empty()) {
+        printf("Failed to setup RTC, removing device\n");
+        char buf[256];
+        snprintf(buf, sizeof(buf), "/sys/bus/i2c/devices/i2c-%d/delete_device", bus);
+        FILE* fd = fopen(buf, "w");
+        if (fd != nullptr) {
+            fwrite(dev.c_str(), dev.size(), 1, fd);
+            fclose(fd);
+        }
+    }
+}
+
+static bool setupRTC(std::string& devAd, bool newDev) {
     int ledType = getSettingInt();
 
     int bus = getBus();
-
     switch (ledType) {
     case 1:
         printf("FPP - Configuring RTC, Setting to rasClock/%d\n", bus);
         modprobe("rtc-pcf2127");
-        addI2CDevice("pcf2127 0x51", bus);
+        devAd = "0x51";
+        if (newDev) {
+            addI2CDevice("pcf2127 0x51", bus);
+        }
         break;
     case 2:
         printf("FPP - Configuring RTC, Setting to DS1307/%d\n", bus);
         modprobe("rtc-ds1307");
-        addI2CDevice("ds1307 0x68", bus);
+        devAd = "0x68";
+        if (newDev) {
+            addI2CDevice("ds1307 0x68", bus);
+        }
         break;
     case 3:
         printf("FPP - Configuring RTC, Setting to mcp7941x/%d\n", bus);
         // modprobe("");
-        addI2CDevice("mcp7941x 0x6f", bus);
+        devAd = "0x6f";
+        if (newDev) {
+            addI2CDevice("mcp7941x 0x6f", bus);
+        }
         break;
     case 4:
         printf("FPP - Configuring RTC, Setting to pcf8523/%d\n", bus);
         modprobe("rtc-pcf8523");
-        addI2CDevice("pcf8523 0x68", bus);
+        devAd = "0x68";
+        if (newDev) {
+            addI2CDevice("pcf8523 0x68", bus);
+        }
         break;
     case 5:
         printf("FPP - Configuring RTC, Setting to pcf85363/%d\n", bus);
         modprobe("rtc-pcf85363");
-        addI2CDevice("pcf85363 0x51", bus);
+        devAd = "0x51";
+        if (newDev) {
+            addI2CDevice("pcf85363 0x51", bus);
+        }
         break;
     default:
         printf("FPP - Configuring RTC, None Selected");
@@ -150,10 +189,9 @@ static std::string getRTCDev() {
 
 int main(int argc, char* argv[]) {
     std::string rtc = getRTCDev();
-    if (!FileExists(rtc)) {
-        if (!setupRTC()) {
-            return 0;
-        }
+    std::string dev;
+    if (!setupRTC(dev, !FileExists(rtc))) {
+        return 0;
     }
     int count = 0;
     while (!FileExists(rtc) && count < 100) {
@@ -167,15 +205,22 @@ int main(int argc, char* argv[]) {
         if (action == "set") {
             hwclock("-w", rtc.c_str());
         } else {
-            hwclock("-s", rtc.c_str());
+            std::string ret = hwclock("-s", rtc.c_str());
+            // printf("%s\n", ret.c_str());
+            if (contains(ret, "Remote I/O error")) {
+                removeI2CDevice(dev, getBus());
+            } else {
 #ifdef PLATFORM_BBB
-            // set the built in rtc to the same time as read from the RTC
-            if (rtc != "/dev/rtc0") {
-                hwclock("-w", rtc.c_str());
-            }
+                // set the built in rtc to the same time as read from the RTC
+                if (rtc != "/dev/rtc0") {
+                    hwclock("-w", rtc.c_str());
+                }
 #endif
-            exec("cp /opt/fpp/etc/update-RTC /etc/cron.daily/");
+                exec("cp /opt/fpp/etc/update-RTC /etc/cron.daily/");
+            }
         }
+    } else if (!dev.empty()) {
+        removeI2CDevice(dev, getBus());
     }
     auto t = std::time(nullptr);
     auto tm = *std::localtime(&t);
