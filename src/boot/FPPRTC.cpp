8b2359668a140d7fb63e05fbb769a36c041222bf
Daniel Kulp <dan@kulp.com>
2024-02-13 16:12:04 -0500

    Wrong license header

--- src/boot/FPPRTC.cpp
+++ src/boot/FPPRTC.cpp
@@ -1,14 +1,13 @@
 /*
  * This file is part of the Falcon Player (FPP) and is Copyright (C)
- * 2013-2022 by the Falcon Player Developers.
+ * 2013-2024 by the Falcon Player Developers.
  *
  * The Falcon Player (FPP) is free software, and is covered under
  * multiple Open Source licenses.  Please see the included 'LICENSES'
  * file for descriptions of what files are covered by each license.
  *
- * This source file is covered under the CC-BY-ND as described in the
- * included LICENSE.CC-BY-ND file.  This file may be modified for
- * personal use, but modified copies MAY NOT be redistributed in any form.
+ * This source file is covered under the LGPL v2.1 as described in the
+ * included LICENSE.LGPL file.
  */
 
 #include <sys/file.h>
