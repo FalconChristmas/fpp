f2871c181b738f3d7997f17660e94da89a7e17d1
darylc <daryl@htb.com.au>
2024-02-17 12:25:04 +1030

    bring back syslog file

--- SD/FPP_Install.sh
+++ SD/FPP_Install.sh
@@ -490,7 +490,7 @@ case "${OSVER}" in
                       gettext apt-utils x265 libtheora-dev libvorbis-dev libx265-dev iputils-ping mp3gain \
                       libmosquitto-dev mosquitto-clients mosquitto libzstd-dev lzma zstd gpiod libgpiod-dev libjsoncpp-dev libcurl4-openssl-dev \
                       fonts-freefont-ttf flex bison pkg-config libasound2-dev mesa-common-dev qrencode libusb-1.0-0-dev \
-                      flex bison pkg-config libasound2-dev python3-distutils libssl-dev libtool bsdextrautils iw"
+                      flex bison pkg-config libasound2-dev python3-distutils libssl-dev libtool bsdextrautils iw rsyslog"
 
         if [ "$FPPPLATFORM" == "Raspberry Pi" -o "$FPPPLATFORM" == "BeagleBone Black" ]; then
             PACKAGE_LIST="$PACKAGE_LIST firmware-realtek firmware-atheros firmware-ralink firmware-brcm80211 firmware-iwlwifi firmware-libertas firmware-zd1211 firmware-ti-connectivity zram-tools"
@@ -1097,6 +1097,7 @@ echo "FPP - Configuring log rotation"
 cp /opt/fpp/etc/logrotate.d/* /etc/logrotate.d/
 sed -i -e "s/#compress/compress/" /etc/logrotate.conf
 sed -i -e "s/rotate .*/rotate 2/" /etc/logrotate.conf
+sed -i -e "s/weekly/daily/" /etc/logrotate.d/rsyslog
 
 #######################################
 # Configure ccache
