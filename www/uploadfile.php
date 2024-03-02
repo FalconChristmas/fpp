53683f4e3c9f64df52ec1bec5adbd4f8a1f5d600
OnlineDynamic <stuart_ledingham@hotmail.com>
2024-03-02 12:12:45 -0500

    video duration title

--- www/uploadfile.php
+++ www/uploadfile.php
@@ -655,7 +655,7 @@ include 'menu.inc';?>
                   <table id="tblVideos" class="tablesorter">
                     <thead>
                         <th>File</th>
-                        <th>Size</th>
+                        <th>Duration</th>
                         <th>Date Modified</th>
                     </thead>
                     <tbody>
@@ -861,6 +861,45 @@ include 'menu.inc';?>
   </div>
 </div>
 
+<script src="jquery/jquery.tablesorter/jquery.tablesorter.js"></script>
+<script src="jquery/jquery.tablesorter/jquery.tablesorter.widgets.js"></script>
+<script>
+
+$(function(){
+    $("#tblVideos").tablesorter({widgets: ['zebra']});
+    $.extend($.tablesorter.themes.bootstrap, {
+      // table classes
+      table: 'table table-bordered table-striped',
+      caption: 'caption',
+      // *** header class names ***
+      // header classes
+      header: 'bootstrap-header',
+      sortNone: '',
+      sortAsc: '',
+      sortDesc: '',
+      // applied when column is sorted
+      active: '',
+      // hover class
+      hover: '',
+      // *** icon class names ***
+      // icon class added to the <i> in the header
+      icons: '',
+      // class name added to icon when column is not sorted
+      iconSortNone: 'bootstrap-icon-unsorted',
+      // class name added to icon when column has ascending sort
+      iconSortAsc: 'icon-chevron-up glyphicon glyphicon-chevron-up sort-asc',
+      // class name added to icon when column has descending sort
+      iconSortDesc: 'icon-chevron-down glyphicon glyphicon-chevron-down',
+      filterRow: '',
+      footerRow: '',
+      footerCells: '',
+      // even row zebra striping
+      even: '',
+      // odd row zebra striping
+      odd: ''
+  });
+  });
+  </script>
 
 <?php	include 'common/footer.inc';?>
 <script>
