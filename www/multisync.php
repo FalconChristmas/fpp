ab18abbe42fde64e85c685a8fe5f1bad36db98a7
Stuart L <68251154+OnlineDynamic@users.noreply.github.com>
2024-03-14 23:09:18 +0000

    Update multisync.php to fix warning msgs on filter (#1821)
    Closes #1819

--- www/multisync.php
+++ www/multisync.php
@@ -191,10 +191,15 @@ include 'common/menuHead.inc';
         // check if Warnings window should be shown
         $(document).find('tr[id*="_warnings"').each(function(){
             let cnt = $(this).find('td[id*="_warningCell"').children().length;
-            if (cnt ==0) {
-                $(this).hide();
-            } else {
-                $(this).show();
+            if (cnt ==0) { //no warnings
+               $(this).addClass('filtered');
+
+            } else { //has warning messages
+                if ($(this).hasClass('filtered')) {
+                    $(this).find('td[id*="_warningCell"').addClass('filtered');
+                } else{
+                    $(this).find('td[id*="_warningCell"').removeClass('filtered');
+                }
             }
         });
 
