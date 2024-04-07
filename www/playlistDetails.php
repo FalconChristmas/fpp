c57ea7c1c0ff6bab35996eece0dd24f778eab930
OnlineDynamic <stuart_ledingham@hotmail.com>
2024-04-07 21:25:38 +0100

    remove invalid width attribute

--- www/playlistDetails.php
+++ www/playlistDetails.php
@@ -1,16 +1,16 @@
 <?
 $simplifiedPlaylist = 0;
 ?>
-<?  if (!$simplifiedPlaylist) { ?>
-<? } ?>
+<?if (!$simplifiedPlaylist) {?>
+<?}?>
 <div id="playlistDetails"  class="unselectable">
     <div id= "playlistDetailsContents">
-        <div id="tblPlaylistDetails" width="100%">
+        <div id="tblPlaylistDetails">
             <div>
                 <div class='colPlaylistNumber'></div>
                 <div class='colPlaylistData1'></div>
             </div>
-<?  if (!$simplifiedPlaylist) { ?>
+<?if (!$simplifiedPlaylist) {?>
             <div class="tblPlaylistLeadInContainer tblPlaylistContainer">
                 <div id='tblPlaylistLeadInHeader' style='display: none;' class='tblPlaylistDetailsHeader'>
                     <div>
@@ -32,7 +32,7 @@ $simplifiedPlaylist = 0;
                 </table>
                 </div>
             </div>
-<? } ?>
+<?}?>
             <div class="tblPlaylisttMainPlaylistContainer tblPlaylistContainer">
                 <div id='tblPlaylistMainPlaylistHeader' style='display: none;' class='tblPlaylistDetailsHeader'>
                     <div>
@@ -62,10 +62,10 @@ $simplifiedPlaylist = 0;
                     </table>
                 </div>
             </div>
-<?  if (!$simplifiedPlaylist) { ?>
+<?if (!$simplifiedPlaylist) {?>
             <div class="tblPlaylistLeadOutContainer tblPlaylistContainer">
                 <div id='tblPlaylistLeadOutHeader' style='display: none;' class='tblPlaylistDetailsHeader'>
-                    <div><h2>Lead Out</h2> 
+                    <div><h2>Lead Out</h2>
                         <div class="tblPlaylistHeaderDetails">
                             <div>
                                 <div class="labelHeading">Items</div>
@@ -79,7 +79,7 @@ $simplifiedPlaylist = 0;
                                  <span class='playlistDurationLeadOut'>00m:00s</span>
                                 </div>
                             </div>
-                            
+
                         </div>
                     </div>
                 </div>
@@ -88,7 +88,7 @@ $simplifiedPlaylist = 0;
                 </table>
                 </div>
             </div>
-<? } ?>
+<?}?>
         </div>
     </div>
 </div>
