3005caa48f19f566c9b7f05ee83a79fca471aa6a
Daniel Kulp <dan@kulp.com>
2024-06-15 13:16:47 -0400

    Make sure capeutils is built/linked before PCA9685

--- src/makefiles/libfpp-co-PCA9685.mk
+++ src/makefiles/libfpp-co-PCA9685.mk
@@ -4,6 +4,6 @@ LIBS_fpp_co_PCA9685_so=-L. -lfpp -ljsoncpp -lfpp_capeutils  -Wl,-rpath=$(SRCDIR)
 TARGETS += libfpp-co-PCA9685.$(SHLIB_EXT)
 OBJECTS_ALL+=$(OBJECTS_fpp_co_PCA9685_so)
 
-libfpp-co-PCA9685.$(SHLIB_EXT): $(OBJECTS_fpp_co_PCA9685_so) libfpp.$(SHLIB_EXT)
+libfpp-co-PCA9685.$(SHLIB_EXT): $(OBJECTS_fpp_co_PCA9685_so) libfpp.$(SHLIB_EXT) libfpp_capeutils.$(SHLIB_EXT) 
 	$(CCACHE) $(CC) -shared $(CFLAGS_$@) $(OBJECTS_fpp_co_PCA9685_so) $(LIBS_fpp_co_PCA9685_so) $(LDFLAGS) $(LDFLAGS_fpp_co_PCA9685_so) -o $@
 
