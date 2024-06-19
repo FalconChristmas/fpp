dc4b7566831c670300d8dc8a20722a703f6c1958
OnlineDynamic <stuart_ledingham@hotmail.com>
2024-06-19 02:08:11 +0100

    Add a check to compare fpp system time against the browser time

--- www/healthCheckHelper.php
+++ www/healthCheckHelper.php
@@ -1,10 +1,10 @@
 <?
-header( "Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: *");
 
 $skipJSsettings = 1;
-require_once("common.php");
+require_once ("common.php");
 
 DisableOutputBuffering();
 
-system($settings["fppDir"] . "/scripts/healthCheck --php");
+system($settings["fppDir"] . "/scripts/healthCheck --php " . $_GET['timestamp']);
 ?>
