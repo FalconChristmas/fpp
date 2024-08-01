4c40f3916face8ad3b40bba474f4480c3fb65898
OnlineDynamic <stuart_ledingham@hotmail.com>
2024-08-01 18:50:58 +0100

    fix links to storage settings tab

--- www/healthCheck.php
+++ www/healthCheck.php
@@ -49,7 +49,7 @@
                         ?>
                         <div id='upgradeFlag' class="alert alert-danger" role="alert">
                             SD card has unused space. Go to
-                            <a href="settings.php?tab=Storage">Storage Settings</a> to expand the
+                            <a href="settings.php#settings-storage">Storage Settings</a> to expand the
                             file system or create a new storage partition.
                         </div>
 
