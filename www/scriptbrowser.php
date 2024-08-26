971ebe2c9fc87f0cc8c0ea2811ccdfa3d28dadc9
darylc <daryl@htb.com.au>
2024-08-26 09:43:21 +0100

    Show incompatible scripts in Advanced mode

--- www/scriptbrowser.php
+++ www/scriptbrowser.php
@@ -7,6 +7,7 @@ include 'common/menuHead.inc';
 
 function normalize_version($version)
 {
+    // convert a version string like 2.7.1-2-dirty to "20701"
     $version = preg_replace('/[v]/', '', $version);
     $version = preg_replace('/-.*/', '', $version);
     $parts = explode('.', $version);
@@ -25,12 +26,10 @@ function normalize_version($version)
 }
 
 $rfs_ver = normalize_version(getFPPVersionTriplet());
-
-// Debug: Check uiLevel
-echo "<!-- uiLevel: " . $settings["uiLevel"] . " -->";
 ?>
-<title><?= $pageTitle; ?></title>
+<title><?echo $pageTitle; ?></title>
 <script>
+
     function ViewRemoteScript(category, filename) {
         DoModalDialog({
             id: "ScriptViewerDialog",
@@ -40,34 +39,30 @@ echo "<!-- uiLevel: " . $settings["uiLevel"] . " -->";
             backdrop: true,
             keyboard: true,
             buttons: {
-                "Close": function() { CloseModalDialog("ScriptViewerDialog"); }
+                "Close": function() {CloseModalDialog("ScriptViewerDialog");}
             }
         });
 
-        $.get("api/scripts/viewRemote/" + encodeURIComponent(category) + "/" + encodeURIComponent(filename))
-            .done(function(data) {
+        $.get("api/scripts/viewRemote/" + encodeURIComponent(category) + "/" + encodeURIComponent(filename)
+        ).done(function(data) {
                 $('#scriptViewerText').html("<pre>" + data.replace(/</g, '&lt;').replace(/>/g, '&gt;') + "</pre>");
-            })
-            .fail(function() {
+            }).fail(function() {
                 $('#scriptViewerText').html("Error loading script contents from repository.");
             });
     }
 
     function InstallRemoteScript(category, filename) {
-        $.get("api/scripts/installRemote/" + encodeURIComponent(category) + "/" + encodeURIComponent(filename))
-            .done(function() {
-                $.jGrowl("Script installed.", { themeState: 'success' });
-            })
-            .fail(function() {
+        $.get("api/scripts/installRemote/" + encodeURIComponent(category) + "/" + encodeURIComponent(filename)
+            ).done(function() {
+                $.jGrowl("Script installed.",{themeState:'success'});
+            }).fail(function() {
                 DialogError("Install Script", "Install Failed");
             });
     }
 
     function filterScripts(searchTxt){
         var value = searchTxt.toLowerCase();
-        $('#fppScripts .fppScriptEntry').filter(function() {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
-        });
+        $('#fppScripts .fppScriptEntry').filter(function(){$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)})
     }
 
     function pageSpecific_PageLoad_PostDOMLoad_ActionsSetup(){
@@ -75,18 +70,19 @@ echo "<!-- uiLevel: " . $settings["uiLevel"] . " -->";
             filterScripts($(this).val());
         });
     }
+
 </script>
 </head>
 <body>
 <div id="bodyWrapper">
     <?php
     $activeParentMenuItem = 'content';
-    include 'menu.inc';
-    ?>
+    include 'menu.inc';?>
     <div class="mainContainer">
         <h1 class="title">Script Repository</h1>
         <div class="pageContent">
             <div id="uiscripts" class="settings">
+
                 <div class="row fppScriptSearch">
                     <div class="col">
                         <input type="text" id="ScriptSearchInput" class="form-control form-control-lg form-control-rounded has-shadow" placeholder="Search for a script..." />
@@ -94,6 +90,7 @@ echo "<!-- uiLevel: " . $settings["uiLevel"] . " -->";
                 </div>
 
                 <div id='fppScripts'>
+
                     <?
                     $indexCSV = file_get_contents("https://raw.githubusercontent.com/FalconChristmas/fpp-scripts/master/index.csv");
                     $lines = explode("\n", $indexCSV);
@@ -120,30 +117,40 @@ echo "<!-- uiLevel: " . $settings["uiLevel"] . " -->";
                             $nonMatchingScripts[] = $parts;
                             continue;
                         }
+
                     ?>
+
                         <div class="backdrop mb-2 fppScriptEntry">
                             <div class="row">
+
+
                                 <div class="col-lg-3 fppScriptEntryCol">
                                     <div class="labelHeading">Filename:</div>
-                                    <h3><?= $parts[1]; ?></h3>
+
+                                    <h3><?echo $parts[1]; ?></h3>
+
+
+
                                 </div>
-                                <div class="col-lg-1 fppScriptEntryCol text-muted">
+                                <div class="col-lg-1  fppScriptEntryCol text-muted">
                                     <div class="labelHeading">Category:</div>
-                                    <?= $parts[0]; ?>
+                                    <?echo $parts[0]; ?>
                                 </div>
-                                <div class="col-lg fppScriptEntryCol text-muted">
+                                <div class="col-lg  fppScriptEntryCol text-muted">
                                     <div class="labelHeading">Description:</div>
-                                    <?= $parts[2]; ?>
+                                    <?echo $parts[2]; ?>
                                 </div>
                                 <div class="col-lg-auto fppScriptEntryCol">
-                                    <input type='button' class='buttons' value='View' onClick='ViewRemoteScript("<?= $parts[0]; ?>", "<?= $parts[1]; ?>");'>
-                                    <input type='button' class='buttons btn-success' value='Install' onClick='InstallRemoteScript("<?= $parts[0]; ?>", "<?= $parts[1]; ?>");'>
+                                    <input type='button' class='buttons' value='View' onClick='ViewRemoteScript("<?echo $parts[0]; ?>", "<?echo $parts[1]; ?>");'>
+                                    <input type='button' class='buttons btn-success' value='Install' onClick='InstallRemoteScript("<?echo $parts[0]; ?>", "<?echo $parts[1]; ?>");'>
                                 </div>
                             </div>
+
                         </div>
                     <?
-                    }
+}
                     ?>
+
                 </div>
 
                 <? if ($settings["uiLevel"] >= 1 && !empty($nonMatchingScripts)) : ?>
@@ -153,7 +160,7 @@ echo "<!-- uiLevel: " . $settings["uiLevel"] . " -->";
 
                     <h2>Incompatible Scripts</h2>
                     <div class="callout">
-                        The scripts below are from other versions of FPP and are known not to function with this version of FPP. They are listed here for your reference only.
+                        The scripts below are from other versions of FPP and are known not to function with this version of FPP. They are listed here for your reference only.  Many of them can be replaced by FPP command functionality for instance.
                     </div>
                     <div id='fppScriptsNonMatching'>
                         <? foreach ($nonMatchingScripts as $parts) : ?>
@@ -182,8 +189,8 @@ echo "<!-- uiLevel: " . $settings["uiLevel"] . " -->";
             </div>
         </div>
     </div>
-    <? include 'common/footer.inc'; ?>
+    <?php include 'common/footer.inc';?>
 </div>
+
 </body>
 </html>
-
