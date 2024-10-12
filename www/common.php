1cd2fc9d4a7fbec096e8fd71b3d4b215e21b6d39
OnlineDynamic <stuart_ledingham@hotmail.com>
2024-10-12 12:21:56 +0100

    add a json_validate function def for php <8.3

--- www/common.php
+++ www/common.php
@@ -118,6 +118,29 @@ function ReadSettingFromFile($settingName, $plugin = "")
     }
 }
 
+if (!function_exists('json_validate')) {
+    /**
+     * Validates a JSON string. This is standard in php >= 8.3
+     * 
+     * @param string $json The JSON string to validate.
+     * @param int $depth Maximum depth. Must be greater than zero.
+     * @param int $flags Bitmask of JSON decode options.
+     * @return bool Returns true if the string is a valid JSON, otherwise false.
+     */
+    function json_validate($json, $depth = 512, $flags = 0)
+    {
+        if (!is_string($json)) {
+            return false;
+        }
+
+        try {
+            json_decode($json, false, $depth, $flags | JSON_THROW_ON_ERROR);
+            return true;
+        } catch (\JsonException $e) {
+            return false;
+        }
+    }
+}
 function WriteSettingToFile($settingName, $setting, $plugin = "")
 {
     global $settingsFile;
