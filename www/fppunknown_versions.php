0b080ba094a163a3ca4258279a05e8e0cb06352f
OnlineDynamic <stuart_ledingham@hotmail.com>
2025-01-13 14:19:19 +0000

    fixes to display UI correct after make clean when fppversion unknown

--- www/fppunknown_versions.php
+++ www/fppunknown_versions.php
@@ -1,28 +1,53 @@
 <?php
 
+function getFPPVersion()
+{
+    return "Unknown";
+}
+function getFPPBranch()
+{
+    return "Unknown";
+}
+function getFPPVersionFloat()
+{
+    return 99.999;
+}
+function getFPPVersionFloatStr()
+{
+    return "99.999";
+}
+function getFPPMajorVersion()
+{
+    return "99";
+}
+function getFPPMinorVersion()
+{
+    return "999";
+}
+function getFPPPatchVersion()
+{
+    return "";
+}
+function getFPPVersionTriplet()
+{
+    return "99.999";
+}
+
+function writeFPPVersionJavascriptFunctions()
+{
+    ?>
+
     function getFPPVersion() { return "Unknown"; }
     function getFPPBranch() { return "Unknown"; }
     function getFPPVersionFloat() { return 99.999; }
     function getFPPVersionFloatStr() { return "99.999"; }
     function getFPPMajorVersion() { return "99"; }
     function getFPPMinorVersion() { return "999"; }
-    function getFPPPatchVersion() { return "Unknown"; }
+    function getFPPPatchVersion() { return ""; }
     function getFPPVersionTriplet() { return "99.999"; }
+    ?>
 
-function writeFPPVersionJavascriptFunctions() {
-?>
-
-    function getFPPVersion() { return "Unknown"; }
-    function getFPPBranch() { return "Unknown"; }
-    function getFPPVersionFloat() { return 99.999; }
-    function getFPPVersionFloatStr() { return "99.999"; }
-    function getFPPMajorVersion() { return "99"; }
-    function getFPPMinorVersion() { return "999"; }
-    function getFPPPatchVersion() { return "Unknown"; }
-    function getFPPVersionTriplet() { return "99.999"; }
-?>
-
-</script>
-<?
+    </script>
+    <?
 }
 ?>
