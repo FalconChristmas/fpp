997c3d244bc1397689cf9d2e6eb6b4c8c6fe9fed
Greg Hormann <ghormann@gmail.com>
2025-03-03 19:42:15 -0500

    Make help page responsive html

--- www/help.php
+++ www/help.php
@@ -22,72 +22,93 @@
 		$activeParentMenuItem = 'help';
 		include 'menu.inc'; ?>
 		<div class="mainContainer">
+
 			<h1 class="title">Help</h1>
 			<div class="pageContent">
-				<div style="margin:0 auto;"> <br>
-					<fieldset class="fieldSection">
-						<legend>Get Help</legend>
-						<div style="overflow: hidden; padding: 10px;">
-                        <h4>Places to get help</h4>
-                                    <a class="<?= $externalLinkClass ?> link-to-fpp-manual"
-                                    href="https://added-by-javascript.net" target="_blank" rel="noopener noreferrer"><i
-                                        class="fas fa-book"></i>
-                                    FPP has an extensive manual - FPP Manual <i class="fas fa-external-link-alt external-link"></i></a>
-                                <br>
-                                <a class="<?= $externalLinkClass ?>" href="https://www.facebook.com/groups/1554782254796752"
-                                    target="_blank" rel="noopener noreferrer"><i class="fas fa-comments"></i> Got a question? Join the FPP Facebook group <i
-                                        class="fas fa-external-link-alt external-link"></i></a>
-                                <a class="<?= $externalLinkClass ?>" href="https://falconchristmas.com/forum"
-                                    target="_blank" rel="noopener noreferrer"><i class="fas fa-comments"></i> or the FPP Forums <i
-                                        class="fas fa-external-link-alt external-link"></i></a>
-                                        <br>
-                                <a class="<?= $externalLinkClass ?>"
-                                    href="https://xlights.org/zoom/" target="_blank"
-                                    rel="noopener noreferrer"><i class="fas fa-headphones"></i> Prefer to talk? Join the xLights Zoom room <i
-                                        class="fas fa-external-link-alt external-link"></i></a>
-                                        <br>
-                                <a class="<?= $externalLinkClass ?>"
-                                    href="https://github.com/FalconChristmas/fpp/issues" target="_blank"
-                                    rel="noopener noreferrer"><i class="fas fa-bug"></i> Need to log a bug? - Issue Tracker <i
-                                        class="fas fa-external-link-alt external-link"></i></a>
-                                <br><br><h4>FPP API</h4>
-                                <a class="dropdown-item" href="apihelp.php"><i class="fas fa-plug"></i> REST API
-                                    Help</a>
-                                <a class="dropdown-item" href="commandhelp.php"><i class="fas fa-cubes"></i> FPP
-                                    Commands Help</a>
+				<h2>Get Help</h2>
+				<div class="row">
+					<div class="col-sm-12 col-md-6 col-xl-4">
+						<h3>Places to get help</h3>
+						<ul class="no-bullets">
+							<li><a class="<?= $externalLinkClass ?> link-to-fpp-manual"
+									href="https://added-by-javascript.net" target="_blank" rel="noopener noreferrer"><i
+										class="fas fa-book"></i>
+									FPP has an extensive manual - FPP Manual <i
+										class="fas fa-external-link-alt external-link"></i></a></li>
+							<li><a class="<?= $externalLinkClass ?>"
+									href="https://www.facebook.com/groups/1554782254796752" target="_blank"
+									rel="noopener noreferrer"><i class="fas fa-comments"></i> Got a
+									question?
+									Join
+									the FPP Facebook group <i class="fas fa-external-link-alt external-link"></i></a>
+							</li>
+							<li><a class="<?= $externalLinkClass ?>" href="https://falconchristmas.com/forum"
+									target="_blank" rel="noopener noreferrer"><i class="fas fa-comments"></i> or the FPP
+									Forums <i class="fas fa-external-link-alt external-link"></i></a></li>
 
-							<div>
-                            <br><br><h4>Specific Topics</h4>
-								<ul>
-									<li><a href='javascript:void(0);'
-											onClick="helpPage='help/backup.php'; DisplayHelp();">Backup & Restore</a>
-									</li>
-									<li><a href='javascript:void(0);'
-											onClick="helpPage='help/channeloutputs.php'; DisplayHelp();">Channel
-											Outputs</a></li>
-									<li><a href='javascript:void(0);'
-											onClick="helpPage='help/gpio.php'; DisplayHelp();">GPIO Input Triggers</a>
-									</li>
-									<li><a href='javascript:void(0);'
-											onClick="helpPage='help/networkconfig.php'; DisplayHelp();">Network
-											Config</a></li>
-									<li><a href='javascript:void(0);'
-											onClick="helpPage='help/outputprocessors.php'; DisplayHelp();">Output
-											Processors</a></li>
-									<li><a href='javascript:void(0);'
-											onClick="helpPage='help/pixeloverlaymodels.php'; DisplayHelp();">Real-Time Pixel
-											Overlay Models</a></li>
-									<li><a href='javascript:void(0);'
-											onClick="helpPage='help/scriptbrowser.php'; DisplayHelp();">Script
-											Repository Browser</a></li>
-								</ul>
-							</div>
-					</fieldset>
+							<li><a class="<?= $externalLinkClass ?>" href="https://xlights.org/zoom/" target="_blank"
+									rel="noopener noreferrer"><i class="fas fa-headphones"></i> Prefer to talk? Join the
+									xLights
+									Zoom room <i class="fas fa-external-link-alt external-link"></i></a></li>
+
+							<li><a class="<?= $externalLinkClass ?>"
+									href="https://github.com/FalconChristmas/fpp/issues" target="_blank"
+									rel="noopener noreferrer"><i class="fas fa-bug"></i> Need to log a bug?
+									-
+									Issue
+									Tracker <i class="fas fa-external-link-alt external-link"></i></a></li>
+
+						</ul>
+					</div>
+
+					<div class="col-sm-12 col-md-6 col-xl-4">
+						<h3>FPP API</h3>
+						<ul class="no-bullets">
+							<li>
+								<a class="dropdown-item" href="apihelp.php"><i class="fas fa-plug"></i> REST API
+									Help</a>
+							</li>
+							<li>
+								<a class="dropdown-item" href="commandhelp.php"><i class="fas fa-cubes"></i> FPP
+									Commands Help</a>
+							</li>
+
+						</ul>
+					</div>
+
+					<div class="col-sm-12 col-md-6 col-xl-4">
+						<h3>Specific Topics</h3>
+						<ul>
+							<li><a href='javascript:void(0);'
+									onClick="helpPage='help/backup.php'; DisplayHelp();">Backup &
+									Restore</a>
+							</li>
+							<li><a href='javascript:void(0);'
+									onClick="helpPage='help/channeloutputs.php'; DisplayHelp();">Channel
+									Outputs</a></li>
+							<li><a href='javascript:void(0);' onClick="helpPage='help/gpio.php'; DisplayHelp();">GPIO
+									Input Triggers</a>
+							</li>
+							<li><a href='javascript:void(0);'
+									onClick="helpPage='help/networkconfig.php'; DisplayHelp();">Network
+									Config</a></li>
+							<li><a href='javascript:void(0);'
+									onClick="helpPage='help/outputprocessors.php'; DisplayHelp();">Output
+									Processors</a></li>
+							<li><a href='javascript:void(0);'
+									onClick="helpPage='help/pixeloverlaymodels.php'; DisplayHelp();">Real-Time Pixel
+									Overlay Models</a></li>
+							<li><a href='javascript:void(0);'
+									onClick="helpPage='help/scriptbrowser.php'; DisplayHelp();">Script
+									Repository Browser</a></li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</div>
 		<?php include 'common/footer.inc'; ?>
 	</div>
+
 </body>
 
 </html>
