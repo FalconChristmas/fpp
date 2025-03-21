d4601b3138fa8a426b7382864dc5699e3d17cb5f
Daniel Kulp <dan@kulp.com>
2025-03-21 13:42:16 -0400

    First pass at new shift register based panel output for PB2

---
+++ src/makefiles/libfpp-co-BBShiftPanel.mk
@@ -0,0 +1,24 @@
+# BeagleBone
+ifeq '$(ARCH)' 'BeagleBone 64'
+
+OBJECTS_fpp_co_BBShiftPanel_so += non-gpl/BBShiftPanel/BBShiftPanel.o non-gpl/BBShiftPanel/MapPixelsByDepth16.o
+
+LIBS_fpp_co_BBShiftPanel_so += -L. -lfpp -ljsoncpp -lfpp_capeutils -Wl,-rpath=$(SRCDIR):.
+
+TARGETS += libfpp-co-matrix-BBShiftPanel.$(SHLIB_EXT) non-gpl/BBShiftPanel/BBShiftPanel.out
+OBJECTS_ALL+=$(OBJECTS_fpp_co_BBShiftPanel_so)
+
+CXXFLAGS_non-gpl/BBShiftPanel/BBShiftPanel.o+=-Wno-address-of-packed-member
+
+non-gpl/BBShiftPanel/MapPixelsByDepth16.o: non-gpl/BBShiftPanel/MapPixelsByDepth16.o_ispc
+	@cp -f non-gpl/BBShiftPanel/MapPixelsByDepth16.o_ispc non-gpl/BBShiftPanel/MapPixelsByDepth16.o
+
+libfpp-co-matrix-BBShiftPanel.$(SHLIB_EXT): $(OBJECTS_fpp_co_BBShiftPanel_so) libfpp.$(SHLIB_EXT) libfpp_capeutils.$(SHLIB_EXT)
+	$(CCACHE) $(CC) -shared $(CFLAGS_$@) $(OBJECTS_fpp_co_BBShiftPanel_so) $(LIBS_fpp_co_BBShiftPanel_so) $(LDFLAGS) $(LDFLAGS_fpp_co_BBShiftPanel_so) -o $@
+
+non-gpl/BBShiftPanel/BBShiftPanel.out: non-gpl/BBShiftPanel/BBShiftPanel.asm
+	/usr/bin/cpp -P -I/opt/fpp/src/pru -DAM62X "/opt/fpp/src/non-gpl/BBShiftPanel/BBShiftPanel.asm" > "/tmp/BBShiftPanel.asm"
+	clpru -v3 -o -DAM62X --endian=little --hardware_mac=on --obj_directory /tmp "/tmp/BBShiftPanel.asm"
+	clpru -v3 -DAM62X -z /opt/fpp/src/pru/AM62x_PRU0.cmd -o "non-gpl/BBShiftPanel/BBShiftPanel.out" "/tmp/BBShiftPanel.obj" -i/usr/share/ti/cgt-pru/lib -i/usr/share/ti/cgt-pru/include --library=libc.a
+	@rm "/tmp/BBShiftPanel.asm" "/tmp/BBShiftPanel.obj"
+endif
