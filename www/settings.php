6eeae4764bd4c645cfd01b44f17752a9d74a4fb4
Daniel Kulp <dan@kulp.com>
2025-06-24 14:15:40 -0400

    UI is all in "US" english...  Fix some miss-spellings

--- www/settings.php
+++ www/settings.php
@@ -118,14 +118,14 @@ $id = 0;
                                 </a>
                             </li>
                             <? if ((!$settings["IsDesktop"]) || ($settings['uiLevel'] > 2)) { ?>
-                                    <li class="nav-item">
-                                        <? $tabIDs["Localisation"] = $id++; ?>
-                                        <a class="nav-link" id="settings-localisation-tab" data-bs-toggle="tab"
-                                            data-bs-target="#settings-localisation" href="#settings-localisation" data-option="Localisation" role="tab"
-                                            aria-controls="settings-localisation" aria-selected="true">
-                                            Localisation
-                                        </a>
-                                    </li>
+                                                    <li class="nav-item">
+                                                        <? $tabIDs["Localization"] = $id++; ?>
+                                                        <a class="nav-link" id="settings-localization-tab" data-bs-toggle="tab"
+                                                            data-bs-target="#settings-localization" href="#settings-localization" data-option="Localization" role="tab"
+                                                            aria-controls="settings-localization" aria-selected="true">
+                                                            Localization
+                                                        </a>
+                                                    </li>
                             <? } ?>
                             <li class="nav-item">
                                 <? $tabIDs["UI"] = $id++; ?>
@@ -138,24 +138,24 @@ $id = 0;
                             <? if ($settings["Platform"] != "MacOS") {
                                 if ($uiLevel >= 1 || $tabId == "Email") {
                                     $tabIDs["Email"] = $id++; ?>
-                                            <li class="nav-item">
-                                                <a class="nav-link" id="settings-email-tab" data-bs-toggle="tab"
-                                                    data-bs-target="#settings-email" href="#settings-email" data-option="Email"
-                                                    role="tab" aria-controls="settings-email" aria-selected="true">
-                                                    Email
-                                                </a>
-                                            </li>
-                                    <? }
+                                                                            <li class="nav-item">
+                                                                                <a class="nav-link" id="settings-email-tab" data-bs-toggle="tab"
+                                                                                    data-bs-target="#settings-email" href="#settings-email" data-option="Email"
+                                                                                    role="tab" aria-controls="settings-email" aria-selected="true">
+                                                                                    Email
+                                                                                </a>
+                                                                            </li>
+                                                    <? }
                             } ?>
                             <? if ($uiLevel >= 1 || $tabId == "MQTT") { ?>
-                                    <li class="nav-item">
-                                        <? $tabIDs["MQTT"] = $id++; ?>
-                                        <a class="nav-link" id="settings-mqtt-tab" data-bs-toggle="tab"
-                                            data-bs-target="#settings-mqtt" href="#settings-mqtt" data-option="MQTT" role="tab"
-                                            aria-controls="settings-mqtt" aria-selected="true">
-                                            MQTT
-                                        </a>
-                                    </li>
+                                                    <li class="nav-item">
+                                                        <? $tabIDs["MQTT"] = $id++; ?>
+                                                        <a class="nav-link" id="settings-mqtt-tab" data-bs-toggle="tab"
+                                                            data-bs-target="#settings-mqtt" href="#settings-mqtt" data-option="MQTT" role="tab"
+                                                            aria-controls="settings-mqtt" aria-selected="true">
+                                                            MQTT
+                                                        </a>
+                                                    </li>
                             <? } ?>
                             <li class="nav-item">
                                 <? $tabIDs["Privacy"] = $id++; ?>
@@ -166,14 +166,14 @@ $id = 0;
                                 </a>
                             </li>
                             <? if ($uiLevel >= 1 || $tabId == "Output") { ?>
-                                    <li class="nav-item">
-                                        <? $tabIDs["Output"] = $id++; ?>
-                                        <a class="nav-link" id="settings-output-tab" data-bs-toggle="tab"
-                                            data-bs-target="#settings-output" href="#settings-output" data-option="Output"
-                                            role="tab" aria-controls="settings-output" aria-selected="true">
-                                            Input/Output
-                                        </a>
-                                    </li>
+                                                    <li class="nav-item">
+                                                        <? $tabIDs["Output"] = $id++; ?>
+                                                        <a class="nav-link" id="settings-output-tab" data-bs-toggle="tab"
+                                                            data-bs-target="#settings-output" href="#settings-output" data-option="Output"
+                                                            role="tab" aria-controls="settings-output" aria-selected="true">
+                                                            Input/Output
+                                                        </a>
+                                                    </li>
                             <? } ?>
                             <li class="nav-item">
                                 <? $tabIDs["Logging"] = $id++; ?>
@@ -192,14 +192,14 @@ $id = 0;
                                 </a>
                             </li>
                             <? if ($uiLevel >= $storageUILevel || $tabId == "Storage") { ?>
-                                    <li class="nav-item">
-                                        <? $tabIDs["Storage"] = $id++; ?>
-                                        <a class="nav-link" id="settings-storage-tab" data-bs-toggle="tab"
-                                            data-bs-target="#settings-storage" href="#settings-storage" data-option="Storage"
-                                            role="tab" aria-controls="settings-storage" aria-selected="true">
-                                            Storage
-                                        </a>
-                                    </li>
+                                                    <li class="nav-item">
+                                                        <? $tabIDs["Storage"] = $id++; ?>
+                                                        <a class="nav-link" id="settings-storage-tab" data-bs-toggle="tab"
+                                                            data-bs-target="#settings-storage" href="#settings-storage" data-option="Storage"
+                                                            role="tab" aria-controls="settings-storage" aria-selected="true">
+                                                            Storage
+                                                        </a>
+                                                    </li>
                             <? } ?>
                             <li class="nav-item">
                                 <? $tabIDs["System"] = $id++; ?>
@@ -210,15 +210,15 @@ $id = 0;
                                 </a>
                             </li>
                             <? if ($uiLevel >= 3 || $tabId == "Developer") { ?>
-                                    <li class="nav-item">
-                                        <? $tabIDs["Developer"] = $id++; ?>
-                                        <a class="nav-link" id="settings-developer-tab" data-bs-toggle="tab"
-                                            data-bs-target="#settings-developer" href="#settings-developer"
-                                            data-option="Developer" role="tab" aria-controls="settings-developer"
-                                            aria-selected="true">
-                                            Developer
-                                        </a>
-                                    </li>
+                                                    <li class="nav-item">
+                                                        <? $tabIDs["Developer"] = $id++; ?>
+                                                        <a class="nav-link" id="settings-developer-tab" data-bs-toggle="tab"
+                                                            data-bs-target="#settings-developer" href="#settings-developer"
+                                                            data-option="Developer" role="tab" aria-controls="settings-developer"
+                                                            aria-selected="true">
+                                                            Developer
+                                                        </a>
+                                                    </li>
                             <? } ?>
                         </ul>
                         <div id="settingsManagerTabsContent" class="tab-content">
@@ -229,21 +229,21 @@ $id = 0;
 
                         <br>
                         <? if ($uiLevel >= 1) { ?>
-                                <div class="backdrop">
-                                    <div class="row">
-                                        <div class="col-auto"><i class='fas fa-fw fa-graduation-cap ui-level-1'></i> - Advanced
-                                            Level Setting</div>
-                                        <? if ($uiLevel >= 2) { ?>
-                                            <div class="col-auto"><i class='fas fa-fw fa-flask ui-level-2'></i> - Experimental Level
-                                                Setting</div>
-                                        <? } ?>
-                                        <? if ($uiLevel >= 3) { ?>
-                                            <div class="col-auto"><i class='fas fa-fw fa-code ui-level-3'></i> - Developer Level
-                                                Setting</div>
-                                        <? } ?>
-                                    </div>
+                                                <div class="backdrop">
+                                                    <div class="row">
+                                                        <div class="col-auto"><i class='fas fa-fw fa-graduation-cap ui-level-1'></i> - Advanced
+                                                            Level Setting</div>
+                                                        <? if ($uiLevel >= 2) { ?>
+                                                                            <div class="col-auto"><i class='fas fa-fw fa-flask ui-level-2'></i> - Experimental Level
+                                                                                Setting</div>
+                                                        <? } ?>
+                                                        <? if ($uiLevel >= 3) { ?>
+                                                                            <div class="col-auto"><i class='fas fa-fw fa-code ui-level-3'></i> - Developer Level
+                                                                                Setting</div>
+                                                        <? } ?>
+                                                    </div>
 
-                                </div>
+                                                </div>
                         <? } ?>
 
                     </div>
@@ -267,7 +267,7 @@ $id = 0;
 if(location.hash){
     if( location.hash == '#settings-system'){ activeTabNumber = tabIDs["System"]}
     if( location.hash == '#settings-av'){ activeTabNumber = tabIDs["AV"]}
-    if( location.hash == '#settings-localisation'){ activeTabNumber = tabIDs["Localisation"] }
+    if( location.hash == '#settings-localization'){ activeTabNumber = tabIDs["Localization"] }
     if( location.hash == '#settings-ui'){ activeTabNumber = tabIDs["UI"] }
     if( location.hash == '#settings-email'){ activeTabNumber = tabIDs["Email"] }
     if( location.hash == '#settings-mqtt'){ activeTabNumber = tabIDs["MQTT"] }
@@ -314,7 +314,7 @@ if(location.hash){
                         };
                     }
                     $('a[data-bs-toggle="tab"]').on('shown.bs.tab', function (e) {
-                        if (($(this).attr("href") == '#settings-localisation') &&
+                        if (($(this).attr("href") == '#settings-localization') &&
                             ($(this).parent().hasClass('active'))) {
                             UpdateCurrentTime();
                         } else if (statusTimeout != null) {
@@ -357,7 +357,7 @@ if(location.hash){
                     },
                     activate: function(event, ui) {
                         $('.ui-tooltip').hide();
-                        if (ui.newTab.find("a").attr("href") == 'settings-localisation.php') {
+                        if (ui.newTab.find("a").attr("href") == 'settings-localization.php') {
                             UpdateCurrentTime();
                     } else if (statusTimeout != null) {
             clearTimeout(statusTimeout);
