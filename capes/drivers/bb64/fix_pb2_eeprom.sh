666189e98e0be7e54258a560ef98be100a64badd
Daniel Kulp <dan@kulp.com>
2025-10-29 12:18:11 -0400

    Add a script to "fix" the PB2 eeprom if it's empty

---
+++ capes/drivers/bb64/fix_pb2_eeprom.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# create a basic eeprom, this specifies week one of 2025 which no
+# real PB2's were made so won't  conflict with  existing
+
+cat > /tmp/eeprom.txt <<EOF
+00000000: aa55 33ee 0137 0010 2e00 504f 434b 4554  .U3..7....POCKET
+00000010: 4245 4147 4c32 4131 3047 4131 3030 3030  BEAGL2A10GA10000
+00000020: 3047 4130 4131 3030 3031 3031 3235 5042  0GA0A100011625PB
+00000030: 3230 3030 3337 3335 1102 00a8 12fe ffff  20003735........
+EOF
+cat /tmp/eeprom.txt | xxd -r > /tmp/eeprom
+
+# Now generate a random count for this instance
+NEWID=$(head /dev/urandom | tr -dc 0-9 | head -c 6)
+
+echo $NEWID | dd of=/tmp/eeprom bs=1 seek=50 count=6 conv=notrunc
+dd if=/tmp/eeprom of=/sys/bus/i2c/devices/0-0050/eeprom
