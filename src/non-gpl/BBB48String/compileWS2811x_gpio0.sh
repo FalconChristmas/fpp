28ffc36a3ddec1c2f8a4212795a0623199c10b26
Daniel Kulp <dan@kulp.com>
2025-11-18 16:06:56 -0500

    Maybe fix issues with starting the second PRU for BB48String capes that need it

--- src/non-gpl/BBB48String/compileWS2811x_gpio0.sh
+++ src/non-gpl/BBB48String/compileWS2811x_gpio0.sh
@@ -3,9 +3,11 @@
 echo $0 $@
 cd /tmp
 
+CPPFLAG="-DAM33XX"
+
 # clpru versions
-/usr/bin/cpp -P -I/opt/fpp/src/pru $@ /opt/fpp/src/non-gpl/BBB48String/FalconWS281x.asm > /tmp/FalconWS281x_gpio0.asm
-clpru -v3 -o $@ /tmp/FalconWS281x_gpio0.asm
+/usr/bin/cpp -P -I/opt/fpp/src/pru $@ $CPPFLAG /opt/fpp/src/non-gpl/BBB48String/FalconWS281x.asm > /tmp/FalconWS281x_gpio0.asm
+clpru -v3 -o $@ $CPPFLAG /tmp/FalconWS281x_gpio0.asm
 clpru -v3 -z --entry_point main /opt/fpp/src/pru/AM335x_PRU.cmd  -o FalconWS281x_gpio0.out FalconWS281x_gpio0.obj
 #objdump -h FalconWS281x_gpio0.out | grep .text |  awk '{print "dd if='FalconWS281x_gpio0.out' of='FalconWS281x_gpio0.bin' bs=1 count=$[0x" $3 "] skip=$[0x" $6 "]"}'  | bash
 
