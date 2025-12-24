0b753d348d9835e5c38194451d7cfaf1a84be1af
Daniel Kulp <dan@kulp.com>
2025-12-24 08:36:34 -0500

    Add SYS_NICE to docker since we start fppd with nice

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -17,7 +17,7 @@ docker build -t falconchristmas/fpp:latest -f Docker/Dockerfile  --build-arg FPP
 # However, editing files from OSX may take an extra second
 # before the changes are available within the docker container.
 
-# docker run --name fpp --hostname fppDocker -t -i -p 80:80 -p 4048:4048/udp -p 5568:5568/udp -p 32320:32320/udp -v ${PWD}:/opt/fpp${DOCKER_MOUNT_FLAG} falconchristmas/fpp:latest
+# docker run --name fpp --hostname fppDocker -t -i -p 80:80 -p 4048:4048/udp -p 5568:5568/udp -p 32320:32320/udp -v ${PWD}:/opt/fpp${DOCKER_MOUNT_FLAG} --cap-add=SYS_NICE falconchristmas/fpp:latest
 
 # A sample docker-compose.yml file to use the docker image is also
 # included in the Docker directory.
