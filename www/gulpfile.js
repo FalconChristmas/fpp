f5f61fa9d7e6ff4fa922f6b61360bc74871e30c1
OnlineDynamic <stuart_ledingham@hotmail.com>
2025-12-28 23:25:00 +0000

    update dependencies in package.json

--- www/gulpfile.js
+++ www/gulpfile.js
@@ -18,39 +18,37 @@ $gulp watch-bs
 
 var gulp = require('gulp');
 var plumber = require('gulp-plumber');
-var sass = require('gulp-sass')(require('node-sass'));
+var sass = require('gulp-sass')(require('sass'));
 var postcss = require('gulp-postcss');
 var touch = require('gulp-touch-fd');
 var browserSync = require('browser-sync').create();
 var autoprefixer = require('autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var cfg = {
-	"browserSyncOptions": {
-        "server": {
-			"baseDir": "./",
-			"index": "components.html"
-        },
-		"notify": false,
+	browserSyncOptions: {
+		server: {
+			baseDir: './',
+			index: 'components.html'
+		},
+		notify: false,
 		ui: {
 			port: 3181
 		},
 		port: 3180
-
 	},
 
-	"browserSyncWatchFiles": [
-		"./css/fpp-bootstrap/dist/fpp-bootstrap.css",
-		"./css/fpp.css",
-		"./components.html"
+	browserSyncWatchFiles: [
+		'./css/fpp-bootstrap/dist/fpp-bootstrap.css',
+		'./css/fpp.css',
+		'./components.html'
 		//"./**/*.html"
 	]
-}
+};
 
 var paths = {
-	scss: "./css/fpp-bootstrap/src",
-	css: "./css/fpp-bootstrap/dist",
-
-}
+	scss: './css/fpp-bootstrap/src',
+	css: './css/fpp-bootstrap/dist'
+};
 
 // Run:
 // gulp sass
@@ -78,17 +76,15 @@ gulp.task('sass', function () {
 // gulp watch
 // Starts watcher. Watcher runs gulp sass task on changes
 gulp.task('watch', function () {
-	gulp.watch([`${paths.scss}/**/*.scss`, `${paths.scss}/*.scss`], gulp.series('styles'));
-
+	gulp.watch(
+		[`${paths.scss}/**/*.scss`, `${paths.scss}/*.scss`],
+		gulp.series('styles')
+	);
 });
-
-
-
 
 gulp.task('styles', function (callback) {
 	return gulp.series('sass')(callback);
 });
-
 
 gulp.task('browser-sync', function () {
 	browserSync.init(cfg.browserSyncWatchFiles, cfg.browserSyncOptions);
@@ -99,6 +95,5 @@ gulp.task('browser-sync', function () {
 // Starts watcher and launches component page with browsersync for live previewing.
 
 gulp.task('watch-bs', gulp.parallel('browser-sync', 'watch'));
-
 
 gulp.task('default', gulp.series('styles'));
