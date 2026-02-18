1547c73b4a234f97cb91969548f8d20f50cac99d
Daniel Kulp <dan@kulp.com>
2026-02-18 11:26:48 -0500

    If the original eeprom has a four digit count  already set, use it

--- capes/drivers/bb64/fix_pb2_eeprom.sh
+++ capes/drivers/bb64/fix_pb2_eeprom.sh
@@ -3,6 +3,9 @@
 # create a basic eeprom, this specifies week one of 2025 which no
 # real PB2's were made so won't  conflict with  existing
 
+OLDNUM=$(dd if=/sys/bus/i2c/devices/0-0050/eeprom bs=1 skip=52 count=4 2>/dev/null)
+OLDNUMHEX=$(dd if=/sys/bus/i2c/devices/0-0050/eeprom bs=1 skip=52 count=4 2>/dev/null | xxd -p)
+
 cat > /tmp/eeprom.txt <<EOF
 00000000: aa55 33ee 0137 0010 2e00 504f 434b 4554  .U3..7....POCKET
 00000010: 4245 4147 4c32 4131 3047 4131 3030 3030  BEAGL2A10GA10000
@@ -11,8 +14,12 @@ cat > /tmp/eeprom.txt <<EOF
 EOF
 cat /tmp/eeprom.txt | xxd -r > /tmp/eeprom
 
-# Now generate a random count for this instance
-NEWID=$(head /dev/urandom | tr -dc 0-9 | head -c 6)
+if [ $OLDNUMHEX != "ffffffff" ]; then
+    NEWID=$OLDNUM
+else
+    # Now generate a random count for this instance
+    NEWID=$(head /dev/urandom | tr -dc 0-9 | head -c 4)
+fi
 
-echo $NEWID | dd of=/tmp/eeprom bs=1 seek=50 count=6 conv=notrunc
+echo $NEWID | dd of=/tmp/eeprom bs=1 seek=52 count=4 conv=notrunc
 dd if=/tmp/eeprom of=/sys/bus/i2c/devices/0-0050/eeprom
