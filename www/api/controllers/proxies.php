6197dcab3cf35bf570f976addec9ee9fb511012e
Justin J. Novack <jnovack@gmail.com>
2026-05-14 09:52:20 -0400

    fix: add a helper to process body messaging

--- www/api/controllers/proxies.php
+++ www/api/controllers/proxies.php
@@ -17,7 +17,7 @@ require_once(__DIR__ . "/../../config.php");
 function RemoteAction_v1()
 {
     global $settings;
-    $body = json_decode(file_get_contents('php://input'), true);
+    $body = get_json_body();
     $ip = htmlspecialchars(isset($body['ip']) ? $body['ip'] : '');
     $action = htmlspecialchars(isset($body['action']) ? $body['action'] : '');
 
