feecf1837d7cbb0b51160c0a3e2c615db9e50645
Daniel Kulp <dan@kulp.com>
2026-06-16 06:11:52 -0400

    Break FPPINIT into chunks to allow slight faster iteration on the slower BBB's

--- src/makefiles/fppinit.mk
+++ src/makefiles/fppinit.mk
@@ -1,6 +1,6 @@
 ifeq '$(BUILD_FPPINIT)' '1'
 OBJECTS_fppinit += \
-	boot/FPPINIT.o  common_mini.o
+	boot/FPPINIT.o  boot/FPPINIT_Config.o  boot/FPPINIT_Network.o  boot/FPPINIT_Audio.o  common_mini.o
 
 TARGETS += fppinit
 OBJECTS_ALL+=$(OBJECTS_fppinit)
@@ -8,10 +8,15 @@ LIBS_fppinit = -ljsoncpp -lsystemd
 
 ifeq '$(BUILD_FPPCAPEDETECT)' '1'
 CXXFLAGS_boot/FPPINIT.o+=-DCAPEDETECT=1
+CXXFLAGS_boot/FPPINIT_Config.o+=-DCAPEDETECT=1
+CXXFLAGS_boot/FPPINIT_Network.o+=-DCAPEDETECT=1
+CXXFLAGS_boot/FPPINIT_Audio.o+=-DCAPEDETECT=1
 LIBS_fppinit += \
 	-L. -lcrypto -Wl,-rpath=$(SRCDIR):.
 FPPINIT_DEPS=libfpp_capeutils.so
 endif
+
+boot/FPPINIT.o  boot/FPPINIT_Config.o  boot/FPPINIT_Network.o  boot/FPPINIT_Audio.o: CFLAGS := $(filter-out -fpch-preprocess -g1,$(CFLAGS))
 
 
 fppinit: $(OBJECTS_fppinit)  $(FPPINIT_DEPS)
