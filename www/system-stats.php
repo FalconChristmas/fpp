76fce7cdc31d5ec18fc6913d38a2f6a52da60f24
jessica12ryan <jessica12ryan@gmail.com>
2026-06-19 21:05:26 -0300

    Fix uptime and memory usage

--- www/system-stats.php
+++ www/system-stats.php
@@ -328,8 +328,8 @@ if (isset($_GET['cpu'])) {
                 }
 
                 // Uptime
-                if (data.uptimeTotalSeconds) {
-                    uptimeSeconds = parseInt(data.uptimeTotalSeconds);
+                if (data.systemUptimeTotalSeconds) {
+                    uptimeSeconds = parseInt(data.systemUptimeTotalSeconds);
                     updateUptimeDisplay();
                     if (!uptimeInterval) {
                         uptimeInterval = setInterval(updateUptimeDisplay, 1000);
@@ -716,7 +716,7 @@ if (isset($_GET['cpu'])) {
                                             </circle>
                                         </svg>
                                         <div class="fpp-gauge__value" id="memValue">
-                                            <?php printf('%.0f', $memTotalUsage); ?>%
+                                            <?php printf('%.0f', $memUsage); ?>%
                                             <span
                                                 class="fpp-gauge__total"><?php echo formatMemBytes($memTotal); ?></span>
                                         </div>
@@ -792,7 +792,7 @@ if (isset($_GET['cpu'])) {
                     <div class="col-md-4">
                         <div class="card compact-card">
                             <div class="card-header">
-                                <h3><i class="fas fa-clock"></i> FPP Uptime</h3>
+                                <h3><i class="fas fa-clock"></i> System Uptime</h3>
                             </div>
                             <div class="card-body uptime-display">
                                 <div class="uptime-counters">
